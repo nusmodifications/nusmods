64ab5330d78b638e1f400bf8d71fbeb3156cac28
Eu Beng Hee <ah@beng.me>
2012-08-22 00:03:58 +0800

    Cleanup whitespace

--- short_url.php
+++ short_url.php
@@ -5,9 +5,9 @@ curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, array(
-	'action'    => 'shorturl',
+  'action'    => 'shorturl',
   'format'    => 'json',
-	'signature' => '3ef261dc03',
+  'signature' => '3ef261dc03',
   'url'       => $_GET['url'],
 ));
 $data = curl_exec($ch);
