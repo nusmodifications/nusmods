356a6c0e34281179ec4e405244b6b7f12fbb29c7
ahbeng <ah@beng.me>
2013-03-06 05:14:46 +0800

    Remove Exhibit 3

--- src/js/exhibit3/locales/manifest.js
+++
@@ -1,6 +0,0 @@
-$(document).bind("registerLocales.exhibit", function() {
-    $(document).trigger("beforeLocalesRegistered.exhibit");
-    new Exhibit.Locale("default", Exhibit.urlPrefix + "locales/en/locale.js");
-    new Exhibit.Locale("en", Exhibit.urlPrefix + "locales/en/locale.js");
-    $(document).trigger("localesRegistered.exhibit");
-});
