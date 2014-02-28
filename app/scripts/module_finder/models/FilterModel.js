4fbafb367a33af2d09464b1268fdada771b9e936
Beng Hee Eu <beng@benghee.eu>
2014-03-01 01:39:44 +0800

    Fix filter selected default - backbone.picky does not use model attributes

--- app/scripts/module_finder/models/FilterModel.js
+++ app/scripts/module_finder/models/FilterModel.js
@@ -2,12 +2,9 @@ define(['underscore', 'backbone', 'backbone.picky'], function(_, Backbone) {
   'use strict';
 
   var Filter = Backbone.Model.extend({
-    defaults: {
-      selected: false
-    },
-
     initialize: function() {
       _.extend(this, new Backbone.Picky.Selectable(this));
+      this.selected = false;
     }
   });
 
