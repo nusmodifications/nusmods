2ebfcaf50b9b334b16cd020c75a1e9b9b862ee17
Beng Hee Eu <beng@benghee.eu>
2014-03-01 02:13:46 +0800

    Add localforage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,26 +9,31 @@ require.config({
   ],
   paths: {
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    'backbone.localforage': '../bower_components/localforage/dist/backbone.localforage.min',
     'backbone.marionette': '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
     'backbone.picky': '../bower_components/backbone.picky/lib/amd/backbone.picky',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
-    backbone: '../bower_components/backbone/backbone',
     'bootstrap-button': '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/button',
     'bootstrap-dropdown': '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/dropdown',
-    hbs: '../bower_components/require-handlebars-plugin/hbs',
-    jquery: '../bower_components/jquery/jquery',
+    'jquery-ui-touch-punch-improved': '../bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved',
     'jquery.ui.core': '../bower_components/jquery.ui/ui/jquery.ui.core',
-    'jquery.ui.widget': '../bower_components/jquery.ui/ui/jquery.ui.widget',
-    'jquery.ui.mouse': '../bower_components/jquery.ui/ui/jquery.ui.mouse',
     'jquery.ui.draggable': '../bower_components/jquery.ui/ui/jquery.ui.draggable',
     'jquery.ui.droppable': '../bower_components/jquery.ui/ui/jquery.ui.droppable',
-    'jquery-ui-touch-punch-improved': '../bower_components/jquery-ui-touch-punch-improved/jquery.ui.touch-punch-improved',
+    'jquery.ui.mouse': '../bower_components/jquery.ui/ui/jquery.ui.mouse',
+    'jquery.ui.widget': '../bower_components/jquery.ui/ui/jquery.ui.widget',
+    backbone: '../bower_components/backbone/backbone',
+    hbs: '../bower_components/require-handlebars-plugin/hbs',
+    jquery: '../bower_components/jquery/jquery',
     json: '../bower_components/requirejs-plugins/src/json',
+    localforage: '../bower_components/localforage/dist/localforage.min',
     qtip2: '../bower_components/qtip2/jquery.qtip',
     select2: '../bower_components/select2/select2',
     zeroclipboard: '../bower_components/zeroclipboard/ZeroClipboard'
   },
   shim: {
+    'backbone.localforage': [
+      'backbone'
+    ],
     'bootstrap-button': [
       'jquery'
     ],
