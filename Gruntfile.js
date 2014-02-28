c30dba4c2812077ddbc4acd8184e10dcf7792c43
Beng Hee Eu <beng@benghee.eu>
2014-03-01 01:32:06 +0800

    Change hostname to 0.0.0.0 to access the server from outside

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
                 port: 9000,
                 livereload: 35729,
                 // Change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
