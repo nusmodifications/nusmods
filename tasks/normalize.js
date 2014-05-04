83e3ad5946460242774407d1a502e2eb257a55ee
Beng Hee Eu <beng@benghee.eu>
2014-05-05 01:17:21 +0800

    Fix missing dayTextToCode mapping

--- tasks/normalize.js
+++ tasks/normalize.js
@@ -20,6 +20,16 @@ module.exports = function (grunt) {
 
     var facultyDepartments = {};
 
+    var dayTextToCode = {
+      MONDAY: '1',
+      TUESDAY: '2',
+      WEDNESDAY: '3',
+      THURSDAY: '4',
+      FRIDAY: '5',
+      SATURDAY: '6',
+      SUNDAY: '7'
+    };
+
     var modules = _.compact(_.map(consolidated, function (rawMod) {
       var modInfo = rawMod.Bulletin || rawMod.CORS;
       if (modInfo) {
