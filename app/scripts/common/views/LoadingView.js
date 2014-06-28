d5c6f08ac8323dbc4da2f144e22c1d0e38006922
Beng Hee Eu <beng@benghee.eu>
2014-06-28 17:05:06 +0800

    Remove unused loading view

--- app/scripts/common/views/LoadingView.js
+++
@@ -1,8 +0,0 @@
-define(['backbone.marionette', 'hbs!../templates/loading'],
-  function (Marionette, template) {
-    'use strict';
-
-    return Marionette.ItemView.extend({
-      template: template
-    });
-  });
