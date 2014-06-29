fb5b31daeff0fdff643273ae27618f85b61c3825
Beng Hee Eu <beng@benghee.eu>
2014-06-30 03:00:27 +0800

    Hijack internal links to use backbone navigate

--- app/scripts/common/views/AppView.js
+++ app/scripts/common/views/AppView.js
@@ -1,10 +1,40 @@
-define(['backbone', 'nusmods'], function(Backbone, NUSMods) {
+define(['backbone', 'nusmods'], function (Backbone, NUSMods) {
   'use strict';
 
   return Backbone.View.extend({
     el: 'body',
 
-    initialize: function() {
+    events: {
+      'click a[href]:not([data-bypass])': 'hijackLinks'
+    },
+
+    // Ref: https://github.com/backbone-boilerplate/backbone-boilerplate/blob/85723839dbab6787d69eedcbbea05e1d59960eff/app/app.js#L52
+    hijackLinks: function (event) {
+      // Do not hijack if modifier key was pressed when the event fired.
+      if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
+        return;
+      }
+
+      // Get the absolute anchor href.
+      var $link = $(event.currentTarget);
+      var href = { prop: $link.prop('href'), attr: $link.attr('href') };
+      // Get the absolute root.
+      var root = location.protocol + '//' + location.host + '/';
+
+      // Ensure the root is part of the anchor href, meaning it's relative.
+      if (href.prop.slice(0, root.length) === root) {
+        // Stop the default event to ensure the link will not cause a page
+        // refresh.
+        event.preventDefault();
+
+        // `Backbone.history.navigate` is sufficient for all Routers and will
+        // trigger the correct events. The Router's internal `navigate` method
+        // calls this anyways.  The fragment is sliced from the root.
+        Backbone.history.navigate(href.attr, {trigger: true});
+      }
+    },
+
+    initialize: function () {
       $.ajaxSetup({
         cache: true
       });
