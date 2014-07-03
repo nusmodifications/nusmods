9a815556c7d7aa6fb04a48fe642aa79ef0352f5c
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-07-03 13:16:05 +0800

    Use markdown parser for keyboard shortcuts

--- app/scripts/help/main.js
+++ app/scripts/help/main.js
@@ -1,5 +1,5 @@
-define(['require', 'app', 'backbone.marionette', 'backbone'],
-  function (require, App, Marionette, Backbone) {
+define(['require', 'app', 'backbone.marionette', 'backbone', 'marked', 'underscore'],
+  function (require, App, Marionette, Backbone, marked, _) {
     'use strict';
 
     var controller = {
@@ -7,8 +7,12 @@ define(['require', 'app', 'backbone.marionette', 'backbone'],
         require(['./views/HelpView'],
           function (HelpView) {
             $.getJSON('/scripts/help/keyboardShortcuts.json', function (data) {
+              _.each(data, function (category) {
+                _.each(category.shortcuts, function (shortcut) {
+                  shortcut.description = marked(shortcut.description);
+                })
+              })
               var helpModel = new Backbone.Model({keyboardShortcuts: data});
-              console.log(data)
               App.mainRegion.show(new HelpView({model: helpModel}));
             });
           });
