b6b3c9fc38f85b310bec6d73d90fda71ef5afed4
Beng Hee Eu <beng@benghee.eu>
2014-07-03 21:37:51 +0800

    Load keyboard shortcuts json using json requirejs plugin so that it can be compiled into build

--- app/scripts/help/main.js
+++ app/scripts/help/main.js
@@ -1,21 +1,20 @@
-define(['require', 'app', 'backbone.marionette', 'backbone', 'marked', 'underscore'],
-  function (require, App, Marionette, Backbone, marked, _) {
+define(['require', 'app', 'backbone.marionette', 'backbone', 'marked',
+    'underscore', 'json!./keyboardShortcuts.json'],
+  function (require, App, Marionette, Backbone, marked, _, keyboardShortcuts) {
     'use strict';
 
     var controller = {
       showHelp: function () {
         require(['./views/HelpView'],
           function (HelpView) {
-            $.getJSON('/scripts/help/keyboardShortcuts.json', function (data) {
-              _.each(data, function (category) {
-                _.each(category.shortcuts, function (shortcut) {
-                  shortcut.description = marked(shortcut.description);
-                })
+            _.each(keyboardShortcuts, function (category) {
+              _.each(category.shortcuts, function (shortcut) {
+                shortcut.description = marked(shortcut.description);
               });
-
-              var helpModel = new Backbone.Model({keyboardShortcuts: data});
-              App.mainRegion.show(new HelpView({model: helpModel}));
             });
+
+            var helpModel = new Backbone.Model({keyboardShortcuts: keyboardShortcuts});
+            App.mainRegion.show(new HelpView({model: helpModel}));
           });
       }
     };
