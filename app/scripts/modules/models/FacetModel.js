226d7f49b4d3cd6d07a4f27e4ce079917988f28b
Beng Hee Eu <beng@benghee.eu>
2014-07-05 12:28:57 +0800

    Sort filters by label

--- app/scripts/modules/models/FacetModel.js
+++ app/scripts/modules/models/FacetModel.js
@@ -1,20 +1,21 @@
 define(['underscore', 'backbone', '../collections/FilterCollection'],
-  function(_, Backbone, FilterCollection) {
-  'use strict';
+  function (_, Backbone, FilterCollection) {
+    'use strict';
 
-  return Backbone.Model.extend({
-    initialize: function() {
-      this.set(
-        'groupedCollection',
-        _.groupBy(this.get('filteredCollection'), this.get('key'))
-      );
-      var filters = _.map(this.get('groupedCollection'), function (mods, key) {
-        return {
-          count: mods.length,
-          label: key
-        };
-      });
-      this.set('filters', new FilterCollection(filters));
-    }
+    return Backbone.Model.extend({
+      initialize: function () {
+        this.set(
+          'groupedCollection',
+          _.groupBy(this.get('filteredCollection'), this.get('key'))
+        );
+        var filters = _.sortBy(_.map(this.get('groupedCollection'),
+          function (mods, key) {
+            return {
+              count: mods.length,
+              label: key
+            };
+          }), this.get('sortBy') || 'label');
+        this.set('filters', new FilterCollection(filters));
+      }
+    });
   });
-});
