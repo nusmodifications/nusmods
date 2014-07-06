319932c92be36fa20298449d18ceb4baab672214
Beng Hee Eu <beng@benghee.eu>
2014-07-07 02:51:58 +0800

    Cleanup loading of tasks, all custom tasks have been moved to api repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -438,8 +438,6 @@ module.exports = function (grunt) {
         }
     });
 
-//    grunt.loadTasks('tasks');
-
     grunt.registerTask('serve', function (target) {
         if (target === 'dist') {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
