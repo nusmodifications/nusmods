45627bc37ad5a0b88f3798b4dd1ade1b74f0b669
Beng Hee Eu <beng@benghee.eu>
2014-07-16 16:18:35 +0800

    Add Node support to nusmods.js

--- app/scripts/nusmods.js
+++ app/scripts/nusmods.js
@@ -5,8 +5,13 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
   } else {
-    // Browser globals
+    // Browser globals (root is window)
     root.NUSMods = factory();
   }
 }(this, function () {
