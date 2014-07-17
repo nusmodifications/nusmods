d0562055da631f688155885d04a610ff09e26855
Beng Hee Eu <beng@benghee.eu>
2014-07-17 17:39:48 +0800

    Remove deprecated grunt-bower-install and its config

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,14 +199,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // Automatically inject Bower components into the HTML file
-        'bower-install': {
-            app: {
-                html: '<%= yeoman.app %>/index.html',
-                ignorePath: '<%= yeoman.app %>/'
-            }
-        },
-
         browserify: {
             all: {
                 files: {
