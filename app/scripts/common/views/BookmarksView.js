a75b62e62bb7d1f79627fc96aeb8d22a6afb9ae0
yangshun <tay.yang.shun@gmail.com>
2014-07-24 01:41:17 +0800

    Remove redundant templateEmpty

--- app/scripts/common/views/BookmarksView.js
+++ app/scripts/common/views/BookmarksView.js
@@ -5,7 +5,6 @@ var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 var BookmarkItemView = require('./BookmarkItemView');
 var template = require('../templates/bookmarks.hbs');
-var templateEmpty = require('../templates/bookmarks_empty.hbs');
 
 var EmptyView = Marionette.ItemView.extend({
   template: _.template('<p>No bookmarks yet!</p>')
