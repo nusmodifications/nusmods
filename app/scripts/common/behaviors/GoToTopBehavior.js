ec8adc6df0577109aa8e82084e11db478cb832dd
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-07-27 10:27:10 +0800

    Refactor back to top button behaviour

--- app/scripts/common/behaviors/GoToTopBehavior.js
+++ app/scripts/common/behaviors/GoToTopBehavior.js
@@ -4,26 +4,23 @@ var Marionette = require('backbone.marionette');
 var _ = require('underscore');
 
 module.exports = Marionette.Behavior.extend({
-	defaults: {
+  defaults: {
     triggerThreshold: 100
-	},
+  },
 
   events: {
     'click @ui.backToTopButton': 'scrollToTop'
   },
 
-  onShow: function() {
+  onShow: function () {
     var that = this;
-    $(window).scroll(_.debounce(function() {
-      if ($(this).scrollTop() > that.options.triggerThreshold) {
-        $(that.view.ui.backToTopButton).addClass('back-to-top-visible');
-      } else {
-        $(that.view.ui.backToTopButton).removeClass('back-to-top-visible');
-      }
+    $(window).scroll(_.debounce(function () {
+      $(that.view.ui.backToTopButton).toggleClass('visible', 
+        $(this).scrollTop() > that.options.triggerThreshold);
     }, 50));
   },
 
-  scrollToTop: function() {
+  scrollToTop: function () {
     $('body').stop().animate({scrollTop: 0}, 400);
     $(this.view.ui.backToTopButton).blur();
   }
