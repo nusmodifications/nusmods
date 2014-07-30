15ac9f730ac2ee251fedfe17ee415cae26428dda
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-07-31 03:37:54 +0800

    Analyse themes and modes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,6 +8,14 @@ var $body = $('body');
     if (!value) {
       value = 'default';
       localforage.setItem(property, value);
+    } else {
+      // For non-first time users, we want to see what modes/themes they are using.
+      if (property === 'theme') {
+        ga('send', 'event', 'Theme', 'Load page with theme', value);
+      }
+      if (property === 'mode') {
+        ga('send', 'event', 'Mode', 'Load page with mode', value);
+      }
     }
     $body.addClass(property + '-' + value);
     $body.attr('data-' + property, value);
