80400bf4dac6bc385c344ed45907e48c3256a7d2
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-07-31 18:10:01 +0800

    Message shown for empty module listing

--- app/scripts/modules/views/ModulesListingView.js
+++ app/scripts/modules/views/ModulesListingView.js
@@ -3,11 +3,18 @@
 var Marionette = require('backbone.marionette');
 var ModuleItemView = require('./ModuleItemView');
 var template = require('../templates/modules_listing.hbs');
+var _ = require('underscore');
+
+var EmptyView = Marionette.ItemView.extend({
+  tagName: 'tr',
+  template: _.template('<td colspan="5" class="empty-module-listing">No modules meet the criteria.</td>')
+});
 
 module.exports = Marionette.CompositeView.extend({
   tagName: 'table',
   className: 'table table-bordered table-striped',
   childView: ModuleItemView,
   childViewContainer: 'tbody',
+  emptyView: EmptyView,
   template: template
 });
