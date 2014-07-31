cb1912567f1793b92eead12a9619ca450a960b1c
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-07-31 18:03:27 +0800

    Message shown for empty exams timetable

--- app/scripts/timetable/views/ExamsView.js
+++ app/scripts/timetable/views/ExamsView.js
@@ -3,14 +3,20 @@
 var ExamView = require('./ExamView');
 var Marionette = require('backbone.marionette');
 var template = require('../templates/exams.hbs');
+var _ = require('underscore');
+
+var EmptyView = Marionette.ItemView.extend({
+  tagName: 'tr',
+  template: _.template('<td colspan="4" class="empty-timetable">No modules added.</td>')
+});
 
 module.exports = Marionette.CompositeView.extend({
   tagName: 'table',
   className: 'table table-bordered table-condensed',
   childView: ExamView,
   childViewContainer: 'tbody',
+  emptyView: EmptyView,
   template: template,
-
   collectionEvents: {
     'add remove': function() {
       $('#clash').toggleClass('hidden', !this.collection.clashCount);
