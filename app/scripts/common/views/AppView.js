3062dadbe9d004f53af09441322f45c7f61e2c27
Beng Hee Eu <beng@benghee.eu>
2014-08-14 17:01:25 +0800

    Comment out corsify and activateCORS to avoid jshint unused variable warnings

--- app/scripts/common/views/AppView.js
+++ app/scripts/common/views/AppView.js
@@ -10,7 +10,7 @@ var BookmarksView = require('./BookmarksView');
 var _ = require('underscore');
 var analytics = require('../../analytics');
 var attachFastClick = require('fastclick');
-var corsify = require('../../cors/corsify');
+// var corsify = require('../../cors/corsify');
 var themePicker = require('../themes/themePicker');
 require('bootstrap/alert');
 require('qTip2');
@@ -67,10 +67,10 @@ module.exports = Backbone.View.extend({
       $('#correct-as-at').text((new Date(lastModified)).toString().slice(0, 21));
     });
 
-    function activateCORS() {
-      $('.cors-round-text').html(corsify.determineRound(Date.now()));
-      $('.cors-round-container').addClass('animated bounceInUp shown').alert();
-    }
+    // function activateCORS() {
+    //   $('.cors-round-text').html(corsify.determineRound(Date.now()));
+    //   $('.cors-round-container').addClass('animated bounceInUp shown').alert();
+    // }
 
     // activateCORS();
 
