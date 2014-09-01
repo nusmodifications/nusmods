a3e416d2a60cb87e0b88d0dcf313e5b54d2f8c5c
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-01 09:53:41 +0800

    Added url redirect tracing script

---
+++ app/redirect.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Max-Age: 1000');
+header('Content-Type: application/json');
+
+function getRedirectUrl ($url) {
+  stream_context_set_default(array(
+    'http' => array(
+      'method' => 'HEAD'
+    )
+  ));
+  $headers = get_headers($url, 1);
+  if ($headers !== false && isset($headers['Location'])) {
+    return $headers['Location'];
+  }
+  return false;
+}
+
+$timetableUrl = $_GET['timetable'];
+$redirectedUrl = getRedirectUrl($timetableUrl);
+
+if (!$redirectedUrl) {
+  $redirectedUrl = $timetableUrl;
+}
+
+echo json_encode(array(
+  'redirectedUrl' => $redirectedUrl
+));
+
+?>
