4043a5730f8722cc692441c4b94583388c3dd4c9
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-10-22 01:38:55 +0800

    Sidebar navigation done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@ var TimetableModuleCollection = require('./common/collections/TimetableModuleCol
 var _ = require('underscore');
 var config = require('./common/config');
 var localforage = require('localforage');
+var $ = require('jquery');
 require('qTip2');
 
 // Set Backbone.History.initialRoute to allow route handlers to find out if they
