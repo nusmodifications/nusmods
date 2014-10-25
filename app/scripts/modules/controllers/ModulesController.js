217ae7e15812e2c40c07246a5289a4bf0dd66373
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-10-25 22:08:05 +0800

    Remove redundant section stuff from module page

--- app/scripts/modules/controllers/ModulesController.js
+++ app/scripts/modules/controllers/ModulesController.js
@@ -10,39 +10,8 @@ var navigationItem = App.request('addNavigationItem', {
   url: '/modules'
 });
 
-var sectionsInfo = [
-  {
-    'sectionType': 'schedule',
-    'tabTitle': 'Schedule',
-    'sectionTitle': 'Class Schedule',
-    'icon': 'clock-o',
-    'active': false
-  },
-  {
-    'sectionType': 'corspedia',
-    'tabTitle': 'Corspedia',
-    'sectionTitle': 'CORS Bidding History',
-    'icon': 'bar-chart-o',
-    'active': false
-  },
-  {
-    'sectionType': 'modmaven',
-    'tabTitle': 'ModMaven',
-    'sectionTitle': 'Prerequisites Tree',
-    'icon': 'code-fork',
-    'active': false
-  },
-  {
-    'sectionType': 'reviews',
-    'tabTitle': 'Module Reviews',
-    'sectionTitle': 'Reviews & Discussions',
-    'icon': 'thumbs-o-up',
-    'active': false
-  }
-];
-
 module.exports = Marionette.Controller.extend({
-  showModules: function (id, section) {
+  showModules: function (id) {
     var ModulesView = require('../views/ModulesView');
     var ModuleView = require('../views/ModuleView');
     var NUSMods = require('../../nusmods');
@@ -56,17 +25,11 @@ module.exports = Marionette.Controller.extend({
       });
     } else {
       var modCode = id.toUpperCase();
-      var sectionTypes = _.pluck(sectionsInfo, 'sectionType');
-      if (!section || sectionTypes.indexOf(section) === -1) {
-        section = 'schedule';
-      }
       NUSMods.getMod(modCode).then(function (data) {
         var moduleModel = new ModuleModel(data);
         var modulePageModel = new ModulePageModel({
           faculties: facultyList,
-          module: moduleModel.attributes,
-          section: section,
-          sectionsInfo: sectionsInfo
+          module: moduleModel.attributes
         });
         App.mainRegion.show(new ModuleView({model: modulePageModel}));
       });
