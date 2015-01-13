dc5bff84671625d8da112ce4b31f39e7564d197c
xbili <xubili93@gmail.com>
2015-01-13 11:49:11 +0800

    Added qtips for hyperlinks on Module pages

--- app/scripts/modules/views/ModuleView.js
+++ app/scripts/modules/views/ModuleView.js
@@ -187,6 +187,42 @@ module.exports = Marionette.LayoutView.extend({
 
     // Index 0 is "All", therefore index no. = sem no.
     $('.js-nm-ls-schedule-tabs a[data-target="#nm-ls-schedule-sem' + config.semester + '"]').tab('show');
+
+    // Module code hover brief info
+    $('.js-hover-info').qtip({
+      content: {
+        text: function(event, api) {
+          var NUSMods = require('../../nusmods');
+          var ModuleModel = require('../../common/models/ModuleModel');
+          
+          var reqModuleCode = $(this).text();
+          var reqModule = NUSMods.getMod(reqModuleCode).then(function(data) {
+            var reqModuleModel = new ModuleModel(data);
+
+            var title = reqModuleModel.get('ModuleTitle') + '\n'
+            api.set('content.title', '<strong>' + title + '</strong>');
+
+            var semesters = reqModuleModel.get('semesterNames');
+            var offeredIn = _.reduce(semesters, function(a, b) {
+              return a + ', ' + b;
+            });
+            api.set('content.text', 'Offered in: ' + offeredIn);
+          });
+
+          return 'Loading...';
+        }
+      },
+      position: {
+        effect: 'false',
+        my: 'bottom center',
+        at: 'top center'
+      },
+      show: {
+        effect: function() {
+          $(this).fadeTo(200, 0.85);
+        }
+      }
+    });
   },
   showAllStats: function () {
     analytics.track('Module cors', 'View full stats', this.model.get('module').ModuleCode);
@@ -238,5 +274,8 @@ module.exports = Marionette.LayoutView.extend({
     setTimeout(function () {
       window.location.hash = target;
     });
+  },
+  onBeforeDestroy: function() {
+    $('.js-hover-info').qtip('hide');
   }
 });
