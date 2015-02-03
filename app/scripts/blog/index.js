86735082c7f4b925671390f07c33ea09b19ad87e
Guo Xiang Tan <tgx_world@hotmail.com>
2015-02-03 21:54:31 +0800

    Add target _blank for link to blog.

--- app/scripts/blog/index.js
+++ app/scripts/blog/index.js
@@ -5,5 +5,6 @@ var App = require('../app');
 App.request('addNavigationItem', {
   name: 'Blog',
   icon: 'newspaper-o',
-  url: 'http://blog.nusmods.com'
+  url: 'http://blog.nusmods.com',
+  target: "_blank"
 });
