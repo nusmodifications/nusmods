2918d43b479a961436f254ad573087d3a9e4f96e
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-06-27 03:12:50 +0800

    Update FB like boxes

--- app/scripts/timetable/views/TimetableView.js
+++ app/scripts/timetable/views/TimetableView.js
@@ -80,7 +80,7 @@ module.exports = Marionette.LayoutView.extend({
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) { return; }
         js = d.createElement(s); js.id = id;
-        js.src = '//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.3&appId=1524196174461544';
+        js.src = '//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.6&appId=1524196174461544';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
     }
