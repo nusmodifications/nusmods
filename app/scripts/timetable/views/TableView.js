b74d2688ecdd8c5e6e1bd632d5eb8aa22cfa3965
Sam-Mauris Yong <mauris@users.noreply.github.com>
2016-08-16 23:01:19 +0800

    Current Day of Week + Time of Day indicator (#249)
    * indicates the current day of the week and time of the day on timetable
    
    * fixed this reference in timer function
    
    * updated indicator to half-hourly accurate
    
    * updated code to use padTwo
    
    * changed setTimeout to setInterval
    
    :coffee: needed
    
    * updated tableview with minor fixes
    
    * switching to moment!
    
    * reverted code to be hourly block indicator instead
    
    * 5 minutes interval for updating
    
    * keeping track of timer and clear it onBeforeDestroy
    
    * changed currentDayTime class to kebab skewer style
    
    * removed redundant .call() for updateDayTimeIndicator

--- app/scripts/timetable/views/TableView.js
+++ app/scripts/timetable/views/TableView.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var $ = require('jquery');
+var padTwo = require('../../common/utils/padTwo');
+var moment = require('moment');
 var LessonView = require('./LessonView');
 var Marionette = require('backbone.marionette');
 var _ = require('underscore');
@@ -17,6 +19,7 @@ module.exports = Marionette.CompositeView.extend({
     };
   },
   template: template,
+  timerUpdateDayTime: null,
 
   events: {
     'mousemove': 'mouseMove',
@@ -25,6 +28,30 @@ module.exports = Marionette.CompositeView.extend({
 
   ui: {
     colgroups: 'colgroup'
+  },
+
+  onRender: function() {
+    var self = this;
+
+    // updates every minute thereafter
+    this.timerUpdateDayTime = window.setInterval(function() {
+      self.updateDayTimeIndicator();
+    }, 300000); // 5 min * 60 s * 1000 ms
+
+    // initial updating
+    this.updateDayTimeIndicator();
+  },
+
+  onBeforeDestroy: function() {
+    this.timerUpdateDayTime = window.clearInterval(this.timerUpdateDayTime);
+  },
+
+  updateDayTimeIndicator: function() {
+    var day = moment().format('ddd').toLowerCase();
+    var hour = padTwo(moment().hour());
+
+    this.$('#timetable .current-day-time').removeClass('current-day-time');
+    this.$('#' + day + ' .h' + hour).addClass('current-day-time');
   },
 
   mouseMove: function(evt) {
