d91de14ba0cf4e5bc39fd20ad7d4b273e0f8cfcf
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-08-17 20:41:59 +0800

    Uglify main js too

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task('usemin', function() {
       html: [function() { return plugins.htmlmin({ collapseWhitespace: true }); }],
       js: ['concat', plugins.uglify, plugins.rev],
       // don't uglify because jsmin is produced by browserify, which uglifies it
-      jsmain: ['concat', plugins.rev]
+      jsmain: ['concat', plugins.uglify, plugins.rev]
     }))
     .on('error', console.error.bind(console)) // eslint-disable-line no-console
     .pipe(gulp.dest('dist/'));
