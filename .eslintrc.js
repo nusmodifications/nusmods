fb7ca04ba97a6e9d3ad973802f4be23e6a01b15a
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-05 01:18:45 +0800

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,13 @@ module.exports = {
     "import"
   ],
   "rules": {
-    // The "always" version causes unpredictable errors.
-    "arrow-body-style": ["error", "as-needed"],
+    // Turning it on causes undecipherable errors.
+    "arrow-body-style": "off",
     // We use webpack resolve root and this conflicts with that.
     "import/no-extraneous-dependencies": "off",
     // We use webpack resolve root and this conflicts with that.
     "import/no-unresolved": "off",
+    "react/jsx-first-prop-new-line": ["error", "never"],
     // It just looks nicer without the space.
     "react/jsx-space-before-closing": "off",
     // TODO: Remove the following rule when eslint-config-airbnb updates to use "import/extensions rule from eslint-plugin-import"
