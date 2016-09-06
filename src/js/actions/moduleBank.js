f9da5f2ffd2f9938aa871ff073c4cc6b1fb7ae24
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-07 01:40:33 +0800

    Don't refetch cached modules

--- src/js/actions/moduleBank.js
+++ src/js/actions/moduleBank.js
@@ -16,13 +16,20 @@ export function getModuleList() {
 
 export const GET_MODULE = 'GET_MODULE';
 export function getModule(moduleCode) {
-  return (dispatch) => dispatch({
-    [API_REQUEST]: {
-      type: GET_MODULE,
-      payload: {
-        method: 'GET',
-        url: NUSModsApi.moduleDetailsUrl(moduleCode),
+  return (dispatch, getState) => {
+    // Module has been fetched before and cached. Don't have to fetch again.
+    if (getState().entities.moduleBank.modules[moduleCode]) {
+      return null;
+    }
+
+    return dispatch({
+      [API_REQUEST]: {
+        type: GET_MODULE,
+        payload: {
+          method: 'GET',
+          url: NUSModsApi.moduleDetailsUrl(moduleCode),
+        },
       },
-    },
-  });
+    });
+  };
 }
