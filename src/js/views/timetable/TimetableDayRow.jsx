f1e7e082159a9695a74e870965b344623d2a656c
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-07 23:40:12 +0800

    Clean up timetable row

--- src/js/views/timetable/TimetableDayRow.jsx
+++ src/js/views/timetable/TimetableDayRow.jsx
@@ -8,68 +8,10 @@ const TimetableDayRow = (props) => {
       {props.dayLessonRows ?
         props.dayLessonRows.map((dayLessonRow, i) => {
           return (
-            <TimetableRow day={props.day} key={i} lessons={dayLessonRow}/>
+            <TimetableRow day={i === 0 ? props.day : ''} key={i} lessons={dayLessonRow}/>
           );
         }) : <TimetableRow day={props.day}/>
       }
-      {/* <div className="timetable-day-row">
-        <div className="timetable-day-cell timetable-hour-cell"><span>Mon</span></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell" style={{ flexGrow: 4 }}>
-          <div className="test-cell"/>
-        </div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell" style={{ flexGrow: 4 }}>
-          <div className="test-cell"/>
-        </div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-      </div>
-      <div className="timetable-day-row">
-        <div className="timetable-day-cell timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell" style={{ flexGrow: 4 }}>
-          <div className="test-cell"/>
-        </div>
-        <div className="timetable-hour-cell" style={{ flexGrow: 4 }}>
-          <div className="test-cell"/>
-        </div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-        <div className="timetable-hour-cell"><span/></div>
-      </div> */}
     </div>
   );
 };
