a710de7025f33448d31af31e2515e76b3fe1be32
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-12 17:47:30 +0800

    Change title upon page navigation

--- src/js/views/modules/ModulePageContainer.jsx
+++ src/js/views/modules/ModulePageContainer.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import DocumentTitle from 'react-document-title';
+import config from 'config';
 
 import { loadModule } from 'actions/moduleBank';
 
@@ -20,45 +22,49 @@ export class ModulePageContainer extends Component {
 
   render() {
     const module = this.props.module;
+    const documentTitle = module ?
+      `${module.ModuleCode} ${module.ModuleTitle} - ${config.brandName}` : 'Not found';
     return (
-      <div>
-        {this.props.fetchModuleRequest.isPending && !module ?
-          <p>Loading...</p> : null
-        }
-        {this.props.fetchModuleRequest.isFailure ? <p>Module not found</p> : null}
-        {this.props.fetchModuleRequest.isSuccessful || module ?
-          <div>
-            <h1 className="display-4">{module.ModuleCode} {module.ModuleTitle}</h1>
-            <hr/>
-            <dl className="row">
-              {module.ModuleDescription ? <dt className="col-sm-3">Description</dt> : null}
-              {module.ModuleDescription ?
-                <dd className="col-sm-9">{module.ModuleDescription}</dd> : null}
+      <DocumentTitle title={documentTitle}>
+        <div className="module-container">
+          {this.props.fetchModuleRequest.isPending && !module ?
+            <p>Loading...</p> : null
+          }
+          {this.props.fetchModuleRequest.isFailure ? <p>Module not found</p> : null}
+          {this.props.fetchModuleRequest.isSuccessful || module ?
+            <div>
+              <h1 className="display-4">{module.ModuleCode} {module.ModuleTitle}</h1>
+              <hr/>
+              <dl className="row">
+                {module.ModuleDescription ? <dt className="col-sm-3">Description</dt> : null}
+                {module.ModuleDescription ?
+                  <dd className="col-sm-9">{module.ModuleDescription}</dd> : null}
 
-              {module.ModuleCredit ? <dt className="col-sm-3">Module Credits (MCs)</dt> : null}
-              {module.ModuleCredit ? <dd className="col-sm-9">{module.ModuleCredit}</dd> : null}
+                {module.ModuleCredit ? <dt className="col-sm-3">Module Credits (MCs)</dt> : null}
+                {module.ModuleCredit ? <dd className="col-sm-9">{module.ModuleCredit}</dd> : null}
 
-              {module.Prerequisite ? <dt className="col-sm-3">Prerequisite(s)</dt> : null}
-              {module.Prerequisite ? <dd className="col-sm-9">{module.Prerequisite}</dd> : null}
+                {module.Prerequisite ? <dt className="col-sm-3">Prerequisite(s)</dt> : null}
+                {module.Prerequisite ? <dd className="col-sm-9">{module.Prerequisite}</dd> : null}
 
-              {module.Corequisite ? <dt className="col-sm-3">Corequisite(s)</dt> : null}
-              {module.Corequisite ? <dd className="col-sm-9">{module.Corequisite}</dd> : null}
+                {module.Corequisite ? <dt className="col-sm-3">Corequisite(s)</dt> : null}
+                {module.Corequisite ? <dd className="col-sm-9">{module.Corequisite}</dd> : null}
 
-              {module.Preclusion ? <dt className="col-sm-3">Preclusion(s)</dt> : null}
-              {module.Preclusion ? <dd className="col-sm-9">{module.Preclusion}</dd> : null}
+                {module.Preclusion ? <dt className="col-sm-3">Preclusion(s)</dt> : null}
+                {module.Preclusion ? <dd className="col-sm-9">{module.Preclusion}</dd> : null}
 
-              {module.Department ? <dt className="col-sm-3">Department</dt> : null}
-              {module.Department ? <dd className="col-sm-9">{module.Department}</dd> : null}
+                {module.Department ? <dt className="col-sm-3">Department</dt> : null}
+                {module.Department ? <dd className="col-sm-9">{module.Department}</dd> : null}
 
-              {module.Workload ? <dt className="col-sm-3">Weekly Workload</dt> : null}
-              {module.Workload ? <dd className="col-sm-9">{module.Workload}</dd> : null}
+                {module.Workload ? <dt className="col-sm-3">Weekly Workload</dt> : null}
+                {module.Workload ? <dd className="col-sm-9">{module.Workload}</dd> : null}
 
-              {/* TODO: Add in exam date for each semester. */}
+                {/* TODO: Add in exam date for each semester. */}
 
-            </dl>
-          </div> : null
-        }
-      </div>
+              </dl>
+            </div> : null
+          }
+        </div>
+      </DocumentTitle>
     );
   }
 }
