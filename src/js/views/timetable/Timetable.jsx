553185b71967bf747dd1032e1acf5ed5c0f53ca7
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-13 00:37:38 +0800

    Drag and drop (#13)
    * Add drag-and-drop
    
    * Fix dragging issue
    
    * Add todo

--- src/js/views/timetable/Timetable.jsx
+++ src/js/views/timetable/Timetable.jsx
@@ -1,37 +1,41 @@
-import React, { PropTypes } from 'react';
-// import { connect } from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+/* eslint-disable new-cap */
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 
 import TimetableBackground from './TimetableBackground';
 import TimetableDayRow from './TimetableDayRow';
 
 const SCHOOLDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-const Timetable = (props) => {
-  return (
-    <div className="timetable-container">
-      <div className="timetable">
-        {SCHOOLDAYS.map((day) => {
-          const dayDisplayText = day.substring(0, 3);
-          if (day === 'Saturday' && !props.lessons.Saturday) {
-            return null;
-          }
-          return (
-            <TimetableDayRow key={dayDisplayText}
-              onModifyCell={props.onModifyCell}
-              day={dayDisplayText}
-              dayLessonRows={props.lessons[day]}
-            />
-          );
-        })}
+class Timetable extends Component {
+  render() {
+    return (
+      <div className="timetable-container">
+        <div className="timetable">
+          {SCHOOLDAYS.map((day) => {
+            const dayDisplayText = day.substring(0, 3);
+            if (day === 'Saturday' && !this.props.lessons.Saturday) {
+              return null;
+            }
+            return (
+              <TimetableDayRow key={dayDisplayText}
+                onModifyCell={this.props.onModifyCell}
+                day={dayDisplayText}
+                dayLessonRows={this.props.lessons[day]}
+              />
+            );
+          })}
+        </div>
+        <TimetableBackground/>
       </div>
-      <TimetableBackground/>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Timetable.propTypes = {
   lessons: PropTypes.object,
   onModifyCell: PropTypes.func,
 };
 
-export default Timetable;
+export default DragDropContext(HTML5Backend)(Timetable);
--- src/js/views/timetable/TimetableCell.jsx
+++ src/js/views/timetable/TimetableCell.jsx
@@ -1,35 +1,81 @@
 import React, { PropTypes } from 'react';
 import classnames from 'classnames';
+/* eslint-disable new-cap */
+import { DragSource, DropTarget } from 'react-dnd';
 
 import { LESSON_TYPE_ABBREV } from 'utils/timetable';
 import prefixer from 'utils/prefixer';
 
+const CELL = 'CELL';
+const lessonSource = {
+  beginDrag(props) {
+    setTimeout(() => {
+      // Begin dragging before showing alternative cells.
+      props.onModifyCell(props.lesson);
+    }, 0);
+    return {};
+  },
+};
+
+const lessonTarget = {
+  drop(props) {
+    props.onModifyCell(props.lesson);
+  },
+};
+
+function dragCollect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
+function dropCollect(connect, monitor) {
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+  };
+}
+
 const TimetableCell = (props) => {
   const lesson = props.lesson;
   const widthStyle = props.width ? prefixer({ flexGrow: props.width }) : null;
+  let cell = null;
+
+  if (lesson) {
+    const moduleCell = (
+      <div className={classnames('timetable-module-cell', {
+        'is-modifiable': lesson.isModifiable,
+        'is-available': lesson.isAvailable,
+        'is-active': lesson.isActive,
+        [`color-${lesson.colorIndex}`]: true,
+      })}
+        onClick={(event) => {
+          event.stopPropagation();
+          props.onModifyCell(lesson);
+        }}
+      >
+        <div className="cell-module-code">{lesson.ModuleCode}</div>
+        <div>
+          <span className="cell-module-lesson-type">{LESSON_TYPE_ABBREV[lesson.LessonType]}</span>
+          <span className="cell-module-class">{' '}[{lesson.ClassNo}]</span>
+        </div>
+        <div><span className="cell-module-venue">{lesson.Venue}</span></div>
+      </div>
+    );
+
+    if (lesson.isModifiable || lesson.isAvailable) {
+      // Lesson can be modified and dragged around.
+      // TODO: Separate out dnd targets and sources depending on modifiable/available.
+      cell = props.connectDropTarget(props.connectDragSource(moduleCell));
+    } else {
+      // Non-modifiable lesson.
+      cell = moduleCell;
+    }
+  }
+
   return (
-    <div className="timetable-cell" style={widthStyle}>
-      {lesson ?
-        <div className={classnames('timetable-module-cell', {
-          'is-modifiable': lesson.isModifiable,
-          'is-available': lesson.isAvailable,
-          'is-active': lesson.isActive,
-          [`color-${lesson.colorIndex}`]: true,
-        })}
-          onClick={(event) => {
-            event.stopPropagation();
-            props.onModifyCell(lesson);
-          }}
-        >
-          <div className="cell-module-code">{lesson.ModuleCode}</div>
-          <div>
-            <span className="cell-module-lesson-type">{LESSON_TYPE_ABBREV[lesson.LessonType]}</span>
-            <span className="cell-module-class">{' '}[{lesson.ClassNo}]</span>
-          </div>
-          <div><span className="cell-module-venue">{lesson.Venue}</span></div>
-        </div> : null
-      }
-    </div>
+    <div className="timetable-cell" style={widthStyle}>{cell}</div>
   );
 };
 
@@ -37,6 +83,11 @@ TimetableCell.propTypes = {
   lesson: PropTypes.object,
   width: PropTypes.number,
   onModifyCell: PropTypes.func,
+  connectDragSource: PropTypes.func.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired,
 };
 
-export default TimetableCell;
+export default DragSource(CELL, lessonSource, dragCollect)(
+  DropTarget(CELL, lessonTarget, dropCollect)(TimetableCell)
+);
