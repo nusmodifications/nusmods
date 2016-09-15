0287b8b9a5cde2a7624273358a0817a16e1d429a
li-kai <falconets25601@hotmail.com>
2016-09-16 00:45:53 +0800

    Init flow

--- v3/src/js/utils/timify.js
+++ v3/src/js/utils/timify.js
@@ -1,14 +1,16 @@
+/* @flow */
+
 // Converts a 24-hour format time string to an index.
 // Each index corresponds to one cell of each timetable row.
 // Each row may not start from index 0, it depends on the config's starting time.
 // 0000 -> 0, 0030 -> 1, 0100 -> 2, ...
-export function convertTimeToIndex(time) {
+export function convertTimeToIndex(time:string):number {
   return (parseInt(time.substring(0, 2), 10) * 2) + (time.substring(2) === '00' ? 0 : 1);
 }
 
 // Reverse of convertTimeToIndex.
 // 0 -> 0000, 1 -> 0030, 2 -> 0100, ...
-export function convertIndexToTime(index) {
+export function convertIndexToTime(index:number):string {
   const hour = parseInt(index / 2, 10);
   const minute = (index % 2) === 0 ? '00' : '30';
   return (hour < 10 ? `0${hour.toString()}` : hour.toString()) + minute;
