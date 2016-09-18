059502338fa2e4c602522f4e276b98675d87f62e
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-18 17:17:48 +0800

    Refactor tests

--- v3/test/utils/modules.js
+++ v3/test/utils/modules.js
@@ -5,22 +5,23 @@ import {
   areLessonsSameClass,
   getModuleSemesterData,
 } from 'utils/modules';
-import mockLesson from './mockLesson.json';
-import mockModule from './mockModule.json';
+
+import cs1010s from '../mocks/modules/CS1010S.json';
+import cs3216 from '../mocks/modules/CS3216.json';
+
+const mockLesson = _.cloneDeep(cs1010s.History[0].Timetable[0]);
+mockLesson.ModuleCode = 'CS1010S';
+mockLesson.ModuleTitle = 'Programming Methodology';
 
 test('modulePagePath should generate route correctly', (t) => {
-  const actual = modulePagePath('MOD101');
-  const expected = '/modules/MOD101';
+  const actual = modulePagePath('CS1010S');
+  const expected = '/modules/CS1010S';
   t.is(actual, expected);
 });
 
-test('areLessonsSameClass should identify same class', (t) => {
-  const deepCloneMockLesson = _.clone(mockLesson, true);
-  t.true(areLessonsSameClass(mockLesson, deepCloneMockLesson));
-});
-
-test('getModuleSemesterData should return array of lessons', (t) => {
-  const actual = getModuleSemesterData(mockModule, 1);
+test('getModuleSemesterData should return semester data if semester is present', (t) => {
+  const sem = 1;
+  const actual = getModuleSemesterData(cs3216, sem);
   const expected = {
     Semester: 1,
     Timetable: [
@@ -39,27 +40,41 @@ test('getModuleSemesterData should return array of lessons', (t) => {
   t.deepEqual(actual, expected);
 });
 
-test('getModuleSemesterData should return empty array', (t) => {
-  // not sure if actual should be undefined
-  const actual = getModuleSemesterData(mockModule, 2);
-  const expected = null;
+test('getModuleSemesterData should return undefined if semester is absent', (t) => {
+  const sem = 2;
+  const actual = getModuleSemesterData(cs3216, sem);
+  const expected = undefined;
   t.is(actual, expected);
 });
 
-function testDifferentProperties(t, property) {
-  const anotherMockLesson = _.clone(mockLesson, true);
-  anotherMockLesson[property] = 'TEST';
-  t.false(areLessonsSameClass(mockLesson, anotherMockLesson));
+function lessonWithDifferentProperty(lesson, property, newValue) {
+  const anotherLesson = _.cloneDeep(lesson);
+  anotherLesson[property] = newValue || 'TEST';
+  return anotherLesson;
 }
 
-test('areLessonsSameClass should identify different ModuleCode', (t) => {
-  testDifferentProperties(t, 'ModuleCode');
+test('areLessonsSameClass should identify identity lessons as same class', (t) => {
+  const deepClonedLesson = _.cloneDeep(mockLesson);
+  t.true(areLessonsSameClass(mockLesson, deepClonedLesson));
 });
 
-test('areLessonsSameClass should identify different ClassNo', (t) => {
-  testDifferentProperties(t, 'ClassNo');
+test('areLessonsSameClass should identify lessons from the same ClassNo but with different timings as same class', (t) => {
+  const otherLesson = lessonWithDifferentProperty(mockLesson, 'StartTime', '0000');
+  const otherLesson2 = lessonWithDifferentProperty(otherLesson, 'EndTime', '2300');
+  t.true(areLessonsSameClass(mockLesson, otherLesson2));
 });
 
-test('areLessonsSameClass should identify different LessonType', (t) => {
-  testDifferentProperties(t, 'LessonType');
+test('areLessonsSameClass should identify lessons with different ModuleCode as different class', (t) => {
+  const otherLesson = lessonWithDifferentProperty(mockLesson, 'ModuleCode');
+  t.false(areLessonsSameClass(mockLesson, otherLesson));
+});
+
+test('areLessonsSameClass should identify lessons with different ClassNo as different class', (t) => {
+  const otherLesson = lessonWithDifferentProperty(mockLesson, 'ClassNo');
+  t.false(areLessonsSameClass(mockLesson, otherLesson));
+});
+
+test('areLessonsSameClass should identify lessons with different LessonType as different class', (t) => {
+  const otherLesson = lessonWithDifferentProperty(mockLesson, 'LessonType');
+  t.false(areLessonsSameClass(mockLesson, otherLesson));
 });
