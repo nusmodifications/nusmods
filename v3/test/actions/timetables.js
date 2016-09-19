b4c80bfa75626ec40cb05cb7f3e76aa1f9423811
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-09-19 23:31:19 +0800

    Fix eslint for tests

--- v3/test/actions/timetables.js
+++ v3/test/actions/timetables.js
@@ -1,22 +1,22 @@
 // @flow
-import type { FSA } from 'types/redux';
-import type { ModuleCode, Semester } from 'types/modules';
+// import type { FSA } from 'types/redux';
+import type { ModuleCode } from 'types/modules';
 
 import test from 'ava';
-import config from 'config';
+// import config from 'config';
 import * as actions from 'actions/timetables';
 
 test('addModule should create an action to add a module', (t) => {
-  const semester: Semester = config.semester;
+  // const semester: Semester = config.semester;
   const moduleCode: ModuleCode = 'CS1010';
 
-  const expected: FSA = {
-    type: actions.ADD_MODULE,
-    payload: {
-      semester,
-      moduleCode,
-    },
-  };
+  // const expected: FSA = {
+  //   type: actions.ADD_MODULE,
+  //   payload: {
+  //     semester,
+  //     moduleCode,
+  //   },
+  // };
   const value: Function = actions.addModule(moduleCode);
   // not sure how to test dispatch
   t.true(typeof value === 'function');
