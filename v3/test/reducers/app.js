d837722b68ce33eaf258ecf3d16e938e581e01e5
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-10-18 22:49:51 +0800

    Add tests for app

--- v3/test/reducers/app.js
+++ v3/test/reducers/app.js
@@ -2,63 +2,84 @@
 
 import test from 'ava';
 import * as actions from 'actions/timetables';
+import * as themeActions from 'actions/theme';
 import reducer from 'reducers/app';
 
-import type { Semester, Lesson } from 'types/modules';
+import type { Lesson, ModuleCode, Semester } from 'types/modules';
 import type { FSA } from 'types/redux';
 import type { AppState } from 'types/reducers';
 import lessons from '../mocks/lessons-array.json';
 
 const semester: Semester = 1;
 const lesson: Lesson = lessons[0];
-const appHasLessonState: AppState = { activeLesson: lesson };
-const appHasNoLessonState: AppState = { activeLesson: null };
+const moduleCode: ModuleCode = 'CS1010S';
+const appInitialState: AppState = { activeLesson: null, activeModule: null };
+const appHasActiveLessonState: AppState = { ...appInitialState, activeLesson: lesson };
+const appHasActiveModuleState: AppState = { ...appInitialState, activeModule: moduleCode };
 
 test('app should return initial state', (t) => {
   const nextState: AppState = reducer(undefined, {});
 
-  t.deepEqual(nextState, appHasNoLessonState);
+  t.deepEqual(nextState, appInitialState);
 });
 
-test('app should instantiate lesson state', (t) => {
+// Test for active lesson.
+
+test('app should instantiate active lesson', (t) => {
   const action: FSA = actions.modifyLesson(lesson);
-  const nextState: AppState = reducer(appHasNoLessonState, action);
+  const nextState: AppState = reducer(appInitialState, action);
 
-  t.deepEqual(nextState, appHasLessonState);
+  t.deepEqual(nextState, appHasActiveLessonState);
 });
 
-test('app should switch lesson state', (t) => {
+test('app should set active lesson', (t) => {
   const anotherLesson: Lesson = lessons[1];
   const action: FSA = actions.modifyLesson(anotherLesson);
-  const nextState: AppState = reducer(appHasLessonState, action);
+  const nextState: AppState = reducer(appHasActiveLessonState, action);
 
-  t.deepEqual(nextState, {
-    activeLesson: anotherLesson,
-  });
+  t.deepEqual(nextState, { ...appInitialState, activeLesson: anotherLesson });
 });
 
-test('app should accept change and return initial state', (t) => {
+test('app should accept lesson change and unset active lesson', (t) => {
   const action: FSA = actions.changeLesson(semester, lesson);
-  const nextState: AppState = reducer(appHasNoLessonState, action);
+  const nextState: AppState = reducer(appInitialState, action);
 
-  t.deepEqual(nextState, appHasNoLessonState);
+  t.deepEqual(nextState, appInitialState);
 });
 
-test('app should accept change and stop holding lesson in state', (t) => {
-  const action: FSA = actions.changeLesson(semester, lesson);
-  const nextState: AppState = reducer(appHasLessonState, action);
+test('app should cancel and unset active lesson', (t) => {
+  const nextState: AppState = reducer(appHasActiveLessonState, actions.cancelModifyLesson());
 
-  t.deepEqual(nextState, appHasNoLessonState);
+  t.deepEqual(nextState, appInitialState);
 });
 
-test('app should cancel and stop holding lesson in state', (t) => {
-  const nextState: AppState = reducer(appHasLessonState, actions.cancelModifyLesson());
+// Test for active module.
 
-  t.deepEqual(nextState, appHasNoLessonState);
+test('app should instantiate active module', (t) => {
+  const action: FSA = themeActions.modifyModuleColor(moduleCode);
+  const nextState: AppState = reducer(appInitialState, action);
+
+  t.deepEqual(nextState, appHasActiveModuleState);
 });
 
-test('app should cancel and return initial state', (t) => {
-  const nextState: AppState = reducer(appHasNoLessonState, actions.cancelModifyLesson());
+test('app should set active module', (t) => {
+  const anotherModuleCode: ModuleCode = 'CS3216';
+  const action: FSA = themeActions.modifyModuleColor(anotherModuleCode);
+  const nextState: AppState = reducer(appInitialState, action);
 
-  t.deepEqual(nextState, appHasNoLessonState);
+  t.deepEqual(nextState, { ...appInitialState, activeModule: anotherModuleCode });
+});
+
+test('app should accept module color change and unset active module', (t) => {
+  const action: FSA = themeActions.selectModuleColor(moduleCode, 1);
+  const nextState: AppState = reducer(appHasActiveModuleState, action);
+
+  t.deepEqual(nextState, appInitialState);
+});
+
+test('app should cancel and unset active module', (t) => {
+  const nextState: AppState = reducer(appHasActiveModuleState,
+                                      themeActions.cancelModifyModuleColor());
+
+  t.deepEqual(nextState, appInitialState);
 });
