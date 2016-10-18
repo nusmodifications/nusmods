f5d3193fe5cf86c884709722b96f755cfa6a395f
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-10-18 23:14:38 +0800

    Change to function

--- v3/src/js/views/components/color-picker/ColorPicker.jsx
+++ v3/src/js/views/components/color-picker/ColorPicker.jsx
@@ -1,6 +1,6 @@
 // @flow
 /* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import type { ColorIndex } from 'types/reducers';
 
 import _ from 'lodash';
@@ -12,26 +12,22 @@ type Props = {
   onChooseColor: Function,
 };
 
-class ColorPicker extends Component {
-  props: Props;
-
-  render() {
-    return (
-      <div className="color-picker-container">
-        <div className="color-picker">
-          {_.range(NUM_DIFFERENT_COLORS).map((index: ColorIndex) => {
-            return (
-              <span className={`color-option color-${index}`}
-                key={index}
-                onClick={() => {
-                  this.props.onChooseColor(index);
-                }}/>
-            );
-          })}
-        </div>
+function ColorPicker(props: Props) {
+  return (
+    <div className="color-picker-container">
+      <div className="color-picker">
+        {_.range(NUM_DIFFERENT_COLORS).map((index: ColorIndex) => {
+          return (
+            <span className={`color-option color-${index}`}
+              key={index}
+              onClick={() => {
+                props.onChooseColor(index);
+              }}/>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ColorPicker;
