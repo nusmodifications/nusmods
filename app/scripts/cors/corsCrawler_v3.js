97df0ce0b9d8e6f9cbdd70499cd79b5847425ff9
li-kai <falconets25601@hotmail.com>
2016-11-05 22:30:24 +0800

    Use moment for date parsing and add error checking

--- app/scripts/cors/corsCrawler_v3.js
+++ app/scripts/cors/corsCrawler_v3.js
@@ -1,34 +1,22 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const fs = require('fs');
+const moment = require('moment');
+const fs = require('graceful-fs');
 
 // constants
 const ROUND_NAMES = ['0', '1A', '1B', '1C', '2A', '2B', '3A', '3B'];
-const MONTHS = ['January', 'Febuary', 'March', 'April', 'May', 'June',
-                'July', 'August', 'September', 'October', 'November', 'December'];
 
 function formatDateTime(date, time, isClosedStart) {
-  const dateFragments = date.split('/');
-  let day;
-  const month = MONTHS[parseInt(dateFragments[1], 10) - 1];
-  const year = dateFragments[2];
-  let timeWithSec;
+  const dateTime = moment(`${date} ${time}`, 'DD/MM/YYYY HH:mm', true);
+  if (!dateTime.isValid()) {
+    throw new Error(`${date} ${time} is not a valid date`);
+  }
   // closed bidding should start 1 sec after the stated time
   // this stops it from clashing with the end of the open bidding time
   if (isClosedStart) {
-    timeWithSec = `${time}:01`;
-  } else {
-    timeWithSec = `${time}:00`;
+    dateTime.add(1, 'seconds');
   }
-  // removes the zero from the front of single digit dates
-  if (dateFragments[0][0] === '0') {
-    day = dateFragments[0][1];
-  } else {
-    day = dateFragments[0];
-  }
-  const monthDay = [month, day].join(' ');
-  const dateTime = [monthDay, year, timeWithSec].join(', ');
-  return dateTime;
+  return dateTime.format('MMMM D, YYYY, HH:mm:ss');
 }
 
 function formatNoClosed(infoArray) {
@@ -88,21 +76,23 @@ function createEntry(roundName, timeArray) {
 }
 
 function processText(textArray) {
-  const roundName = textArray[0];
-  const roundInfo = textArray.slice(1);
-  const roundType = roundInfo.reduce((count, text) => {
-    return text === 'to' ? count + 1 : count;
-  }, 0);
+  const roundName = textArray.shift();
+  const roundInfo = textArray;
+  const roundType = roundInfo.filter(text => text === 'to').length;
   let timingArray;
-  if (roundType === 1) {
-    // This means that the round has no closed bidding period
-    timingArray = formatNoClosed(roundInfo);
-  } else if (roundType === 3) {
-    // This means that the round has 2 open bidding periods
-    timingArray = formatRoundTwoOpen(roundInfo);
-  } else {
-    // General case
-    timingArray = formatGeneral(roundInfo);
+  switch (roundType) {
+    case 1:
+      // This means that the round has no closed bidding period
+      timingArray = formatNoClosed(roundInfo);
+      break;
+    case 3:
+      // This means that the round has 2 open bidding periods
+      timingArray = formatRoundTwoOpen(roundInfo);
+      break;
+    default:
+      // General case
+      timingArray = formatGeneral(roundInfo);
+      break;
   }
   const output = createEntry(roundName, timingArray);
   return output;
@@ -115,20 +105,27 @@ function getFileName(rawHeader) {
 
 function writeToFile(roundArray, fileName) {
   const jsonArray = JSON.stringify(roundArray, null, 4);
-  fs.appendFileSync(fileName, jsonArray);
+  fs.writeFile(fileName, jsonArray, (error) => {
+    if (error) {
+      throw new Error(`write error: ${error.message}`);
+    }
+  });
 }
 
 const corsSite = 'http://www.nus.edu.sg/cors/schedule.html';
 console.log('visiting ' + corsSite);
 request(corsSite, (error, response, body) => {
+  if (error) {
+    throw new Error(`requesting site failed: ${error}`);
+  }
   const $page = cheerio.load(body);
   const outputArray = [];
   // retrieve the academics year and semester for the file name
   const rawHeader = $page('span.middletabletext:has(span.scheduleheader1)').text().trim();
   const fileName = getFileName(rawHeader);
   // retrieve the bidding schedules
-  $page('tbody:has(td.tableheader)>tr').each(function() {
-    const rawText = $page(this).text().trim();
+  $page('tbody:has(td.tableheader)>tr').each((index, element) => {
+    const rawText = $page(element).text().trim();
     // clean up all the white space from the text
     const text = rawText.replace(/(\r\n\t\t\t\t|\r\n|\n|\r)/g, ' ')
                      .replace(/\s+/g, ' ');
@@ -141,4 +138,3 @@ request(corsSite, (error, response, body) => {
   });
   writeToFile(outputArray, fileName);
 });
-
