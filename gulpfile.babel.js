f0fb14921efae8b5f5c1db6ac0f6bd797bdd466a
Kai <falconets25601@gmail.com>
2016-12-04 06:44:08 +0000

    Add year calculation

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,15 @@
 import gulp from 'gulp';
 
+const now = new Date();
+const currentMonth = now.getMonth() + 1;
+const currentYear = now.getFullYear();
+
+const JULY = 7;
+const schoolYear = currentYear + (currentMonth < JULY);
+
+const AY_START = process.env.YEAR || schoolYear;
+const AY_END = AY_START + 1;
+
 function test(done) {
   done();
 }
