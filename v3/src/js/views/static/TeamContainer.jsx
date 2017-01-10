ca309d033b475b35db65ead35924b80af4fd0e13
zameschua <zameschua@gmail.com>
2017-01-10 21:53:53 +0800

    Port team page to v3 (#312)

---
+++ v3/src/js/views/static/TeamContainer.jsx
@@ -0,0 +1,190 @@
+import React from 'react';
+import DocumentTitle from 'react-document-title';
+
+export default function TeamContainer() {
+  return (
+    <DocumentTitle title="Team">
+      <div className="row">
+        <div className="col-md-8 offset-md-1">
+          <h2>Team</h2>
+          <hr />
+          <p><em>NUSMods is an open source project that relies on the continuous support of its individual contributors and NUS student community. We hope to involve more developers and contributors in making NUSMods even better. Please reach out to us if you are interested in helping!</em></p>
+          <p>These are the members of our core team:</p>
+          <hr />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/ahbeng">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/601eddec66c2e48891195af393144c17?s=256" alt="Beng" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Beng (Eu Beng Hee)</h3>
+              <p>He&#39;s Beng. Nuff&#39; said.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/benghee"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.twitter.com/ahbengish"><i className="fa fa-twitter fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/benghee"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/ahbeng"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/yangshun">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/66eca275a85a6a0c06b0a0f7039b074b?s=256" alt="Tay Yang Shun" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Tay Yang Shun</h3>
+              <p>Front-end developer who loves good code, design and typography.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/yangshun"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.twitter.com/yangshunz"><i className="fa fa-twitter fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/yangshun"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/yangshun"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/xinan">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/802da99824fb41330a44af89256da661.png?s=256" alt="Liu Xinan" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Liu Xinan</h3>
+              <p>A crazy developer who likes magic, ...and tea!</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/xinan.liu"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/xinan"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/xinan"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/ngzhian">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/959333b0fda0517060c9750f8c49ead0?s=256" alt="Ng Zhi An" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Ng Zhi An</h3>
+              <p>undefined is not a function.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/ngzhian"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.twitter.com/ngzhian"><i className="fa fa-twitter fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/ngzhian"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/ngzhian"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/li-kai">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/3d2bcece0409c7578b2e9d87f0c1b6d7?s=256" alt="Li Kai" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Li Kai</h3>
+              <p>Happy to help.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/falconets"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://twitter.com/falconets"><i className="fa fa-twitter fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/li-kai"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.github.com/xbili">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/a26e376c4bc050614f13581cf6a13e09.png?s=256" alt="Xu Bili" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Xu Bili</h3>
+              <p>Loves to read and write elegant code. JavaScript enthusiast.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/xbili"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.twitter.com/lantis_play"><i className="fa fa-twitter fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/xbili"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.github.com/xbili"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-md-3">
+              <a href="https://www.facebook.com/ang.yenling">
+                <img className="img-fluid" src="https://www.gravatar.com/avatar/630fe6848c11082312b57b1360a0dd09?s=256" alt="Ang Yen Ling" />
+              </a>
+            </div>
+            <div className="col-md-9">
+              <h3>Ang Yen Ling</h3>
+              <p>Bubbly girl who loves new things, matcha ice cream and marketing.</p>
+              <div className="row">
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/ang.yenling"><i className="fa fa-facebook-square fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.linkedin.com/in/yen-ling-ang-a56158b6"><i className="fa fa-linkedin fa-lg" /></a>
+                </div>
+                <div className="col-md-1 col-xs-3">
+                  <a href="https://www.facebook.com/ang.yenling"><i className="fa fa-github-square fa-lg" /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DocumentTitle>
+  );
+}
