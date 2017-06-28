3c228ce60253c37d4ad2679f8f334c05a6cf7c23
Li Kai <falconets25601@gmail.com>
2017-06-28 21:22:54 +0800

    Update tests for actions to use snapshots

--- v3/__tests__/actions/settings.js
+++ v3/__tests__/actions/settings.js
@@ -1,45 +1,24 @@
 // @flow
-import type { FSA } from 'types/redux';
 import type { Faculty, ModuleCode } from 'types/modules';
 
 import * as actions from 'actions/settings';
 
 test('should dispatch a select of a newStudent value', () => {
   const newStudent = false;
-  const expectedResult: FSA = {
-    type: actions.SELECT_NEW_STUDENT,
-    payload: newStudent,
-  };
-  const resultOfAction: FSA = actions.selectNewStudent(newStudent);
-  expect(resultOfAction).toEqual(expectedResult);
+  expect(actions.selectNewStudent(newStudent)).toMatchSnapshot();
 });
 
 test('should dispatch a select of a faculty value', () => {
   const faculty: Faculty = 'Some faculty';
-  const expectedResult: FSA = {
-    type: actions.SELECT_FACULTY,
-    payload: faculty,
-  };
-  const resultOfAction: FSA = actions.selectFaculty(faculty);
-  expect(resultOfAction).toEqual(expectedResult);
+  expect(actions.selectFaculty(faculty)).toMatchSnapshot();
 });
 
 test('should dispatch a module code for hiding', () => {
   const moduleCode: ModuleCode = 'CS1010';
-  const expectedResult: FSA = {
-    type: actions.HIDE_LESSON_IN_TIMETABLE,
-    payload: moduleCode,
-  };
-  const resultOfAction: FSA = actions.hideLessonInTimetable(moduleCode);
-  expect(resultOfAction).toEqual(expectedResult);
+  expect(actions.hideLessonInTimetable(moduleCode)).toMatchSnapshot();
 });
 
 test('should dispatch a module code for showing', () => {
   const moduleCode: ModuleCode = 'CS1020';
-  const expectedResult: FSA = {
-    type: actions.SHOW_LESSON_IN_TIMETABLE,
-    payload: moduleCode,
-  };
-  const resultOfAction: FSA = actions.showLessonInTimetable(moduleCode);
-  expect(resultOfAction).toEqual(expectedResult);
+  expect(actions.showLessonInTimetable(moduleCode)).toMatchSnapshot();
 });
