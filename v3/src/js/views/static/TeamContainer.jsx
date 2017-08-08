62574742809a026b2dabb9a767b1e09b324e5840
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-08-08 17:28:22 +0800

    Add aria-labels for accessibility (#421)
    In line with Font Awesome best practices, if the icons are semantic
    (ie. they have meaning), and if there are no text labels, the icons
    should be accompanied aria text labels

--- v3/src/js/views/static/TeamContainer.jsx
+++ v3/src/js/views/static/TeamContainer.jsx
@@ -34,22 +34,30 @@ export default function TeamContainer() {
                   <div className="row">
                     {teamMember.facebook &&
                       <div className="col-sm-1 col-xs-3">
-                        <a href={teamMember.facebook}><i className="fa fa-facebook-square fa-lg" /></a>
+                        <a href={teamMember.facebook} title="Facebook profile" aria-label="Facebook profile">
+                          <i className="fa fa-facebook-square fa-lg" aria-hidden="true" />
+                        </a>
                       </div>
                     }
                     {teamMember.twitter &&
                       <div className="col-sm-1 col-xs-3">
-                        <a href={teamMember.twitter}><i className="fa fa-twitter fa-lg" /></a>
+                        <a href={teamMember.twitter} title="Twitter feed" aria-label="Twitter feed">
+                          <i className="fa fa-twitter fa-lg" aria-hidden="true" />
+                        </a>
                       </div>
                     }
                     {teamMember.linkedin &&
                       <div className="col-sm-1 col-xs-3">
-                        <a href={teamMember.linkedin}><i className="fa fa-linkedin fa-lg" /></a>
+                        <a href={teamMember.linkedin} title="LinkedIn profile" aria-label="LinkedIn profile">
+                          <i className="fa fa-linkedin fa-lg" aria-hidden="true" />
+                        </a>
                       </div>
                     }
                     {teamMember.github &&
                       <div className="col-sm-1 col-xs-3">
-                        <a href={teamMember.github}><i className="fa fa-github-square fa-lg" /></a>
+                        <a href={teamMember.github} title="GitHub profile" aria-label="GitHub profile" >
+                          <i className="fa fa-github-square fa-lg" aria-hidden="true" />
+                        </a>
                       </div>
                     }
                   </div>
