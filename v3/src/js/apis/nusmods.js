6878bef00a57f4519f188a809173424f241c1db7
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-08-15 12:00:47 +0800

    Add moduleInformation API endpoint and FETCH_ALL_MODULES action

--- v3/src/js/apis/nusmods.js
+++ v3/src/js/apis/nusmods.js
@@ -7,13 +7,20 @@ const ayBaseUrl: string = `${config.apiBaseUrl}/${config.academicYear.replace('/
 
 const NUSModsApi = {
   ayBaseUrl: (): string => ayBaseUrl,
+
   // List of modules for the entire acad year.
   moduleListUrl: (): string => {
     return `${ayBaseUrl}/moduleList.json`;
   },
+
   // Module for that acad year. Not tied to any semester.
   moduleDetailsUrl: (moduleCode: ModuleCode): string => {
     return `${ayBaseUrl}/modules/${moduleCode}.json`;
+  },
+
+  // List of all modules for the entire acad year
+  modulesUrl: (): string => {
+    return `${ayBaseUrl}/moduleInformation.json`;
   },
 };
 
