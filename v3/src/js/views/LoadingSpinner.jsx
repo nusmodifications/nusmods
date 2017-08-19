d94412c41ec83d628e1d1ce5ddb37d43ef239643
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-08-19 17:33:37 +0800

    Use fa-4x instead of inline styles

--- v3/src/js/views/LoadingSpinner.jsx
+++ v3/src/js/views/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function LoadingSpinner() {
   return (
     <div className="loading-spinner">
-      <i className="fa fa-fw fa-circle-o-notch fa-spin" style={{ fontSize: '4rem' }} aria-hidden="true" />
+      <i className="fa fa-4x fa-fw fa-circle-o-notch fa-spin" aria-hidden="true" />
       <p className="loading-text">Loading...</p>
     </div>
   );
