fb0e8678b803eebc4f48f1aaf5777e0cc29a8a74
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-08-21 19:46:08 +0800

    Improve comments for LinkModuleCodes

--- v3/src/js/views/components/LinkModuleCodes.jsx
+++ v3/src/js/views/components/LinkModuleCodes.jsx
@@ -11,20 +11,25 @@ type Props = {
   moduleCodes: Set<ModuleCode>,
 };
 
+// Look for strings that look like module codes - eg.
+// ACC1010  - 3 chars, 4 digits, no suffix
+// CS1010FC - 2 chars, 4 digits, 2 chars
+// CS2014R  - 2 chars, 4 digits, 1 char
+// BMA 5001 - 3 chars, space, 4 digits
+const MODULE_CODE_REGEX = /\b(\w{2,3}\s*\d{4}\w{0,2})\b/g;
+
 export function LinkModuleCodesComponent(props: Props) {
   const { children, moduleCodes } = props;
 
-  // Look for strings that look like module codes - eg.
-  // ACC1010  - 3 chars, 4 digits, no suffix
-  // CS1010FC - 2 chars, 4 digits, 2 chars
-  // CS2014R  - 2 chars, 4 digits, 1 char
-  // BMA 5001 - 3 chars, space, 4 digits
+  const parts = children.split(MODULE_CODE_REGEX);
 
-  // Add a space in front to force regex matches to be even
-  // Otherwise if the first character is part of a match, the matches will be
-  // odd elements
-  const parts = (' ' + children).split(/\b(\w{2,3}\s*\d{4}\w{0,2})\b/g); // eslint-disable-line prefer-template
-  parts[0] = parts[0].slice(1);
+  // We want to ensure the resulting array always has ModuleCode at even position
+  // eg. ['Some text ', 'CS1010S', ' more text ', 'CS3216', 'more text'].
+  // This allows us to replace the even position elements with <Link> components.
+  // However, if the string starts with a module code, then the first element will be a module
+  // so we add in an empty string to pad module codes to even positions
+  if (parts.length && MODULE_CODE_REGEX.test(parts[0])) parts.unshift('');
+
   return (
     <span>{parts.map((part, i) => {
       if (i % 2 === 0) return part;
