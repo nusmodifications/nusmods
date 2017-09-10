2616f30bb4ab9c5dba9ba5584f22c7c7568330ae
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-09-10 23:46:53 +0800

    Add typedef for Config object

--- v3/src/js/config/index.js
+++ v3/src/js/config/index.js
@@ -1,9 +1,34 @@
 // @flow
-import type { Semester } from 'types/modules';
+import type { Semester, AcadYear } from 'types/modules';
 
 import appConfig from './app-config.json';
 
-const augmentedConfig: Object = {
+type Config = {
+  brandName: string,
+  academicYear: AcadYear,
+  semester: Semester,
+
+  apiBaseUrl: string,
+  corsUrl: string,
+  ivleUrl: string,
+
+  disqusShortname: string,
+
+  semesterNames: { [Semester]: string },
+  shortSemesterNames: { [Semester]: string },
+
+  defaultPreferences: {
+    theme: string,
+    mode: string,
+    faculty: string,
+    student: string,
+    account: string,
+  },
+
+  semTimetableFragment: (Semester) => string,
+};
+
+const augmentedConfig: Config = {
   ...appConfig,
   semTimetableFragment: (semester: Semester = appConfig.semester): string => {
     // For use in the URL: E.g. `timetable/2016-2017/sem1`
