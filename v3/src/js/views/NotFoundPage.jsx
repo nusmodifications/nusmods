b72d1b2b761cca15e30994177826685309140149
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-10-06 19:10:14 +0800

    Add user report button to NotFoundPage

--- v3/src/js/views/NotFoundPage.jsx
+++ v3/src/js/views/NotFoundPage.jsx
@@ -1,10 +1,27 @@
 // @flow
 import React from 'react';
+import Helmet from 'react-helmet';
+import Raven from 'raven-js';
+
+import config from 'config';
 
 export default function NotFoundPage() {
+  Raven.captureMessage('404 - Page Not Found');
+  const eventId = Raven.lastEventId();
+
   return (
     <div>
+      <Helmet>
+        <title>404 Not Found - {config.brandName}</title>
+      </Helmet>
+
       <h1>404 Not Found</h1>
+      <p>
+        Were you expecting something here?
+        <button className="btn btn-link" onClick={() => Raven.showReportDialog({ eventId })}>
+        Tell us
+        </button>
+      </p>
     </div>
   );
 }
