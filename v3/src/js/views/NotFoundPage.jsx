d4820fad31258b5480568c22de77002d9b108bc5
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-10-12 19:53:02 +0800

    Improve 404 page usability

--- v3/src/js/views/NotFoundPage.jsx
+++ v3/src/js/views/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
 import Raven from 'raven-js';
 
 import config from 'config';
@@ -12,16 +13,30 @@ export default function NotFoundPage() {
   return (
     <div>
       <Helmet>
-        <title>404 Not Found - {config.brandName}</title>
+        <title>Page Not Found - {config.brandName}</title>
       </Helmet>
 
-      <h1>404 Not Found</h1>
-      <p>
-        Were you expecting something here?
-        <button className="btn btn-link" onClick={() => Raven.showReportDialog({ eventId })}>
-        Tell us
-        </button>
-      </p>
+      <div className="page-container">
+        <div className="ml-md-5 mt-3">
+          <p className="mb-0 h1 text-primary">Oops...</p>
+          <h1 className="mb-4">page not found.</h1>
+          <p>
+            If you think something <em>should</em> be here,
+            <button
+              className="btn-link p-0"
+              onClick={() => Raven.showReportDialog({ eventId })}
+            >
+              do tell us
+            </button>
+          </p>
+
+          <p className="font-weight-bold mb-0">Otherwise, if you want to</p>
+          <ul className="list-unstyled">
+            <li>...plan your timetable? <Link to="/">Go back to Nusmods.com</Link></li>
+            <li>...find a module? <Link to="/modules">Try the module finder</Link></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
