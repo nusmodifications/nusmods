005c0fb0d7050575bb21b22ca219f96640336272
Tay Yang Shun <tay.yang.shun@gmail.com>
2017-11-03 02:18:15 +0800

    Fix test lint

--- v3/src/js/views/components/semester-switcher/SemesterSwitcher.test.jsx
+++ v3/src/js/views/components/semester-switcher/SemesterSwitcher.test.jsx
@@ -7,10 +7,11 @@ import SemesterSwitcher from './SemesterSwitcher';
 describe('<SemesterSwitcher />', () => {
   test('simulates click events', () => {
     const onButtonClick = jest.fn();
-    const wrapper = shallow(<SemesterSwitcher
-      semester={2}
-      onSelectSemester={onButtonClick}
-      />
+    const wrapper = shallow(
+      <SemesterSwitcher
+        semester={2}
+        onSelectSemester={onButtonClick}
+      />,
     );
     const buttons = wrapper.find('button');
     buttons.at(0).simulate('click');
@@ -22,10 +23,11 @@ describe('<SemesterSwitcher />', () => {
   describe('semester switching buttons are disabled appropriately', () => {
     test('left button', () => {
       const onButtonClick = jest.fn();
-      const wrapper = shallow(<SemesterSwitcher
-        semester={1}
-        onSelectSemester={onButtonClick}
-        />
+      const wrapper = shallow(
+        <SemesterSwitcher
+          semester={1}
+          onSelectSemester={onButtonClick}
+        />,
       );
       const buttons = wrapper.find('button');
       buttons.at(0).simulate('click');
@@ -36,10 +38,11 @@ describe('<SemesterSwitcher />', () => {
 
     test('right button', () => {
       const onButtonClick = jest.fn();
-      const wrapper = shallow(<SemesterSwitcher
-        semester={4}
-        onSelectSemester={onButtonClick}
-        />
+      const wrapper = shallow(
+        <SemesterSwitcher
+          semester={4}
+          onSelectSemester={onButtonClick}
+        />,
       );
       const buttons = wrapper.find('button');
       buttons.at(1).simulate('click');
