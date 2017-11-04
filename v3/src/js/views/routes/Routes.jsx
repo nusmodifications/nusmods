4c304b02f310cec0d5dbe5f7fc8cfaa142c7e64e
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-11-04 22:39:32 +0800

    Move routes under their own folder

--- v3/src/js/views/routes/Routes.jsx
+++ v3/src/js/views/routes/Routes.jsx
@@ -1,6 +1,10 @@
 // @flow
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect, type MapStateToProps } from 'react-redux';
+import { withRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import type { Semester } from 'types/modules';
+import type { State } from 'reducers/index';
 
 import TimetableContainer from 'views/timetable/TimetableContainer';
 import ModulePageContainer from 'views/browse/ModulePageContainer';
@@ -11,14 +15,21 @@ import TeamContainer from 'views/static/TeamContainer';
 import DevelopersContainer from 'views/static/DevelopersContainer';
 import FaqContainer from 'views/static/FaqContainer';
 import NotFoundPage from 'views/errors/NotFoundPage';
+import { timetablePage } from 'views/routes/paths';
 
-export default function Routes() {
+type Props = {
+  activeSemester: Semester,
+}
+
+function Routes(props: Props) {
   return (
     <Switch>
-      <Redirect exact from="/" to="/timetable" />
+      <Redirect exact from="/" to={timetablePage(props.activeSemester)} />
+      <Redirect exact from="/timetable" to={timetablePage(props.activeSemester)} />
+
       <Route path="/about" component={AboutContainer} />
       <Route path="/faq" component={FaqContainer} />
-      <Route path="/timetable" component={TimetableContainer} />
+      <Route path="/timetable/:semester" component={TimetableContainer} />
       <Route exact path="/modules" component={ModuleFinderContainer} />
       <Route path="/modules/:moduleCode/:slug?" component={ModulePageContainer} />
       <Route path="/settings" component={SettingsContainer} />
@@ -28,3 +39,8 @@ export default function Routes() {
     </Switch>
   );
 }
+
+const mapStateToProps: MapStateToProps<*, *, *> = (state: State) => ({
+  activeSemester: state.app.activeSemester,
+});
+export default withRouter(connect(mapStateToProps)(Routes));
