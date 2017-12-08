6fb50380e99a79f8f4fbad949a680856b6e835db
E-Liang Tan <eliang@eliangtan.com>
2017-12-08 16:03:30 +0800

    Implement venue search using SearchBox

--- v3/src/js/views/venues/VenuesContainer.jsx
+++ v3/src/js/views/venues/VenuesContainer.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import axios from 'axios';
 import Raven from 'raven-js';
+import { pick } from 'lodash';
 
 import type { VenueInfo } from 'types/venues';
 import type { Semester } from 'types/modules';
@@ -11,6 +12,7 @@ import type { Semester } from 'types/modules';
 import ErrorPage from 'views/errors/ErrorPage';
 import LoadingSpinner from 'views/components/LoadingSpinner';
 import VenueList from 'views/venues/VenueList';
+import SearchBox from 'views/components/SearchBox';
 
 import config from 'config';
 import nusmods from 'apis/nusmods';
@@ -23,6 +25,7 @@ type State = {
   loading: boolean,
   venues: VenueInfo,
   error?: any,
+  searchTerm: string,
 };
 
 const pageHead = (
@@ -35,6 +38,7 @@ class VenuesContainer extends Component<Props, State> {
   state: State = {
     loading: true,
     venues: {},
+    searchTerm: '',
   }
 
   componentDidMount() {
@@ -51,8 +55,23 @@ class VenuesContainer extends Component<Props, State> {
       });
   }
 
+  filteredVenues() {
+    const { venues, searchTerm } = this.state;
+    if (!venues) {
+      return null;
+    }
+
+    if (searchTerm === '') {
+      return venues;
+    }
+
+    const lowercaseSearchStr = searchTerm.toLowerCase();
+    return pick(venues, Object.keys(venues).filter(name =>
+      name.toLowerCase().indexOf(lowercaseSearchStr) !== -1));
+  }
+
   render() {
-    const { venues, loading, error } = this.state;
+    const { loading, error } = this.state;
 
     if (error) {
       return <ErrorPage error="cannot load venues info" eventId={Raven.lastEventId()} />;
@@ -67,14 +86,22 @@ class VenuesContainer extends Component<Props, State> {
       );
     }
 
+    const venues = this.filteredVenues();
+
     return (
       <div className="modules-page-container page-container">
         {pageHead}
 
         <div className="row">
           <div className="col-sm-12">
-            <h1 className="page-title">Venues</h1>
-            <VenueList venues={venues} />
+            <SearchBox
+              throttle={0}
+              useInstantSearch
+              initialSearchTerm={this.state.searchTerm}
+              placeholder="Venues"
+              onSearch={searchTerm => this.setState({ searchTerm })}
+            />
+            {venues ? <VenueList venues={venues} /> : null}
           </div>
         </div>
       </div>
