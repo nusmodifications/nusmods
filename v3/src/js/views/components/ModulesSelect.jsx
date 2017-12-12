3cc279127af7a46ba289fb9d72f33d464d964d19
Li Kai <falconets25601@gmail.com>
2017-12-12 14:42:26 +0800

    Add module select

--- v3/src/js/views/components/ModulesSelect.jsx
+++ v3/src/js/views/components/ModulesSelect.jsx
@@ -1,13 +1,12 @@
 // @flow
 import React, { Component } from 'react';
 import _ from 'lodash';
-import VirtualizedSelect from 'react-virtualized-select';
-import createFilterOptions from 'react-select-fast-filter-options';
-import { PrefixIndexStrategy } from 'js-search';
+import Downshift from 'downshift';
+import classnames from 'classnames';
 
 import type { ModuleSelectList } from 'types/reducers';
 
-import { ModulesSearchIndex, ModulesTokenizer } from 'utils/modulesSearch';
+import styles from './ModulesSelect.scss';
 
 type Props = {
   moduleList: ModuleSelectList,
@@ -20,21 +19,70 @@ class ModulesSelect extends Component<Props> {
     return _.size(this.props.moduleList) !== _.size(nextProps.moduleList);
   }
 
-  render() {
-    const filterOptions = createFilterOptions({
-      options: this.props.moduleList,
-      indexStrategy: new PrefixIndexStrategy(),
-      tokenizer: new ModulesTokenizer(),
-      searchIndex: new ModulesSearchIndex(),
-    });
+  getFilteredModules = (inputValue: string) => {
+    if (inputValue === '') {
+      return [];
+    }
 
+    const reg = RegExp(`\\b${inputValue}`, 'i');
+    const codeMatches = [];
+    const titleMatches = [];
+
+    this.props.moduleList.forEach((mod) => {
+      const index = mod.label.search(reg);
+      if (index >= 6) {
+        titleMatches.push(mod);
+      } else if (index >= 0) {
+        codeMatches.push(mod);
+      }
+    });
+    return codeMatches.concat(titleMatches);
+  };
+
+  render() {
     return (
-      <VirtualizedSelect
-        options={this.props.moduleList}
-        filterOptions={filterOptions}
-        placeholder={this.props.placeholder || 'Search module'}
-        onChange={this.props.onChange}
-      />
+      <Downshift onSelect={this.props.onChange}>
+        {({ getLabelProps, getInputProps, getItemProps, isOpen, inputValue, highlightedIndex }) => (
+          <div className={styles.container}>
+            <label htmlFor="search" className="sr-only" {...getLabelProps()}>
+              Languages
+            </label>
+            <input
+              id="search"
+              className={classnames(styles.input, 'form-control')}
+              {...getInputProps({
+                placeholder: 'Add a module',
+              })}
+            />
+            {isOpen && (
+              <ol className={styles.selectList}>
+                {this.getFilteredModules(inputValue).map((module, index) => {
+                  return (
+                    <li
+                      {...getItemProps({
+                        key: module.value,
+                        item: module.value,
+                        index,
+                      })}
+                    >
+                      <button
+                        className={classnames('btn', styles.option, {
+                          [styles.optionSelected]: highlightedIndex === index,
+                        })}
+                      >
+                        {module.label}
+                      </button>
+                    </li>
+                  );
+                })}
+                <li className={styles.defaultPanel}>
+                  Try CS1010 or Programming. Searching <strong>{this.props.moduleList.length}</strong> modules.
+                </li>
+              </ol>
+            )}
+          </div>
+        )}
+      </Downshift>
     );
   }
 }
