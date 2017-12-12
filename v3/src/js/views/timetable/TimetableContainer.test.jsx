1fe2e0fd56ebc7afc4498a414b9fa613c6f7244d
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-12-12 17:58:23 +0800

    Add tests

--- v3/src/js/views/timetable/TimetableContainer.test.jsx
+++ v3/src/js/views/timetable/TimetableContainer.test.jsx
@@ -1,96 +1,129 @@
-// // @flow
-//
-// import React from 'react';
-// import { Redirect } from 'react-router-dom';
-// import { shallow } from 'enzyme';
-// import type { Semester } from 'types/modules';
-// import type { SemTimetableConfig } from 'types/timetables';
-//
-// // react-router-dom internal dependency, used here to construct the history
-// // object needed for testing. This is not added as a dev dependency to avoid
-// // version desync between the version depended on by react-router-dom
-// import createHistory from 'history/createMemoryHistory'; // eslint-disable-line import/no-extraneous-dependencies
-// import { timetablePage, fromSemester } from 'views/routes/paths';
-//
-// import { TimetableContainerComponent } from './TimetableContainer';
-// import { deserializeTimetable } from '../../utils/timetables';
-//
-// const TIMETABLE: SemTimetableConfig = {
-//   CS2104: { Lecture: '1' },
-//   CS2105: { Lecture: '1' },
-// };
-//
-// function create(semester: ?Semester, timetable: SemTimetableConfig) {
-//   const history = createHistory();
-//   let path;
-//   let params;
-//
-//   if (semester == null) {
-//     path = '/timetable';
-//     params = {};
-//   } else {
-//     path = timetablePage(semester);
-//     params = { semester: fromSemester[semester] };
-//   }
-//
-//   const match = {
-//     path,
-//     params,
-//     url: path,
-//     isExact: true,
-//   };
-//
-//   return {
-//     history,
-//     wrapper: shallow(
-//       <TimetableContainerComponent
-//         history={history}
-//         location={history.location}
-//         match={match}
-//         activeSemester={1}
-//         semester={semester}
-//         timetable={timetable}
-//         selectSemester={jest.fn()}
-//         setTimetable={jest.fn()}
-//         fetchModule={jest.fn()}
-//       />,
-//     ),
-//   };
-// }
-//
-// test('should redirect to activeSemester when semester is empty', () => {
-//   const wrapper = create(null, {}).wrapper;
-//
-//   expect(wrapper.type()).toEqual(Redirect);
-//   expect(wrapper.props()).toMatchObject({
-//     to: timetablePage(1),
-//   });
-// });
-//
-// test('should not have query string when there are no modules selected', () => {
-//   expect(create(1, {}).history.location.search).toBe('');
-// });
-//
-// test('should update query string when timetable changes', () => {
-//   const container = create(1, TIMETABLE);
-//   expect(container.history.location.search).toMatch('CS2104');
-//   expect(container.history.location.search).toMatch('CS2105');
-//
-//   // Simulate lesson choice update
-//   container.wrapper.setProps({
-//     timetable: {
-//       CS2104: { Lecture: '2' },
-//       CS2105: { Lecture: '1' },
-//     },
-//   });
-//   expect(deserializeTimetable(container.history.location.search).CS2104.Lecture)
-//     .toBe('2');
-//
-//   // Simulate deleting module
-//   container.wrapper.setProps({
-//     timetable: {
-//       CS2104: { Lecture: '2' },
-//     },
-//   });
-//   expect(container.history.location.search).not.toMatch('CS2105');
-// });
+// @flow
+
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { shallow } from 'enzyme';
+import type { SemTimetableConfig } from 'types/timetables';
+import type { ModulesMap } from 'reducers/entities/moduleBank';
+
+// react-router-dom internal dependency, used here to construct the history
+// object needed for testing. This is not added as a dev dependency to avoid
+// version desync between the version depended on by react-router-dom
+import createHistory from 'history/createMemoryHistory'; // eslint-disable-line import/no-extraneous-dependencies
+
+import { timetablePage, semesterForTimetablePage, timetableShare } from 'views/routes/paths';
+import NotFoundPage from 'views/errors/NotFoundPage';
+
+/** @var {Module} */
+import CS1010S from '__mocks__/modules/CS1010S.json';
+/** @var {Module} */
+import CS3216 from '__mocks__/modules/CS3216.json';
+
+import { TimetableContainerComponent } from './TimetableContainer';
+import TimetableContent from './TimetableContent';
+import LoadingSpinner from '../components/LoadingSpinner';
+
+function create(
+  path: string,
+  params: { [string]: ?string } = {},
+  timetable: SemTimetableConfig = {},
+  modules: ModulesMap = { CS1010S, CS3216 },
+) {
+  const history = createHistory({ initialEntries: [path] });
+  const match = {
+    path,
+    params,
+    url: path,
+    isExact: true,
+  };
+
+  const selectSemester = jest.fn();
+  const setTimetable = jest.fn();
+  const fetchModule = jest.fn();
+
+  return {
+    history,
+    selectSemester,
+    setTimetable,
+    fetchModule,
+
+    wrapper: shallow(
+      <TimetableContainerComponent
+        history={history}
+        location={history.location}
+        match={match}
+
+        activeSemester={1}
+        semester={semesterForTimetablePage(params.semester)}
+        timetable={timetable}
+        colors={{}}
+        modules={modules}
+
+        selectSemester={selectSemester}
+        setTimetable={setTimetable}
+        fetchModule={fetchModule}
+      />,
+    ),
+  };
+}
+
+function createWithImport(
+  importedTimetable: SemTimetableConfig,
+  modules: ModulesMap = {},
+  existingTimetable: SemTimetableConfig = {},
+) {
+  const semester = 1;
+  const path = timetableShare(semester, importedTimetable);
+  return create(path, { semester: 'sem-1', action: 'share' }, existingTimetable, modules);
+}
+
+test('should redirect to activeSemester when semester is empty', () => {
+  const wrapper = create('/timetable').wrapper;
+
+  expect(wrapper.type()).toEqual(Redirect);
+  expect(wrapper.props()).toMatchObject({ to: timetablePage(1) });
+});
+
+test('should show 404 when the URL is invalid', () => {
+  expect(create('/timetable/hello', { semester: 'hello' })
+    .wrapper.type()).toEqual(NotFoundPage);
+
+  expect(create('/timetable/sem-1/hello', { semester: '1', action: 'hello' })
+    .wrapper.type()).toEqual(NotFoundPage);
+});
+
+test('should load modules from imported timetable', () => {
+  const container = createWithImport({ CS2105: { Lecture: '1' } });
+  expect(container.fetchModule).toBeCalledWith('CS2105');
+});
+
+test('should display spinner when loading modules', () => {
+  const timetable = { CS2105: { Lecture: '1' } };
+  const wrapper = createWithImport(timetable).wrapper;
+
+  expect(wrapper.type()).toEqual(LoadingSpinner);
+});
+
+test('should display imported timetable', () => {
+  const timetable = { CS2105: { Lecture: '1' } };
+  const wrapper = createWithImport(timetable, { CS2105: CS3216 }).wrapper;
+
+  expect(wrapper.type()).toEqual(TimetableContent);
+  expect(wrapper.props()).toMatchObject({
+    timetable,
+    readOnly: true,
+  });
+});
+
+test('should display saved timetable when there is no imported timetable', () => {
+  const semester = 1;
+  const timetable = { CS2105: { Lecture: '1' } };
+
+  const wrapper = create(timetablePage(semester), { semester: 'sem-1' }, timetable).wrapper;
+
+  expect(wrapper.type()).toEqual(TimetableContent);
+  expect(wrapper.props()).toMatchObject({
+    timetable,
+    readOnly: false,
+  });
+});
