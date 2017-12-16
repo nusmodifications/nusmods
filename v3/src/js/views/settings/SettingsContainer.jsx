90cce9af5b40af29211dd73a3d556ab696ab9d82
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-12-16 10:05:11 +0800

    Hide dark mode settings when CSS variables are not supported

--- v3/src/js/views/settings/SettingsContainer.jsx
+++ v3/src/js/views/settings/SettingsContainer.jsx
@@ -12,6 +12,7 @@ import { selectNewStudent, selectFaculty, selectMode } from 'actions/settings';
 import availableThemes from 'data/themes.json';
 import FacultySelect from 'views/components/FacultySelect';
 import NewStudentSelect from 'views/components/NewStudentSelect';
+import { supportsCSSVariables } from 'utils/react';
 import ThemeOption from './ThemeOption';
 import ModeSelect from './ModeSelect';
 
@@ -65,18 +66,20 @@ function SettingsContainer(props: Props) {
           </div>
           <hr />
 
-          <h4>Night Mode</h4>
-          <div className="row">
-            <div className="col-sm-7">
-              <p>Night mode turns the light surfaces of the page dark, creating an
-              experience ideal for the dark. Try it out!</p>
-            </div>
-            <div className="col-sm-4 offset-sm-1 text-xs-right">
-              <ModeSelect mode={props.mode} onSelectMode={props.selectMode} />
-            </div>
-          </div>
-          <hr />
-
+          {supportsCSSVariables() &&
+            <div>
+              <h4>Night Mode</h4>
+              <div className="row">
+                <div className="col-sm-7">
+                  <p>Night mode turns the light surfaces of the page dark, creating an
+                    experience ideal for the dark. Try it out!</p>
+                </div>
+                <div className="col-sm-4 offset-sm-1 text-xs-right">
+                  <ModeSelect mode={props.mode} onSelectMode={props.selectMode} />
+                </div>
+              </div>
+              <hr />
+            </div>}
 
           <h4>Theme</h4>
           <div>
