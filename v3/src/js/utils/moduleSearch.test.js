00bdd483a99a8552f1627ce9a9fb7958e95ec529
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-12-25 14:47:33 +0800

    Improve moduleSearch tests

--- v3/src/js/utils/moduleSearch.test.js
+++ v3/src/js/utils/moduleSearch.test.js
@@ -1,53 +1,50 @@
+// @flow
+import type { ModuleCode } from 'types/modules';
 import moduleList from '__mocks__/modules';
+
 import { createSearchFilter, sortModules } from './moduleSearch';
 
-test('it searches by module prefix', () => {
-  const filter = createSearchFilter('CS').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual(['CS1010S', 'CS3216']);
+function testSearchTerm(term: string, expectedModules: ModuleCode[]) {
+  const filter = createSearchFilter(term).initFilters(moduleList);
+  expect(filter.filteredModules())
+    .toEqual(new Set(expectedModules));
+}
+
+test('searches by module prefix', () => {
+  testSearchTerm('CS', ['CS1010S', 'CS3216']);
 });
 
-test('it searches by module code', () => {
-  const filter = createSearchFilter('10').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual(['BFS1001', 'CS1010S', 'GES1021']);
+test('searches by module code', () => {
+  testSearchTerm('10', ['BFS1001', 'CS1010S', 'GES1021']);
 });
 
-test('it searches by module title', () => {
-  const filter = createSearchFilter('Managerial').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual(['ACC2002']);
+test('searches by module title', () => {
+  testSearchTerm('Managerial', ['ACC2002']);
 });
 
-test('it searches by module description, if available', () => {
-  const filter = createSearchFilter('concepts').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual(['ACC2002', 'CS1010S']);
+test('searches by module description, if available', () => {
+  testSearchTerm('concepts', ['ACC2002', 'CS1010S']);
 });
 
-test('it searches multiword terms', () => {
-  const filter = createSearchFilter('coding,  testing ').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual(['CS1010S']);
+test('searches multiword terms', () => {
+  testSearchTerm('coding,  testing', ['CS1010S']);
 });
 
-test('its searches get more specific with more keywords', () => {
-  const filter = createSearchFilter('to by').initFilters(moduleList);
-  const filtered = [...filter.filteredModules()];
-  expect(filtered).toEqual([]);
+test('searches get more specific with more keywords', () => {
+  testSearchTerm('to by', []);
 });
 
-test('it sorts by prefering module prefix', () => {
+test('sorts by preferring module prefix', () => {
   const [first, second] = sortModules('cs', moduleList).map(m => m.ModuleCode);
   expect([first, second]).toEqual(['CS1010S', 'CS3216']);
 });
 
-test('it sorts by prefering module code', () => {
+test('sorts by preferring module code', () => {
   const [one, two, three] = sortModules('10', moduleList).map(m => m.ModuleCode);
   expect([one, two, three]).toEqual(['BFS1001', 'CS1010S', 'GES1021']);
 });
 
-test('it sorts by prefering title second', () => {
+test('sorts by preferring title second', () => {
   const [first] = sortModules('managerial', moduleList).map(m => m.ModuleCode);
   expect(first).toBe('ACC2002');
 });
