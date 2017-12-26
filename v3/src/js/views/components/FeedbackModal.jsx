0d123b13b64a25c417044df65e077996b89a81d4
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-12-26 09:29:22 +0800

    Improve modal styling and code

--- v3/src/js/views/components/FeedbackModal.jsx
+++ v3/src/js/views/components/FeedbackModal.jsx
@@ -5,8 +5,9 @@ import { connect } from 'react-redux';
 
 import type { State } from 'reducers';
 
+import config from 'config';
 import { toggleFeedback } from 'actions/app';
-import { Heart, GitHub, MessageCircle, Mail } from './icons';
+import { Heart, GitHub, Facebook, Mail } from './icons';
 import CloseButton from './CloseButton';
 import Modal from './Modal';
 import styles from './FeedbackModal.scss';
@@ -19,34 +20,43 @@ type Props = {
 export class FeedbackModalComponent extends PureComponent<Props> {
   render() {
     return (
-      <Modal isOpen={this.props.isOpen} onRequestClose={this.props.toggleFeedback}>
+      <Modal
+        isOpen={this.props.isOpen}
+        onRequestClose={this.props.toggleFeedback}
+        className={styles.modal}
+      >
         <CloseButton onClick={this.props.toggleFeedback} />
         <div className={styles.content}>
           <Heart className={styles.topIcon} />
-          <h1>Tell us what you think</h1>
+          <h1>Talk to us!</h1>
           <p>Thank you for your time! You can talk to us on Messenger, file
             an issue on GitHub, or fill up a feedback form</p>
 
           <div className={styles.links}>
             <a
               className={styles.messenger}
-              href="https://www.m.me/nusmods"
+              href={config.contact.facebook}
               target="_blank"
               rel="noreferrer noopener"
             >
-              <MessageCircle />
+              <Facebook />
               Messenger
             </a>
             <a
               className={styles.github}
-              href="https://github.com/nusmodifications/nusmods/issues"
+              href={config.contact.githubRepo}
               target="_blank"
               rel="noreferrer noopener"
             >
               <GitHub />
               GitHub
             </a>
-            <a className={styles.form} href="">
+            <a
+              className={styles.form}
+              href=""
+              target="_blank"
+              rel="noreferrer noopener"
+            >
               <Mail />
               Feedback Form
             </a>
