a6f10642d229167d85d875a7380e7891382b9534
Yangshun Tay <tay.yang.shun@gmail.com>
2017-12-27 19:30:10 -0800

    Update Travis

--- ci.sh
+++ ci.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 
-if [ $PROJECT == "www" ]
+if [ $PROJECT == "api" ]
+  then
+    echo "Running tests for api"
+    cd api && yarn install
+    npm run ci
+elif [ $PROJECT == "www" ]
   then
     echo "Running tests for www"
     cd www && yarn install
