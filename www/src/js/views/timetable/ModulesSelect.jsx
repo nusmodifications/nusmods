1f8bf34a4c90e0a6624a4b81912020aa4a0ccdd0
Zhang Yi Jiang <mediumdeviation@gmail.com>
2017-12-28 20:08:15 +0800

    Add search result highlighting

--- www/src/js/views/timetable/ModulesSelect.jsx
+++ www/src/js/views/timetable/ModulesSelect.jsx
@@ -5,8 +5,9 @@ import Downshift from 'downshift';
 import classnames from 'classnames';
 
 import type { ModuleSelectList } from 'types/reducers';
-import { createSearchPredicate, sortModules } from 'utils/moduleSearch';
+import { createSearchPredicate, sortModules, tokenize } from 'utils/moduleSearch';
 import { breakpointUp } from 'utils/css';
+import { highlight } from 'utils/react';
 import makeResponsive from 'views/hocs/makeResponsive';
 import Modal from 'views/components/Modal';
 import CloseButton from 'views/components/CloseButton';
@@ -26,7 +27,7 @@ type State = {
   isOpen: boolean,
 };
 
-const RESULTS_LIMIT = 500;
+const RESULTS_LIMIT = 400;
 
 class ModulesSelect extends Component<Props, State> {
   input: ?HTMLInputElement;
@@ -47,13 +48,13 @@ class ModulesSelect extends Component<Props, State> {
 
   getFilteredModules = (inputValue: string) => {
     if (!inputValue) {
-      return [];
+      return [[], []];
     }
 
     const predicate = createSearchPredicate(inputValue);
     const results = this.props.moduleList.filter(predicate);
 
-    return sortModules(inputValue, results.slice(0, RESULTS_LIMIT));
+    return [sortModules(inputValue, results.slice(0, RESULTS_LIMIT)), tokenize(inputValue)];
   };
 
   // downshift attaches label for us; autofocus only applies to modal
@@ -69,9 +70,10 @@ class ModulesSelect extends Component<Props, State> {
   }: any) => {
     const { placeholder, disabled } = this.props;
     const { isModalOpen } = this.state;
-    const results = this.getFilteredModules(inputValue);
+    const [results, highlightTokens] = this.getFilteredModules(inputValue);
     const showResults = isOpen && results.length > 0;
     const showTip = isModalOpen ? !results.length : isOpen && !results.length;
+
     return (
       <div className={styles.container}>
         <label className="sr-only" {...getLabelProps()}>
@@ -103,7 +105,9 @@ class ModulesSelect extends Component<Props, State> {
                       [styles.optionSelected]: highlightedIndex === index,
                     })}
                   >
-                    {`${module.ModuleCode} ${module.ModuleTitle}`}
+                    <span>
+                      {highlight(`${module.ModuleCode} ${module.ModuleTitle}`, highlightTokens)}
+                    </span>
                     <div>
                       <span className="badge badge-info">Added</span>
                     </div>
@@ -119,7 +123,9 @@ class ModulesSelect extends Component<Props, State> {
                       [styles.optionSelected]: highlightedIndex === index,
                     })}
                   >
-                    {`${module.ModuleCode} ${module.ModuleTitle}`}
+                    <span>
+                      {highlight(`${module.ModuleCode} ${module.ModuleTitle}`, highlightTokens)}
+                    </span>
                   </li>
                 ),
             )}
