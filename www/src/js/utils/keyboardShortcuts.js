5b5758dbf253bc0e099a74f441a1db7589222055
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-03 10:21:34 +0800

    Reduce timeout and debounce calls to openNotification

--- www/src/js/utils/keyboardShortcuts.js
+++ www/src/js/utils/keyboardShortcuts.js
@@ -2,6 +2,7 @@
 
 import type { Store } from 'redux';
 import Mousetrap from 'mousetrap';
+import { debounce } from 'lodash';
 
 import type { State } from 'reducers';
 import themes from 'data/themes.json';
@@ -9,6 +10,8 @@ import { DARK_MODE } from 'types/settings';
 import { toggleMode } from 'actions/settings';
 import { cycleTheme } from 'actions/theme';
 import { openNotification } from 'actions/app';
+
+const THEME_NOTIFICATION_TIMEOUT = 1000;
 
 export default function initKeyboardShortcuts(store: Store<State, *, *>) {
   // Toggle night mode
@@ -20,14 +23,22 @@ export default function initKeyboardShortcuts(store: Store<State, *, *>) {
   });
 
   // Cycle through themes
-  function notifyThemeChange() {
-    const themeId = store.getState().theme.id;
-    const theme = themes.find((t) => t.id === themeId);
+  const notifyThemeChange = debounce(
+    () => {
+      const themeId = store.getState().theme.id;
+      const theme = themes.find((t) => t.id === themeId);
 
-    if (theme) {
-      store.dispatch(openNotification(`Theme switched to ${theme.name}`));
-    }
-  }
+      if (theme) {
+        store.dispatch(
+          openNotification(`Theme switched to ${theme.name}`, {
+            timeout: THEME_NOTIFICATION_TIMEOUT,
+          }),
+        );
+      }
+    },
+    THEME_NOTIFICATION_TIMEOUT,
+    { leading: true },
+  );
 
   Mousetrap.bind('z', () => {
     store.dispatch(cycleTheme(-1));
