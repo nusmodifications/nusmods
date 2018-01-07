9a000e24aef6414768b6fa46453052381fbdcfaf
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-07 12:24:30 +0800

    Use flexbox row-wrapped to style Venues listing

--- www/src/js/views/venues/VenueList.jsx
+++ www/src/js/views/venues/VenueList.jsx
@@ -1,41 +1,49 @@
 // @flow
 import React from 'react';
-import VenueDetailRow from 'views/venues/VenueDetailRow';
+import classnames from 'classnames';
+import { entries, map, groupBy } from 'lodash';
 
+import type { OnSelectVenue } from 'types/views';
 import type { Venue, VenueInfo } from 'types/venues';
 
 import styles from './VenueList.scss';
 
 type Props = {
   venues: VenueInfo,
-  expandedVenue: Venue,
-  onSelect: (Venue, string, HTMLElement) => void, // Called with venue name and venue URL (/venues/<venue>)
+  selectedVenue: ?Venue,
+  onSelect: OnSelectVenue, // Called with venue name and venue URL (/venues/<venue>)
 };
 
 // $FlowFixMe: Flow doesn't have Intl typedefs https://github.com/facebook/flow/issues/1270
 const collator = new Intl.Collator('en', { sensitivity: 'base', numeric: true });
 
 export default function VenueList(props: Props) {
-  const rowRefs: { [Venue]: HTMLElement } = {};
-  const { venues, expandedVenue, onSelect } = props;
-  const lowercaseExpandedVenue = expandedVenue.toLowerCase();
-
   // Case-insensitive, natural sort of venue names
-  const sortedVenueNames = Object.keys(venues).sort(collator.compare);
+  const sortedVenues = entries(props.venues).sort(([a], [b]) => collator.compare(a, b));
+  const venueListPages = groupBy(sortedVenues, ([venue]) => venue.charAt(0).toUpperCase());
 
   return (
     <ul className={styles.venueList}>
-      {sortedVenueNames.map((name) => (
-        <VenueDetailRow
-          key={name}
-          name={name}
-          availability={venues[name]}
-          expanded={name.toLowerCase() === lowercaseExpandedVenue}
-          rootElementRef={(row) => {
-            if (row) rowRefs[name] = row;
-          }}
-          onClick={(selectedVenue, venueURL) => onSelect(selectedVenue, venueURL, rowRefs[name])}
-        />
+      {map(venueListPages, (venues, heading) => (
+        <li key={heading}>
+          <h3 className={styles.heading}>{heading}</h3>
+
+          <ul className={styles.subList}>
+            {venues.map(([venue]) => (
+              <li key={venue}>
+                <button
+                  type="button"
+                  className={classnames('btn btn-link', {
+                    [styles.selected]: venue === props.selectedVenue,
+                  })}
+                  onClick={() => props.onSelect(venue)}
+                >
+                  {venue}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </li>
       ))}
     </ul>
   );
