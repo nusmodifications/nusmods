f5fa957850bd3285b2bc91efa563a3711fc1c4a6
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-09 07:16:41 +0800

    Fix conflict between CSS modules and postcss-custom-properties

--- www/.postcssrc.js
+++ www/.postcssrc.js
@@ -1,9 +1,12 @@
+/* eslint-disable global-require, import/no-extraneous-dependencies */
 module.exports = {
-  plugins: {
-    autoprefixer: {},
-    "postcss-custom-properties": {
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-custom-properties')({
       preserve: true, // Preserve the original CSS variable declaration
       warnings: false, // Ignore warnings about variables declared on non-:root selectors
-    }
-  }
+    }),
+    // Custom plugin used to remove extra ':root' rules
+    require('./scripts/postcss-single-root-plugin'),
+  ],
 };
