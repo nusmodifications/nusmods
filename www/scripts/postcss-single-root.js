d7a86342bf2a3e9533e912659ebf316536ca45c1
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-09 10:47:28 +0800

    Minor tweaks

--- www/scripts/postcss-single-root.js
+++ www/scripts/postcss-single-root.js
@@ -18,16 +18,17 @@ const rootSelectors = [':root', 'body.mode-dark'];
 module.exports = postcss.plugin('postcss-single-root', () => (css) => {
   let isMainCSS = false;
 
-  // eslint-disable-next-line consistent-return
   css.walkComments((comment) => {
     // Because PostCSS is run before css-loader, this plugin will receive a new
     // CSS object for every CSS module. This means that it needs to identify
     // which is the 'main.scss' file which it can then ignore so that it won't
     // drop the rules from the 'real' css-variables.scss file
-    if (comment.text.includes('single-root-entry')) {
+    if (comment.text.includes('@postcss-single-root-entry')) {
       isMainCSS = true;
       return false;
     }
+
+    return true;
   });
 
   if (isMainCSS) return;
