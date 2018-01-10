85b838dc4f4f4ee02d3c14ec6c44c3ffc280428e
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-10 20:58:02 +0800

    Tighten FSA and reducer types

--- www/src/js/types/redux.js
+++ www/src/js/types/redux.js
@@ -3,11 +3,11 @@
 import type { State } from 'reducers';
 
 // Flux Standard Action: https://github.com/acdlite/flux-standard-action
-export type FSA = {
+export type FSA = {|
   type: string,
   payload: any,
   meta?: any,
   error?: boolean,
-};
+|};
 
 export type GetState = () => State;
