e6718b8d3a84ab65fbcfd49e1d6aecd6cc47ecab
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-10 19:46:28 +0800

    Fix Raven middleware stateTransformer

--- www/src/js/middlewares/raven-middleware.js
+++ www/src/js/middlewares/raven-middleware.js
@@ -5,18 +5,14 @@ import createRavenMiddleware from 'raven-for-redux';
 
 import type { State } from 'reducers/index';
 import type { ModuleBank } from 'reducers/moduleBank';
-
-function stringifyModuleBank(moduleBank: ModuleBank): string {
-  return `${moduleBank.moduleList.length} modules`;
-}
+import type { VenueBank } from 'reducers/venueBank';
 
 export default createRavenMiddleware(Raven, {
   stateTransformer(state: State) {
     return {
       ...state,
-      entities: {
-        moduleBank: stringifyModuleBank(state.moduleBank),
-      },
+      moduleBank: (moduleBank: ModuleBank): string => `${moduleBank.moduleList.length} modules`,
+      venueBank: (venueBank: VenueBank): string => `${venueBank.venueList.length} venues`,
     };
   },
 });
