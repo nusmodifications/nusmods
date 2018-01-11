30e2eea1e4d013c80e6f6795c54ee408ba4f2635
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-11 09:13:47 +0800

    Properly fix raven-middleware

--- www/src/js/middlewares/raven-middleware.js
+++ www/src/js/middlewares/raven-middleware.js
@@ -4,15 +4,13 @@ import Raven from 'raven-js';
 import createRavenMiddleware from 'raven-for-redux';
 
 import type { State } from 'reducers/index';
-import type { ModuleBank } from 'reducers/moduleBank';
-import type { VenueBank } from 'reducers/venueBank';
 
 export default createRavenMiddleware(Raven, {
   stateTransformer(state: State) {
     return {
       ...state,
-      moduleBank: (moduleBank: ModuleBank): string => `${moduleBank.moduleList.length} modules`,
-      venueBank: (venueBank: VenueBank): string => `${venueBank.venueList.length} venues`,
+      moduleBank: `${state.moduleBank.moduleList.length} modules`,
+      venueBank: `${state.venueBank.venueList.length} venues`,
     };
   },
 });
