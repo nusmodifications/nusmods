f52a2449b29ec8ce2ac11cd91eac09808cc4856c
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-12 19:27:42 +0800

    Add localstorage size check to see if we're actually hitting quota

--- www/src/js/storage/index.js
+++ www/src/js/storage/index.js
@@ -11,7 +11,25 @@ function setItem(key: string, value: any) {
   try {
     localStorage.setItem(key, isString(value) ? value : JSON.stringify(value));
   } catch (e) {
-    Raven.captureException(e);
+    // Calculate used size and attach it to the error report. This is diagnostics
+    // for https://sentry.io/nusmods/v3/issues/432778991/
+    let usedSpace = 0;
+    try {
+      for (let i = 0; i < localStorage.length; i++) {
+        const k = localStorage.key(i);
+        let item;
+        if (typeof k === 'string') item = localStorage.getItem(k);
+        if (item) usedSpace += item.length;
+      }
+    } catch (error) {
+      // Ignore error
+    }
+
+    Raven.captureException(e, {
+      data: {
+        usedSpace,
+      },
+    });
   }
 }
 
