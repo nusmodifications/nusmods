9ba62bdf1044ab8af872ac8ddc76f288e1e32cb2
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-01-17 10:37:09 +0800

    Venues page bug fixes (#728)
    * Fix availability search default options
    
    * Use CSS grid for browsers that support it
    
    * Fix border colors for hover and focus state
    
    * Reuse timify functions
    
    * Switch to btn-outline-primary without border and add grid-gap
    
    * Fix timezones for tests
    
    * Fix comments and button active state

---
+++ www/src/js/views/venues/AvailabilitySearch.test.jsx
@@ -0,0 +1,37 @@
+import { defaultSearchOptions } from 'views/venues/AvailabilitySearch';
+
+describe('defaultSearchOptions', () => {
+  test('should the nearest slots during school hours', () => {
+    // Monday
+    expect(defaultSearchOptions(new Date('2018-01-15T12:30:00'))).toMatchObject({
+      time: 12,
+      day: 0,
+    });
+
+    // Saturday
+    expect(defaultSearchOptions(new Date('2018-01-20T08:12:00'))).toMatchObject({
+      time: 8,
+      day: 5,
+    });
+  });
+
+  test("should set time to something within school hours if we're outside it", () => {
+    // 6am - before school starts
+    expect(defaultSearchOptions(new Date('2018-01-15T06:12:00'))).toMatchObject({
+      time: 8,
+      day: 0,
+    });
+
+    // 1am - after school ends
+    expect(defaultSearchOptions(new Date('2018-01-16T01:12:00'))).toMatchObject({
+      time: 8,
+      day: 1,
+    });
+
+    // Sunday
+    expect(defaultSearchOptions(new Date('2018-01-21T09:12:00'))).toMatchObject({
+      time: 9,
+      day: 0,
+    });
+  });
+});
