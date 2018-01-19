bb2880e95e20d1907b9a52dca7f9e416a8e56fda
E-Liang Tan <eliang@eliangtan.com>
2018-01-19 18:10:00 +0800

    Reword some FAQs

--- www/src/js/views/static/FaqContainer.jsx
+++ www/src/js/views/static/FaqContainer.jsx
@@ -11,38 +11,36 @@ export default function FaqContainer() {
     <StaticPage title="FAQ">
       <h2>Frequently Asked Questions</h2>
       <hr />
-      <div className={styles.question}>
-        <p>
-          Hi there! Before contacting us, please read the following FAQ (Frequently Asked Questions)
-          first. In most cases, you <strong>DO NOT</strong> need to contact us.
-        </p>
-      </div>
+      <p>
+        Hi there! Before contacting us, please read the following FAQ (Frequently Asked Questions)
+        first. In most cases, you <strong>DO NOT</strong> need to contact us.
+      </p>
 
       <div className={styles.question}>
         <h5>NUSMods is cool, where do you guys come from?</h5>
         <p>
-          NUSMods is a student-run initiative and does not have any affiliations with the National
-          University of Singapore. NUSMods was born out of the frustration of a lack of usable NUS
-          timetable planners (the official one makes anyone who uses it want to pull their hair
-          out). It seems like NUS does not have any intentions to improve it anytime soon anyway.
-          Since most of us come from School of Computing, we decided to put our technical skills to
-          good use to create something that will make your lives as students easier and better.
+          NUSMods is a student-run initiative and is not affiliated with the National University of
+          Singapore. NUSMods was born out of the frustration of a lack of usable NUS timetable
+          planners (<a href="https://webrb.nus.edu.sg/ctt/builder.aspx">the official one</a>{' '}
+          provides a delightful hair-pulling experience &ndash; go ahead, try it out). Also, it
+          seems like NUS does not intend to improve it anytime soon. Since most of us come from the
+          School of Computing, we decided to put our technical skills to use, creating something
+          that will make your lives easier and better.
         </p>
-
         <p>
-          NUSMods does not get to make decisions regarding your curriculum, module availablity and
-          module timetable. If you have questions regarding your curriculum, CORS bidding, IVLE or
-          anything unrelated to NUSMods, maybe it would be better to contact your faculty and
+          NUSMods does not make decisions regarding your curriculum, module availability and module
+          timetable. If you have questions regarding your curriculum, CORS bidding, IVLE, or
+          anything unrelated to NUSMods, it would be better to either contact your faculty and
           department or just <a href="https://www.google.com/">Google</a> it.
         </p>
       </div>
 
-      <div className={styles.question}>
+      <div className={styles.question} id="wait-but-when">
         <h5>When will semester X data be available?</h5>
         <p>
-          We do not own nor come up with any of the modules data shown here. They are all obtained
-          from CORS. As soon as the data becomes available, we will reflect it here within a day or
-          two by using the latest blockchain technology.
+          All module data shown in NUSMods is obtained from CORS. As soon as the data becomes
+          available, we will reflect it here within a day or two by using the latest blockchain
+          technology.
         </p>
       </div>
 
@@ -51,7 +49,9 @@ export default function FaqContainer() {
           The module NM2220 is on the CORS timetable for semester 2 but it is not on NUSMods. I hope
           you can update this.
         </h5>
-        <p>Refer to the answer for the previous question.</p>
+        <p>
+          Refer to <a href="#wait-but-when">the previous answer</a>.
+        </p>
       </div>
 
       <div className={styles.question}>
@@ -60,21 +60,21 @@ export default function FaqContainer() {
           on NUSMods there is only one lecture slot.
         </h5>
         <p>
-          Yes, there is a mistake, but in most cases, it is a mistake with <em>your faculty</em>.
-          Many faculties (FASS in particular) like to maintain their own module timetable schedule
-          on their own faculty website, without updating the official school data sources such as
-          CORS and IVLE. NUSMods obtains timetable data automatically from CORS and IVLE, and the
-          only way for the updated data to be reflected on NUSMods is to have CORS and IVLE updated.
+          Yes, there is a mistake, but in most cases, <em>your faculty</em> made the mistake. Many
+          faculties (FASS in particular) maintain their module timetable schedule on their faculty
+          website, without updating the official school data sources such as CORS and IVLE. NUSMods
+          obtains timetable data automatically from CORS and IVLE, and the only way for the updated
+          data to be reflected on NUSMods is to have CORS and IVLE updated.
         </p>
         <p>
           Check <a href="https://myaces.nus.edu.sg/cors/jsp/report/ModuleInfoListing.jsp">CORS</a>{' '}
-          for the official timetable data (and see if that module even exists in CORS). Only if it
-          differs from NUSMods, then report it to NUSMods, else, kindly inform your faculty to
-          update the official sources and NUSMods will reflect the updates respectively. If CORS is
-          updated while NUSMods is not, give it a day or two for NUSMods to update it via our
-          automated state-of-the-art artificial intelligence algorithms and machine learning models.
-          If NUSMods is still not updated after two days, then there might be a problem. Please
-          contact us and we will look into it.
+          for the official timetable data (and see if that module even exists in CORS). If it
+          differs from NUSMods, do report it to us. Otherwise, inform your faculty to update the
+          official sources and NUSMods will reflect the updates in time. If CORS is updated while
+          NUSMods is not, give it a day or two for NUSMods to update it via our automated
+          state-of-the-art artificial intelligence algorithms and machine learning models. If
+          NUSMods is still not updated after two days, then there might be a problem. Please contact
+          us and we will look into it.
         </p>
       </div>
 
@@ -83,7 +83,7 @@ export default function FaqContainer() {
           On my faculty website, the exam date for PS3242 is different from the one shown on
           NUSMods. It should be on 28th Nov and not 24th Nov.
         </h5>
-        <p>Refer to the answer for the previous question.</p>
+        <p>Refer to the previous answer.</p>
       </div>
 
       <div className={styles.question}>
@@ -91,12 +91,12 @@ export default function FaqContainer() {
           I can add IS1112 into my list of modules but why are there no lectures or tutorial slots
           after adding them?
         </h5>
-        <p>Refer to the answer for the previous question.</p>
+        <p>Refer to the previous answer.</p>
       </div>
 
       <div className={styles.question}>
         <h5>SE5221 cannot be found on your website, can you update your system?</h5>
-        <p>Refer to the answer for the previous question.</p>
+        <p>Refer to the previous answer.</p>
       </div>
 
       <div className={styles.question}>
@@ -104,21 +104,19 @@ export default function FaqContainer() {
           SC3202 is indicated as only available in Sem 1 in NUSMods but the FASS module list shows
           it as available in Sem 2 too?
         </h5>
-        <p>Refer to the answer for the previous question.</p>
+        <p>Refer to the previous answer.</p>
       </div>
 
       <div className={styles.question}>
         <h5>Why is ... ?</h5>
-        <p>
-          Before we hear the rest of your question, refer to the answer for the previous question.
-        </p>
+        <p>Before we hear the rest of your question, refer to the previous answer.</p>
       </div>
 
       <hr />
 
       <p>
-        Congratulations for making it to the end! If you are still bent on contacting us, you may
-        reach us via email at nusmods&#123;at&#125;googlegroups[dot]com or via{' '}
+        Congratulations for making it to the end! If you still want to contact us, you may reach us
+        via email at nusmods&#123;at&#125;googlegroups[dot]com or via{' '}
         <a href={config.contact.messenger} target="_blank" rel="noopener noreferrer">
           Messenger
         </a>. Please allow up to 90 working days for a reply.
