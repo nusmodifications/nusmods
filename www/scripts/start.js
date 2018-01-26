e37d8ce685f3f5cbff79de470c379c03245739d9
E-Liang Tan <eliang@eliangtan.com>
2018-01-26 14:02:00 +0800

    Enable HMR over network (#766)
    * Enable HMR on networked devices
    
    * Change default export URL for consistency
    
    * Add default export port and proxy to dev server host
    
    * Enable HMR on forwarded ports
    
    Removed the hardcoded port 8080 requirement, so that HMR works if you forward
    a different Internet-facing port (say 1234) to port 8080 on your host.
    
    This has also been tested on localhost.
    
    * Disable browser opening of timetable export view

--- www/scripts/start.js
+++ www/scripts/start.js
@@ -10,14 +10,14 @@ const developmentConfig = require(CONFIG_FILE); // eslint-disable-line import/no
 
 // If you use Docker, Vagrant or Cloud9, set
 // host: options.host || '0.0.0.0';
-//
-// 0.0.0.0 is available to all network devices
-// unlike default `localhost`.
+// 0.0.0.0 is available to all network devices unlike `localhost`.
+const DEFAULT_HOST = process.env.HOST || '0.0.0.0';
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 8080;
-const DEFAULT_HOST = process.env.HOST || 'localhost';
+const DEFAULT_EXPORT_PORT = parseInt(process.env.EXPORT_PORT, 10) || 3000;
 const PROTOCOL = process.env.HTTPS === 'true' ? 'https' : 'http';
+const OPEN_BROWSER = process.env.OPEN_BROWSER !== '0'; // Open browser unless told otherwise
 
-function runDevServer(host, port, protocol) {
+function runDevServer(host, port, exportPort, protocol, shouldOpenBrowser) {
   const compiler = Webpack(developmentConfig);
   const devServer = new WebpackDevServer(compiler, {
     // Enable history API fallback so HTML5 History API based
@@ -46,8 +46,6 @@ function runDevServer(host, port, protocol) {
       performance: false,
     },
     https: protocol === 'https',
-    host,
-    port,
 
     proxy: {
       // Proxy the short_url.php endpoint because it does not support CORS
@@ -58,7 +56,7 @@ function runDevServer(host, port, protocol) {
 
       // Proxy export endpoints to the local version for development
       '/export': {
-        target: 'http://localhost:3000',
+        target: `http://${host}:${exportPort}`,
         pathRewrite: {
           '^/export': '',
         },
@@ -67,7 +65,7 @@ function runDevServer(host, port, protocol) {
   });
 
   // Launch WebpackDevServer.
-  devServer.listen(port, (err) => {
+  devServer.listen(port, host, (err) => {
     if (err) {
       console.log(err);
       return;
@@ -76,8 +74,10 @@ function runDevServer(host, port, protocol) {
     console.log(chalk.cyan('Starting the development server...'));
     console.log();
 
-    openBrowser(`${protocol}://${host}:${port}/`);
+    if (shouldOpenBrowser) {
+      openBrowser(`${protocol}://${host}:${port}/`);
+    }
   });
 }
 
-runDevServer(DEFAULT_HOST, DEFAULT_PORT, PROTOCOL);
+runDevServer(DEFAULT_HOST, DEFAULT_PORT, DEFAULT_EXPORT_PORT, PROTOCOL, OPEN_BROWSER);
