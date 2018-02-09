0326827c98e590b73ade8ba3e5b651a736a600c5
E-Liang Tan <eliang@eliangtan.com>
2018-02-09 10:47:15 +0800

    Minor cleanup of sync-middleware

--- www/src/js/middlewares/sync-middleware.js
+++ www/src/js/middlewares/sync-middleware.js
@@ -7,13 +7,15 @@ import { syncDataReceived } from 'actions/sync';
 const SYNC_COLLECTION_NAME = 'mods';
 
 export type SyncConfig = {
-  actions: string[],
-  keyPaths?: string[],
+  actions: string[], // Action types to watch for
+  keyPaths?: string[], // Key paths to send to server. If absent, sends entire reducer state
 };
+
+export type PerReducerSyncConfig = { [string]: SyncConfig };
 
 // Returns a map of action types to reducer names.
 // No two reducers should watch for the same action.
-export function mapActionsToReducers(config: { [string]: SyncConfig }): { [string]: string } {
+export function mapActionsToReducers(config: PerReducerSyncConfig): { [string]: string } {
   return fromPairs(
     flatten(
       values(
@@ -26,8 +28,8 @@ export function mapActionsToReducers(config: { [string]: SyncConfig }): { [strin
   );
 }
 
-export default function createSyncMiddleware(perReducerConfig: { [string]: SyncConfig }) {
-  let unsubscribeSync; // Function to stop subscribing to Firebase update snapshots
+export default function createSyncMiddleware(perReducerConfig: PerReducerSyncConfig) {
+  let unsubscribeSync: ?Function; // Function to stop subscribing to Firebase update snapshots
   const actionToReducerMap = mapActionsToReducers(perReducerConfig);
 
   const syncMiddleware: Middleware<*, *, *> = (store) => {
@@ -37,12 +39,18 @@ export default function createSyncMiddleware(perReducerConfig: { [string]: SyncC
         unsubscribeSync = firestore
           .collection(SYNC_COLLECTION_NAME)
           .doc(user.uid)
-          .onSnapshot((doc) => {
-            // Ignore changes that originated locally
-            // https://firebase.google.com/docs/firestore/query-data/listen#events-local-changes
-            if (doc.metadata.hasPendingWrites) return;
-            store.dispatch(syncDataReceived(doc.data()));
-          });
+          .onSnapshot(
+            (doc) => {
+              // Ignore docs that don't exist
+              if (!doc.exists) return;
+              // Ignore changes that originated locally
+              // https://firebase.google.com/docs/firestore/query-data/listen#events-local-changes
+              if (doc.metadata.hasPendingWrites) return;
+              store.dispatch(syncDataReceived(doc.data()));
+            },
+            // TODO: Handle errors properly
+            (err) => alert(`Subscription error ${err}`),
+          );
       } else {
         // No user is signed in.
         // eslint-disable-next-line no-lonely-if
@@ -67,9 +75,10 @@ export default function createSyncMiddleware(perReducerConfig: { [string]: SyncC
         firestore
           .collection(SYNC_COLLECTION_NAME)
           .doc(loggedInUser.uid)
-          .set({ [reducerName]: stateToSend }, { merge: true });
-        // .then(() => console.log("My god Jim, we've synced@!", stateToSend));
-        // TODO: Handle errors
+          .set({ [reducerName]: stateToSend }, { merge: true })
+          // .then(() => console.log("My god Jim, we've synced@!", stateToSend));
+          // TODO: Handle errors properly
+          .catch((err) => alert(`Send error ${err}`));
       }
 
       return result;
