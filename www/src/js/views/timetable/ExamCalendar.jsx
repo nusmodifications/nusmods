864d26decf71707c8b9c23c54f88c0cb57b57fec
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-02-20 16:42:21 +0800

    Refactor ExamCalendar's rendering code

--- www/src/js/views/timetable/ExamCalendar.jsx
+++ www/src/js/views/timetable/ExamCalendar.jsx
@@ -29,13 +29,6 @@ type ModuleWithExamTime = {
   timeSegment: TimeSegment,
 };
 
-type ExamDay = {
-  date: Date,
-  groupedModules: {
-    [TimeSegment]: ModuleWithExamTime[],
-  },
-};
-
 const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 // The API returns examDate with hhmm as the TZ specifier, but we want this to work
@@ -44,9 +37,8 @@ function examDateStringToDate(date: string): Date {
   return new Date(`${date.slice(0, 16)}Z`);
 }
 
-function getExamDate(date: ?string): ?string {
-  if (!date) return null;
-  return formatExamDate(date).split(' ')[0];
+function getExamDate(date: Date): string {
+  return formatExamDate(date.toISOString()).split(' ')[0];
 }
 
 // NUS exams are grouped into morning, afternoon and evening exams. Afternoon exams happen at 2.30PM
@@ -78,12 +70,24 @@ function ExamModule({ module }: { module: ModuleWithColor }) {
   );
 }
 
-function ExamWeek({ week, weekNumber }: { week: ExamDay[], weekNumber: number }) {
-  // Each week consists of a header row and three module rows, one for each
-  // possible time segment
+function ExamWeek({
+  modules,
+  weekNumber,
+  firstDayOfExams,
+  days,
+}: {
+  modules: { [string]: ModuleWithExamTime[] },
+  weekNumber: number,
+  firstDayOfExams: Date,
+  days: number,
+}) {
+  // Array of dates to display
+  const dayDates = range(days).map((offset) => daysAfter(firstDayOfExams, weekNumber * 7 + offset));
+
+  // Each week consists of a header row and three module rows, one for each possible time segment
   const headerRow = (
     <tr>
-      {week.map(({ date }, dayNumber) => {
+      {dayDates.map((date, dayNumber) => {
         // Format the date number for each day of the exam. We avoid repeating
         // the month name unnecessarily by only showing it for the very first cell,
         // and when the month changes
@@ -103,15 +107,19 @@ function ExamWeek({ week, weekNumber }: { week: ExamDay[], weekNumber: number })
 
   const moduleRows = TIME_SEGMENTS.map((timeSegment) => (
     <tr key={timeSegment}>
-      {week.map(({ groupedModules }, dayNumber) => {
-        const modules = groupedModules[timeSegment];
+      {dayDates.map((date) => {
+        // Get modules with exams on this day and time segment
+        const modulesOnThisDay = modules[getExamDate(date)] || [];
+        const modulesAtThisTime = modulesOnThisDay.filter(
+          (module) => module.timeSegment === timeSegment,
+        );
 
         return (
-          <td className={styles.day} key={dayNumber}>
-            {modules && (
+          <td className={styles.day} key={date}>
+            {!!modulesAtThisTime.length && (
               <Fragment>
-                <h4>{modules[0].time}</h4>
-                {modules.map(({ module }) => (
+                <h4>{modulesAtThisTime[0].time}</h4>
+                {modulesAtThisTime.map(({ module }) => (
                   <ExamModule key={module.ModuleCode} module={module} />
                 ))}
               </Fragment>
@@ -166,12 +174,8 @@ export default class ExamCalendar extends PureComponent<Props> {
     return [firstDayOfExams, weekCount];
   }
 
-  groupModulesByExams() {
+  modulesWithExamDate() {
     const { semester } = this.props;
-    const [firstDayOfExams, weekCount] = this.getExamCalendar();
-
-    // If there are no modules with exams, then we can just stop here
-    if (weekCount === 0) return null;
 
     // Wrap each module with its exam date info. This means we don't have to recalculate these
     // every time we need them
@@ -192,34 +196,13 @@ export default class ExamCalendar extends PureComponent<Props> {
       });
     });
 
-    // Get the number of days of the week which have exams on them. Default to Monday to Friday
-    // (5 days), and expand as necessary
-    const daysWithExams = Math.max(
-      5,
-      ...modulesWithExams.map((module) => examDateStringToDate(module.dateTime).getUTCDay()),
-    );
-
-    // Group modules by their exam date and time
-    const modulesByExamDate = groupBy(modulesWithExams, (module) => module.date);
-    return range(weekCount).map((week) =>
-      // Group by days
-      range(daysWithExams).map((day) => {
-        const date = daysAfter(firstDayOfExams, week * 7 + day);
-        const modules = modulesByExamDate[getExamDate(date.toISOString())] || [];
-
-        // Group by time segment
-        return {
-          date,
-          groupedModules: groupBy(modules, (module: ModuleWithExamTime) => module.timeSegment),
-        };
-      }),
-    );
+    return modulesWithExams;
   }
 
   render() {
-    const modulesByExamDate = this.groupModulesByExams();
+    const [firstDayOfExams, weekCount] = this.getExamCalendar();
 
-    if (!modulesByExamDate) {
+    if (!weekCount) {
       return (
         <p className={styles.noExams}>
           You don&apos;t have any final exams this semester{' '}
@@ -230,29 +213,47 @@ export default class ExamCalendar extends PureComponent<Props> {
       );
     }
 
+    const modulesWithExams = this.modulesWithExamDate();
+
+    // Get the number of days of the week which have exams on them. Default to Monday to Friday
+    // (5 days), and expand as necessary
+    const daysWithExams = Math.max(
+      5,
+      ...modulesWithExams.map((module) => examDateStringToDate(module.dateTime).getUTCDay()),
+    );
+
+    const modulesByExamDate = groupBy(modulesWithExams, (module) => module.date);
+
     // The table consists of the following <tr>s
     // - Day of the week (Mon - Sat)
-    // - Exam date (1 - 31)
-    // - Morning exams
-    // - Afternoon exams
-    // - Evening exams
-    // - Repeat the above four rows for every week
+    // - For each week:
+    //   - Exam date (1 - 31)
+    //   - Morning exams
+    //   - Afternoon exams
+    //   - Evening exams
     return (
       <Fragment>
         <div className="scrollable">
           <table className={styles.table}>
             <thead>
               <tr>
-                {modulesByExamDate[0].map((_, day) => (
+                {range(daysWithExams).map((day) => (
                   <th key={day} className={styles.dayName}>
                     {DaysOfWeek[day].slice(0, 3)}
                   </th>
                 ))}
               </tr>
             </thead>
+
             <tbody>
-              {modulesByExamDate.map((week, index) => (
-                <ExamWeek key={index} week={week} weekNumber={index} />
+              {range(weekCount).map((week) => (
+                <ExamWeek
+                  key={week}
+                  days={daysWithExams}
+                  weekNumber={week}
+                  firstDayOfExams={firstDayOfExams}
+                  modules={modulesByExamDate}
+                />
               ))}
             </tbody>
           </table>
