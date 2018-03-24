04a2bb6b03ed157c6f6253d037ba581a9761359b
E-Liang Tan <eliang@eliangtan.com>
2018-03-24 17:00:45 +0800

    Implement a maxInsertSize function

--- api/data/src/dbfy/venues.js
+++ api/data/src/dbfy/venues.js
@@ -5,20 +5,18 @@ import R from 'ramda';
 
 import config from '../../config';
 import { walkJsonDir } from '../util/walkDir';
+import { maxInsertSize } from '../util/db';
 
 const SCHOOL_ID = { school_id: 1 };
-// Prevent "Too many SQL variable errors thrown by SQLite"
-// Default by SQLite is 999. Since we have 4 variables per row...
-const MAX_INSERT_SIZE = Math.floor(999 / 4);
 
 export default async function populateVenues(db: Knex) {
-  const { dataFolder } = config;
-  const rawData = await walkJsonDir(dataFolder, 'venues.json', 2);
+  const { dataFolder, venuesFileName } = config;
+  const rawData = await walkJsonDir(dataFolder, venuesFileName, 2);
 
   const mapToRow = (venueName: string) => ({ ...SCHOOL_ID, name: venueName });
   const getVenues = R.compose(R.map(mapToRow), R.uniq, R.flatten, R.map(R.values), R.values);
   const venues = getVenues(rawData);
   // console.log(venues);
 
-  await db.batchInsert('venues', venues, MAX_INSERT_SIZE);
+  await db.batchInsert('venues', venues, maxInsertSize(4));
 }
