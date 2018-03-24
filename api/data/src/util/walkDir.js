ccf024e406f4f5e939c8d3863d9c7a1e1abb1bb8
E-Liang Tan <eliang@eliangtan.com>
2018-03-24 16:45:05 +0800

    Add depth param to walkJsonDir

--- api/data/src/util/walkDir.js
+++ api/data/src/util/walkDir.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs-extra';
+import { pickBy } from 'lodash';
 
 /**
  * Walks the directory, and returns all nested files of designated file name.
@@ -8,19 +9,26 @@ import fs from 'fs-extra';
  * @param {string} folderPath folder path to traverse
  * @param {string} destFileName file name to match
  */
-async function walkJsonDir(folderPath, destFileName) {
-  const folders = await fs.readdir(folderPath);
+async function walkJsonDir(folderPath, destFileName, depth) {
+  const folders = await fs.readdir(folderPath).catch(() => null);
+  if (!folders) return null;
+
   const folderToJsonMap = {};
   await Promise.all(
     folders.map(async (folder) => {
-      const filePath = path.join(folderPath, folder, destFileName);
-      const fileContent = await fs.readJson(filePath).catch(() => null);
-      if (fileContent) {
-        folderToJsonMap[folder] = fileContent;
+      if (depth > 1) {
+        const fPath = path.join(folderPath, folder);
+        folderToJsonMap[folder] = await walkJsonDir(fPath, destFileName, depth - 1);
+      } else {
+        const filePath = path.join(folderPath, folder, destFileName);
+        const fileContent = await fs.readJson(filePath).catch(() => null);
+        if (fileContent) {
+          folderToJsonMap[folder] = fileContent;
+        }
       }
     }),
   );
-  return folderToJsonMap;
+  return pickBy(folderToJsonMap);
 }
 
 /**
