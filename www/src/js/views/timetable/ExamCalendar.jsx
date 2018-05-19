160f7d610d524041ac7071d104c6c4097582708c
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-05-19 15:20:00 +0800

    Fix 6.30pm exam timing (#1011)

--- www/src/js/views/timetable/ExamCalendar.jsx
+++ www/src/js/views/timetable/ExamCalendar.jsx
@@ -52,6 +52,7 @@ function getTimeSegment(time: string): TimeSegment {
     case '2:30 PM':
       return 'Afternoon';
     case '5:00 PM':
+    case '6:30 PM':
       return 'Evening';
     default:
       throw new Error(`Unrecognized exam time: ${time}`);
