25235155b6bf4adcdfeeee87ef51c554793fcdf0
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-05-26 16:31:58 +0800

    Fix fs returning buffer instead of utf-8

--- www/src/js/ssr/data.js
+++ www/src/js/ssr/data.js
@@ -23,7 +23,7 @@ const axiosResponseToAction = (type: string) => (response: $AxiosXHR<*, *>) => (
 });
 
 const fsToAction = async (type: string, filePath: string) => {
-  const [content, stat] = await Promise.all([fs.readFile(filePath), fs.stat(filePath)]);
+  const [content, stat] = await Promise.all([fs.readFile(filePath, 'utf-8'), fs.stat(filePath)]);
 
   return {
     type: type + SUCCESS,
