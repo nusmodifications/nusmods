4b422655181779e96fcd030da0a78bee1fd4acf9
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-05-27 21:43:20 +0800

    Add help dialog

--- www/src/js/views/components/KeyboardShortcuts.jsx
+++ www/src/js/views/components/KeyboardShortcuts.jsx
@@ -1,30 +1,53 @@
 // @flow
-import { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { withRouter, type ContextRouter } from 'react-router-dom';
 import type { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import Mousetrap from 'mousetrap';
+import { groupBy, map } from 'lodash';
 
 import { DARK_MODE } from 'types/settings';
 import themes from 'data/themes.json';
 import { cycleTheme } from 'actions/theme';
 import { openNotification } from 'actions/app';
 import { toggleMode } from 'actions/settings';
-import type { State } from 'reducers';
+import type { State as StoreState } from 'reducers';
+import { intersperse } from 'utils/array';
+import Modal from './Modal';
+import styles from './KeyboardShortcuts.scss';
 
 type Props = ContextRouter & {
   dispatch: Dispatch<*>,
-  state: State,
+  state: StoreState,
+};
+
+type State = {
+  helpShown: boolean,
+};
+
+type Section = 'Appearance' | 'Navigation';
+const APPEARANCE: Section = 'Appearance';
+const NAVIGATION: Section = 'Navigation';
+
+type Shortcut = string | string[];
+type KeyBinding = {
+  key: Shortcut,
+  section: Section,
+  description: string,
 };
 
 const THEME_NOTIFICATION_TIMEOUT = 1000;
 
-class KeyboardShortcutsComponent extends PureComponent<Props> {
+class KeyboardShortcutsComponent extends PureComponent<Props, State> {
+  state = {
+    helpShown: false,
+  };
+
   componentDidMount() {
     const { state, dispatch } = this.props;
 
     // Toggle night mode
-    Mousetrap.bind('x', () => {
+    this.bind('x', APPEARANCE, 'Toggle Night Mode', () => {
       this.props.dispatch(toggleMode());
 
       dispatch(
@@ -35,15 +58,29 @@ class KeyboardShortcutsComponent extends PureComponent<Props> {
     });
 
     // Cycle through themes
-    Mousetrap.bind('z', () => {
+    this.bind('z', APPEARANCE, 'Previous Theme', () => {
       dispatch(cycleTheme(-1));
       this.notifyThemeChange();
     });
 
-    Mousetrap.bind('c', () => {
+    this.bind('c', APPEARANCE, 'Next Theme', () => {
       dispatch(cycleTheme(1));
       this.notifyThemeChange();
     });
+
+    this.bind('?', NAVIGATION, 'Show this help', () =>
+      this.setState({ helpShown: !this.state.helpShown }),
+    );
+  }
+
+  shortcuts: KeyBinding[] = [];
+
+  closeModal = () => this.setState({ helpShown: false });
+
+  bind(key: Shortcut, section: Section, description: string, action: () => void) {
+    this.shortcuts.push({ key, description, section });
+
+    Mousetrap.bind(key, action);
   }
 
   notifyThemeChange() {
@@ -58,6 +95,47 @@ class KeyboardShortcutsComponent extends PureComponent<Props> {
         }),
       );
     }
+  }
+
+  renderShortcut = (shortcut: Shortcut) => {
+    if (typeof shortcut === 'string') {
+      const capitalized = shortcut.replace(/\b([a-z])/, (c) => c.toUpperCase());
+      return <kbd key={shortcut}>{capitalized}</kbd>;
+    }
+
+    return intersperse(shortcut.map(this.renderShortcut), ' or ');
+  };
+
+  render() {
+    const sections = groupBy(this.shortcuts, (shortcut) => shortcut.section);
+
+    return (
+      <Modal
+        isOpen={this.state.helpShown}
+        onRequestClose={this.closeModal}
+        className={styles.modal}
+      >
+        <h2>Keyboard shortcuts</h2>
+
+        <table className="table table-sm">
+          {map(sections, (shortcuts, heading) => (
+            <tbody key={heading}>
+              <tr>
+                <th />
+                <th>{heading}</th>
+              </tr>
+
+              {shortcuts.map((shortcut) => (
+                <tr key={shortcut.key}>
+                  <td className={styles.key}>{this.renderShortcut(shortcut.key)}</td>
+                  <td>{shortcut.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          ))}
+        </table>
+      </Modal>
+    );
   }
 }
 
