122601dbf0e0979a430b50640c8b3b30b08e0263
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-07-27 22:14:32 +0800

    Add cross mark to indicate currently selected color

--- www/src/js/views/components/ColorPicker.jsx
+++ www/src/js/views/components/ColorPicker.jsx
@@ -23,32 +23,35 @@ type Props = {
  */
 class ColorPicker extends PureComponent<Props> {
   // TODO: Inject types from downshift when https://github.com/paypal/downshift/pull/180 is implemented
-  renderColorPicker = ({ getToggleButtonProps, getItemProps, isOpen }: any) => (
-    <div className={styles.container}>
-      <button
-        {...getToggleButtonProps({
-          title: this.props.label,
-        })}
-        className={classnames(
-          'btn btn-block hoverable',
-          `color-${this.props.color}`,
-          styles.moduleColor,
-          { [styles.hidden]: this.props.isHidden },
+  renderColorPicker = ({ getToggleButtonProps, getItemProps, isOpen }: any) => {
+    const { label, color, isHidden } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <button
+          {...getToggleButtonProps({
+            title: label,
+          })}
+          className={classnames('btn btn-block hoverable', `color-${color}`, styles.moduleColor, {
+            [styles.hidden]: isHidden,
+          })}
+        />
+        {isOpen && (
+          <div className={styles.palette}>
+            {_.range(NUM_DIFFERENT_COLORS).map((index: ColorIndex) => (
+              <button
+                {...getItemProps({ item: index })}
+                key={index}
+                className={classnames(styles.option, `color-${index}`, {
+                  [styles.selected]: index === color,
+                })}
+              />
+            ))}
+          </div>
         )}
-      />
-      {isOpen && (
-        <div className={styles.palette}>
-          {_.range(NUM_DIFFERENT_COLORS).map((index: ColorIndex) => (
-            <button
-              {...getItemProps({ item: index })}
-              key={index}
-              className={classnames(styles.option, `color-${index}`)}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  };
 
   render() {
     return <Downshift onChange={this.props.onChooseColor} render={this.renderColorPicker} />;
