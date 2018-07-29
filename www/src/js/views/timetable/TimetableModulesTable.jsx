37487453bfc269415e743c27ece4df57bdabfd45
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-07-29 12:29:14 +0800

    Add module table footer

--- www/src/js/views/timetable/TimetableModulesTable.jsx
+++ www/src/js/views/timetable/TimetableModulesTable.jsx
@@ -1,9 +1,10 @@
 // @flow
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
+import { sum, sortBy, map } from 'lodash';
 
 import type { ModuleCode, ModuleWithColor, Semester } from 'types/modules';
 import type { ColorIndex } from 'types/reducers';
@@ -16,9 +17,21 @@ import {
   selectModuleColor,
 } from 'actions/timetables';
 import { getModuleExamDate, getFormattedModuleExamDate } from 'utils/modules';
+import { NBSP } from 'utils/react';
 import { modulePage } from 'views/routes/paths';
 
 import styles from './TimetableModulesTable.scss';
+
+type ModuleOrder = {
+  label: string,
+  orderBy: (ModuleWithColor, Semester) => string | number,
+};
+
+const moduleOrders = {
+  exam: { label: 'Exam Date', orderBy: (module, semester) => getModuleExamDate(module, semester) },
+  mc: ({ label: 'Module Credits', orderBy: (module) => module.ModuleCredit }: ModuleOrder),
+  code: ({ label: 'Module Code', orderBy: (module) => module.ModuleCode }: ModuleOrder),
+};
 
 type Props = {
   selectModuleColor: Function,
@@ -31,7 +44,23 @@ type Props = {
   readOnly: boolean,
 };
 
-class TimetableModulesTable extends Component<Props> {
+type State = {
+  moduleOrder: $Keys<typeof moduleOrders>,
+};
+
+function renderMCs(moduleCredits) {
+  return `${moduleCredits}${NBSP}${moduleCredits === 1 ? 'MC' : 'MCs'}`;
+}
+
+class TimetableModulesTable extends Component<Props, State> {
+  state = {
+    moduleOrder: 'exam',
+  };
+
+  totalMCs() {
+    return sum(this.props.modules.map((module) => parseInt(module.ModuleCredit, 10)));
+  }
+
   renderModuleActions(module) {
     const hideBtnLabel = `${module.hiddenInTimetable ? 'Show' : 'Hide'} ${module.ModuleCode}`;
     const removeBtnLabel = `Remove ${module.ModuleCode} from timetable`;
@@ -79,42 +108,70 @@ class TimetableModulesTable extends Component<Props> {
     }
 
     const { readOnly, semester, horizontalOrientation } = this.props;
+    const modules = sortBy(this.props.modules, (module) =>
+      moduleOrders[this.state.moduleOrder].orderBy(module, semester),
+    );
 
     return (
-      <div className={classnames(styles.modulesTable, 'row')}>
-        {this.props.modules.map((module) => (
-          <div
-            className={classnames(styles.modulesTableRow, 'col-sm-6', {
-              'col-lg-4': horizontalOrientation,
-              'col-md-12': !horizontalOrientation,
-            })}
-            key={module.ModuleCode}
-          >
-            <div className={styles.moduleColor}>
-              <ColorPicker
-                label={`Change ${module.ModuleCode} timetable color`}
-                color={module.colorIndex}
-                onChooseColor={(colorIndex: ColorIndex) => {
-                  this.props.selectModuleColor(semester, module.ModuleCode, colorIndex);
-                }}
-              />
-            </div>
-            <div className={styles.moduleInfo}>
-              {!readOnly && this.renderModuleActions(module)}
-              <Link to={modulePage(module.ModuleCode, module.ModuleTitle)}>
-                {module.ModuleCode} {module.ModuleTitle}
-              </Link>
-              <div className={styles.moduleExam}>
-                {getModuleExamDate(module, semester)
-                  ? `Exam: ${getFormattedModuleExamDate(module, semester)}`
-                  : 'No Exam'}
-                &nbsp;&middot;&nbsp;
-                {module.ModuleCredit} MCs
+      <Fragment>
+        <div className={classnames(styles.modulesTable, 'row')}>
+          {modules.map((module) => (
+            <div
+              className={classnames(styles.modulesTableRow, 'col-sm-6', {
+                'col-lg-4': horizontalOrientation,
+                'col-md-12': !horizontalOrientation,
+              })}
+              key={module.ModuleCode}
+            >
+              <div className={styles.moduleColor}>
+                <ColorPicker
+                  label={`Change ${module.ModuleCode} timetable color`}
+                  color={module.colorIndex}
+                  onChooseColor={(colorIndex: ColorIndex) => {
+                    this.props.selectModuleColor(semester, module.ModuleCode, colorIndex);
+                  }}
+                />
+              </div>
+              <div className={styles.moduleInfo}>
+                {!readOnly && this.renderModuleActions(module)}
+                <Link to={modulePage(module.ModuleCode, module.ModuleTitle)}>
+                  {module.ModuleCode} {module.ModuleTitle}
+                </Link>
+                <div className={styles.moduleExam}>
+                  {getModuleExamDate(module, semester)
+                    ? `Exam: ${getFormattedModuleExamDate(module, semester)}`
+                    : 'No Exam'}
+                  &nbsp;&middot;&nbsp;{renderMCs(module.ModuleCredit)}
+                </div>
               </div>
             </div>
+          ))}
+        </div>
+
+        <div className={classnames(styles.footer, 'row align-items-center')}>
+          <div className="col-12">
+            <hr />
           </div>
-        ))}
-      </div>
+          <div className="col">
+            Total Module Credits: <strong>{renderMCs(this.totalMCs())}</strong>
+          </div>
+          <div className={classnames(styles.moduleOrder, 'col')}>
+            <label htmlFor="moduleOrder">Order</label>
+            <select
+              onChange={(evt) => this.setState({ moduleOrder: evt.target.value })}
+              className={classnames(styles.moduleOrder, 'form-control form-control-sm')}
+              value={this.state.moduleOrder}
+              id="moduleOrder"
+            >
+              {map(moduleOrders, ({ label }, key) => (
+                <option key={key} value={key}>
+                  {label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
