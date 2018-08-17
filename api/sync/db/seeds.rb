a0e85aef11fb7fcba768dd8c198e8d8f12921e29
E-Liang Tan <eliang@eliangtan.com>
2018-08-17 10:07:06 +0800

    Install Faker gem and add school and acad year seeds

--- api/sync/db/seeds.rb
+++ api/sync/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@num_fakes = 5
+
+def new_acad_years
+  Array.new(@num_fakes) do
+    AcadYear.new(
+      name: Faker::Cannabis.strain
+    )
+  end
+end
+
+@num_fakes.times do
+  long_name = Faker::University.name
+  short_name = long_name.split.map(&:first).join
+
+  School.create(
+    short_name: short_name,
+    long_name: long_name,
+    slug: short_name.downcase,
+    acad_years: new_acad_years
+  )
+end
