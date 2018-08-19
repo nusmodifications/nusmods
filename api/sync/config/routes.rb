4a84e6e470f6d1e60d48d3f721e67b50758737cd
E-Liang Tan <eliang@eliangtan.com>
2018-08-19 10:42:00 +0800

    Add rails_admin

--- api/sync/config/routes.rb
+++ api/sync/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
 end
