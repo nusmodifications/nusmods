9d42e11c18da92be1ea9584c43698f614b95636a
E-Liang Tan <eliang@eliangtan.com>
2018-08-19 20:17:00 +0800

    Implement updateSettings mutation

--- api/sync/app/graphql/types/mutation_type.rb
+++ api/sync/app/graphql/types/mutation_type.rb
@@ -2,11 +2,29 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
+    field :update_settings, Types::UserSettingsType,
+          null: false,
+          description: 'Update settings for the signed in user' do
+      argument :school_id, ID, required: true
+      argument :mode, String, required: false
+      argument :theme_id, String, required: false
+      argument :cors_notification_enabled, Boolean, required: false
+      guard ->(_obj, _args, ctx) { ctx[:current_user].present? }
+    end
+    def update_settings(school_id:, **args)
+      current_user = context[:current_user]
+      return nil if current_user.nil?
+
+      settings = current_user.settings(school_id: school_id)
+      if settings.nil?
+        settings = UserSetting.new
+        settings.school = School.find(school_id)
+        settings.user = current_user
+      end
+
+      settings.assign_attributes(args)
+      settings.save!
+      settings
     end
   end
 end
