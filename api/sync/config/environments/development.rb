42ebc0325e9ac386be2f89f607ad894d8d8813ad
E-Liang Tan <eliang@eliangtan.com>
2018-08-19 10:48:09 +0800

    Add letter opener gem to view sent mail

--- api/sync/config/environments/development.rb
+++ api/sync/config/environments/development.rb
@@ -30,10 +30,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
+  # Letter Opener settings for email. Emails are not actually sent - they're
+  # captured by the Letter Opener gem. "Sent" emails are stored in
+  # tmp/letter_opener and can be browsed at /letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- api/sync/config/routes.rb
+++ api/sync/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
 end
