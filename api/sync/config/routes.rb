77a45c5fbbed596b5010e4ad7476e790969d06f7
E-Liang Tan <eliang@eliangtan.com>
2018-08-19 10:16:01 +0800

    Add GraphiQL

--- api/sync/config/routes.rb
+++ api/sync/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
