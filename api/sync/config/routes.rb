da75f0fdb075dc3c7fa42cfc39a4d03032ad3a0c
E-Liang Tan <eliang@eliangtan.com>
2018-08-22 12:02:19 +0800

    Replace GraphiQL with GraphQL Playground for custom HTTP headers

--- api/sync/config/routes.rb
+++ api/sync/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  root to: redirect('/graphiql')
+  root to: redirect('/playground')
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  mount GraphqlPlayground::Rails::Engine, at: '/playground', graphql_path: '/graphql'
   post "/graphql", to: "graphql#execute"
 end
