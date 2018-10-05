2e8dda2083551d34dbb6a915b4c48d2f956fcf76
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-10-05 13:35:10 +0800

    Add shirt image and use our logo

--- www/src/js/views/static/HacktoberfestContainer.jsx
+++ www/src/js/views/static/HacktoberfestContainer.jsx
@@ -6,6 +6,8 @@ import styles from 'views/static/HacktoberfestContainer.scss';
 import ExternalLink from 'views/components/ExternalLink';
 import HacktoberfestLogo from 'views/static/HacktoberfestLogo';
 import { Heart } from 'views/components/icons';
+import logo from 'img/nusmods-logo.png';
+import shirt from 'img/nusmods-shirts.png';
 
 const title = 'Hacktoberfest!';
 
@@ -13,7 +15,7 @@ export default function TeamContainer() {
   return (
     <StaticPage title={title} className={styles.page}>
       <h2>
-        <span>NUSMods</span> <Heart className={styles.heart} />
+        <img src={logo} alt="NUSMods" className={styles.logo} /> <Heart className={styles.heart} />
         <HacktoberfestLogo />
         <span className="sr-only">{title}</span>
       </h2>
@@ -24,19 +26,20 @@ export default function TeamContainer() {
         Ocean, as well as exclusive NUSMods shirts!
       </p>
 
+      <img
+        src={shirt}
+        className={styles.shirts}
+        alt="Gray shirts with NUSMods logotype on the front and a hexagonal N NUSMods logo at the back"
+      />
+
       <ol className={styles.steps}>
         <li>
-          <ExternalLink href="https://hacktoberfest.digitalocean.com/" className={styles.button}>
-            Register
-          </ExternalLink>{' '}
-          for Hacktoberfest
+          <ExternalLink href="https://hacktoberfest.digitalocean.com/">Register</ExternalLink> for
+          Hacktoberfest
         </li>
         <li>
           Find an{' '}
-          <ExternalLink
-            href="https://github.com/nusmodifications/nusmods/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+label%3AHacktoberfest+-label%3ATaken"
-            className={styles.button}
-          >
+          <ExternalLink href="https://github.com/nusmodifications/nusmods/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+label%3AHacktoberfest+-label%3ATaken">
             open issues
           </ExternalLink>{' '}
           and claim it by adding a comment
@@ -76,7 +79,7 @@ export default function TeamContainer() {
         <small>
           Free NUSMods shirt only available for NUS students on a first-come-first-serve basis.
           NUSMods is not affiliated with Hacktoberfest. Official Hacktoberfest shirt only available
-          on a limited basis if you submit five pull requests.
+          on a limited basis if you submit five pull requests to any projects on GitHub.
         </small>
       </p>
     </StaticPage>
