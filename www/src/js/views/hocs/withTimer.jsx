cd3524d267b8bc567bd112ceb18bb790aa669296
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-10-25 23:17:29 +0800

    Use page visibility API to update when user comes back to page

--- www/src/js/views/hocs/withTimer.jsx
+++ www/src/js/views/hocs/withTimer.jsx
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import { differenceInMilliseconds } from 'date-fns';
 import { wrapComponentName } from 'utils/react';
 
 export type TimerData = {|
@@ -21,11 +22,26 @@ function withTimer<Props: {}>(
 
     componentDidMount() {
       this.intervalId = setInterval(() => this.setState({ currentTime: new Date() }), intervalInMs);
+      document.addEventListener('visibilitychange', this.onPageVisibilityChange);
     }
 
     componentWillUnmount() {
       clearInterval(this.intervalId);
+      document.removeEventListener('visibilitychange', this.onPageVisibilityChange);
     }
+
+    onPageVisibilityChange = () => {
+      // Page visibility changes when tabs go in and out of focus. When tabs
+      // are out of focus, mobile browsers slow down timers, so we run an
+      // additional check to make sure the page state has not drifted too far
+      // from the wall clock
+      if (
+        !document.hidden &&
+        differenceInMilliseconds(new Date(), this.state.currentTime) > intervalInMs
+      ) {
+        this.setState({ currentTime: new Date() });
+      }
+    };
 
     render() {
       return <WrappedComponent {...this.state} {...this.props} />;
