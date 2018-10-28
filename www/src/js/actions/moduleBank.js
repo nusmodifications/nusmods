0ff0e8a8cc8b0bad5979aa53ff64fadf9becfed6
raiden-x <raambalajid@gmail.com>
2018-10-28 22:36:49 +0530

    addressing comments for issue #1245

--- www/src/js/actions/moduleBank.js
+++ www/src/js/actions/moduleBank.js
@@ -1,7 +1,8 @@
 // @flow
 import type { ModuleCode } from 'types/modules';
-import type { FSA } from 'types/redux';
+import type { FSA, GetState } from 'types/redux';
 
+import { size, get } from 'lodash';
 import { requestAction } from 'actions/requests';
 import NUSModsApi from 'apis/nusmods';
 
@@ -17,8 +18,62 @@ export function fetchModuleRequest(moduleCode: ModuleCode) {
   return `${FETCH_MODULE}_${moduleCode}`;
 }
 
-export function fetchModule(moduleCode: ModuleCode): FSA {
-  return requestAction(fetchModuleRequest(moduleCode), FETCH_MODULE, {
-    url: NUSModsApi.moduleDetailsUrl(moduleCode),
-  });
+export const UPDATE_MODULE_TIMESTAMP = 'UPDATE_MODULE_TIMESTAMP';
+export function updateModuleTimestamp(moduleCode: ModuleCode) {
+  return {
+    type: UPDATE_MODULE_TIMESTAMP,
+    payload: moduleCode,
+  };
+}
+
+export const REMOVE_LRU_MODULE = 'REMOVE_LRU_MODULE';
+export function removeLRUModule(moduleCode: ModuleCode) {
+  return {
+    type: REMOVE_LRU_MODULE,
+    payload: moduleCode,
+  };
+}
+
+function getLRUModule(modules, lessons, currentModule) {
+  // Pull all the modules in all the timetables
+  // used parseInt since object.keys returns array of strings even if the key is a integer
+  const timeTableModules = Object.keys(lessons).reduce(
+    (moduleList, semester) => [...moduleList, ...Object.keys(lessons[parseInt(semester, 10)])],
+    [],
+  );
+  const sortedModules = Object.keys(modules).sort(
+    (currElem, nextElem) =>
+      get(modules[currElem], ['timestamp'], 0) - get(modules[nextElem], ['timestamp'], 0),
+  );
+  // Remove the module which is least recently used and which is not in timetable and not the currently loaded one
+  const moduleToBeDeleted = sortedModules.find(
+    (module) => !timeTableModules.includes(module) && module !== currentModule,
+  );
+  return moduleToBeDeleted || null;
+}
+
+const MAX_MODULE_LIMIT: number = 100;
+export function fetchModule(moduleCode: ModuleCode) {
+  return (dispatch: Function, getState: GetState) =>
+    dispatch(
+      requestAction(fetchModuleRequest(moduleCode), FETCH_MODULE, {
+        url: NUSModsApi.moduleDetailsUrl(moduleCode),
+      }),
+    ).finally(() => {
+      // Update the timestamp of the accessed module if it is in the store.
+      if (getState().moduleBank.modules[moduleCode]) {
+        dispatch(updateModuleTimestamp(moduleCode));
+      }
+      // Remove the LRU module if the size exceeds the maximum one and if anything can be removed
+      if (size(getState().moduleBank.modules) > MAX_MODULE_LIMIT) {
+        const LRUModule = getLRUModule(
+          getState().moduleBank.modules,
+          getState().timetables.lessons,
+          moduleCode,
+        );
+        if (LRUModule) {
+          dispatch(removeLRUModule(LRUModule));
+        }
+      }
+    });
 }
--- www/src/js/reducers/moduleBank.js
+++ www/src/js/reducers/moduleBank.js
@@ -3,16 +3,18 @@ import type { FSA } from 'types/redux';
 import type { Module, ModuleCode, Semester } from 'types/modules';
 import type { SemTimetableConfig } from 'types/timetables';
 import type { ModuleCodeMap, ModuleList, ModuleSelectListItem } from 'types/reducers';
-import { SUCCESS, FAILURE } from 'types/reducers';
+import { SUCCESS } from 'types/reducers';
 
 import { REHYDRATE } from 'redux-persist';
-import { keyBy, size, zipObject, get, omit } from 'lodash';
+import { keyBy, size, zipObject, omit } from 'lodash';
 
-import LocalStorage from 'storage/localStorage';
-
-import { FETCH_MODULE, FETCH_MODULE_LIST } from 'actions/moduleBank';
+import {
+  FETCH_MODULE,
+  FETCH_MODULE_LIST,
+  UPDATE_MODULE_TIMESTAMP,
+  REMOVE_LRU_MODULE,
+} from 'actions/moduleBank';
 import { SET_EXPORTED_DATA } from 'actions/export';
-import { API_REQUEST } from 'actions/requests';
 
 export type ModulesMap = {
   [ModuleCode]: Module,
@@ -31,40 +33,11 @@ const defaultModuleBankState: ModuleBank = {
   apiLastUpdatedTimestamp: undefined,
 };
 
-const MAX_MODULE_LIMIT: number = 100;
-
 function precomputeFromModuleList(moduleList: ModuleList) {
   // Cache a mapping of all module codes to module data for fast module data lookup
   const moduleCodes = zipObject(moduleList.map((module) => module.ModuleCode), moduleList);
 
   return { moduleCodes };
-}
-
-function getLRUModule(modules) {
-  // Need to access localStorage here since this comp does not have access to the timetable lessons.
-  const timeTables = JSON.parse(JSON.parse(LocalStorage().getItem('persist:timetables')).lessons);
-  // Pull all the modules in all the timetables
-  const timeTableModules = Object.keys(timeTables).reduce(
-    (moduleList, semester) => [...moduleList, ...Object.keys(timeTables[semester])],
-    [],
-  );
-  const sortedModules = Object.keys(modules).sort((currElem, nextElem) => {
-    const currElemTimestamp = get(modules[currElem], ['timestamp'], 0);
-    const nextElemTimestamp = get(modules[nextElem], ['timestamp'], 0);
-    if (currElemTimestamp < nextElemTimestamp) return -1;
-    if (currElemTimestamp > nextElemTimestamp) return 1;
-    return 0;
-  });
-  const moduleToBeDeleted = sortedModules.find((module) => !timeTableModules.includes(module));
-  return moduleToBeDeleted || '';
-}
-
-function removeLRUModule(modules, loadedModuleCode) {
-  // No need to remove from state when no new module is loaded
-  if (modules[loadedModuleCode]) {
-    return modules;
-  }
-  return omit(modules, getLRUModule(modules));
 }
 
 function moduleBank(state: ModuleBank = defaultModuleBankState, action: FSA): ModuleBank {
@@ -77,20 +50,32 @@ function moduleBank(state: ModuleBank = defaultModuleBankState, action: FSA): Mo
         apiLastUpdatedTimestamp: action.meta && action.meta.responseHeaders['last-modified'],
       };
 
-    case FETCH_MODULE + SUCCESS: {
-      let modules = state.modules;
-      if (size(modules) > MAX_MODULE_LIMIT) {
-        modules = removeLRUModule(state.modules, action.payload.ModuleCode);
-      }
+    case FETCH_MODULE + SUCCESS:
       return {
         ...state,
         modules: {
-          ...modules,
-          [action.payload.ModuleCode]: {
-            ...action.payload,
+          ...state.modules,
+          [action.payload.ModuleCode]: action.payload,
+        },
+      };
+
+    case UPDATE_MODULE_TIMESTAMP:
+      return {
+        ...state,
+        modules: {
+          ...state.modules,
+          [action.payload]: {
+            ...state.modules[action.payload],
             timestamp: new Date().getTime(),
           },
         },
+      };
+
+    case REMOVE_LRU_MODULE: {
+      const trimmedModules = omit(state.modules, action.payload);
+      return {
+        ...state,
+        modules: trimmedModules,
       };
     }
 
@@ -99,24 +84,6 @@ function moduleBank(state: ModuleBank = defaultModuleBankState, action: FSA): Mo
         ...state,
         modules: keyBy(action.payload.modules, (module: Module) => module.ModuleCode),
       };
-
-    case FETCH_MODULE + FAILURE: {
-      const moduleCode = get(action, ['meta', API_REQUEST], '').split('_')[2];
-      // Update the timestamp to keep track of when the module was last accessed even when the call fails if it is in state/cache
-      if (state.modules[moduleCode]) {
-        return {
-          ...state,
-          modules: {
-            ...state.modules,
-            [moduleCode]: {
-              ...state.modules[moduleCode],
-              timestamp: new Date().getTime(),
-            },
-          },
-        };
-      }
-      return state;
-    }
 
     case REHYDRATE:
       if (!size(state.moduleCodes) && state.moduleList) {
