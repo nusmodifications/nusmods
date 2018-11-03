660666ad4c52e0e8e6025147c079ef6ec30ffe2b
raiden-x <raambalajid@gmail.com>
2018-11-04 00:44:48 +0530

    adding test cases for #1245

--- www/src/js/actions/moduleBank.test.js
+++ www/src/js/actions/moduleBank.test.js
@@ -1,5 +1,6 @@
 // @flow
 import type { FSA } from 'types/redux';
+import type { TimetableConfig } from 'types/timetables';
 
 import * as actions from 'actions/moduleBank';
 import NUSModsApi from 'apis/nusmods';
@@ -19,5 +20,24 @@ test('fetchModuleList should return a request action', () => {
 
 test('fetchModule should return a thunk', () => {
   const resultOfAction = actions.fetchModule('CS1010S');
+  expect(resultOfAction).toMatchSnapshot();
+});
+
+test('getLRUModule should return the LRU and non-timetable module', () => {
+  /* eslint-disable no-useless-computed-key */
+  const timetableConfig: TimetableConfig = {
+    [1]: {
+      ACC1001: ({}: any),
+    },
+  };
+  /* eslint-enable */
+  const modules = {
+    ACC1001: ({ timestamp: 1 }: any),
+    ACC1002: ({ timestamp: 2 }: any),
+    ACC1003: ({ timestamp: 3 }: any),
+    ACC1004: ({ timestamp: 4 }: any),
+  };
+  const currentModule = 'ACC1002';
+  const resultOfAction = actions.getLRUModule(modules, timetableConfig, currentModule);
   expect(resultOfAction).toMatchSnapshot();
 });
