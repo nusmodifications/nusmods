97a6c0cc7a0be0c33c59bceb586018d5b09c5b12
Andrea Thniah <andreathniah@gmail.com>
2018-11-09 12:04:09 +0800

    Add instance variable in RandomKawaii

--- www/src/js/views/components/RandomKawaii.jsx
+++ www/src/js/views/components/RandomKawaii.jsx
@@ -1,27 +1,27 @@
 // @flow
-import React from 'react';
+import React, { PureComponent, type ComponentType } from 'react';
 import { Planet, SpeechBubble, Mug, Browser, Ghost } from 'react-kawaii';
 
 type Props = {
-  Kawaii: Component,
-  size?: int,
+  size?: number,
   mood?: string,
   color?: string,
-  title?: int,
-  'aria-label'?: int,
+  title?: string,
+  'aria-label'?: string,
 };
 
-function RandomKawaii(props: Props) {
-  return (
-    <div aria-label={props['aria-label']} title={props.title}>
-      <props.Kawaii size={props.size} mood={props.mood} color={props.color} />
-    </div>
-  );
+class RandomKawaii extends PureComponent<Props> {
+  kawaii: ComponentType;
+  constructor() {
+    super();
+    const icons = [Planet, SpeechBubble, Mug, Browser, Ghost];
+    this.kawaii = icons[Math.floor(Math.random() * icons.length)];
+  }
+  render() {
+    const { size, mood, color } = this.props;
+    const Kawaii = this.kawaii;
+    return <Kawaii size={size} mood={mood} color={color} />;
+  }
 }
-
-const icons = [Planet, SpeechBubble, Mug, Browser, Ghost];
-RandomKawaii.defaultProps = {
-  Kawaii: icons[Math.floor(Math.random() * icons.length)],
-};
 
 export default RandomKawaii;
