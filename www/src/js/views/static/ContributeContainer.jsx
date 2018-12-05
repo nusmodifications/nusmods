b6ffb2dc75540066a04914fa0d2c880c03127289
Monika <manuela.monika97@gmail.com>
2018-12-06 02:21:40 +0800

    Add contributors

--- www/src/js/views/static/ContributeContainer.jsx
+++ www/src/js/views/static/ContributeContainer.jsx
@@ -1,73 +1,185 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import classnames from 'classnames';
 import { toggleFeedback } from 'actions/app';
 import config from 'config';
 
 import { Mail, Layers, GitHub } from 'views/components/icons';
 import ExternalLink from 'views/components/ExternalLink';
+import Loader from 'views/components/LoadingSpinner';
 
 import StaticPage from './StaticPage';
 import styles from './AboutContainer.scss';
+
+const CONTRIBUTORS_URL =
+  'https://api.github.com/repos/NUSModifications/NUSMods/contributors?per_page=100';
+const CONTRIBUTOR_TYPE_USER = 'User';
+const CONTRIBUTOR_ID_RENOVATE = 25180681;
 
 type Props = {
   toggleFeedback: Function,
 };
 
-function ContributeContainer(props: Props) {
-  return (
-    <StaticPage title="Contribute">
-      <h3>Help us help you!</h3>
-      <p>
-        NUSMods is a 100% student-run, open source project. We rely on the continuous support of our
-        valued contributors and the NUS student community. Many students have reported issues,
-        suggested improvements, and even contributed code. Join us to make NUS a better place for
-        its students (your friends)! NUSMods is a fast-evolving project, and there are many things
-        to be done. Help us help you!
-      </p>
+type Contributor = {
+  avatar_url: string,
+  contributions: number,
+  events_url: string,
+  followers_url: string,
+  following_url: string,
+  gists_url: string,
+  gravatar_id: string,
+  html_url: string,
+  id: number,
+  login: string,
+  organizations_ur: string,
+  received_events_ur: string,
+  repos_ur: string,
+  site_admin: boolean,
+  starred_ur: string,
+  subscriptions_ur: string,
+  type: string,
+  url: string,
+};
 
-      <h3>Here&apos;s how you can help</h3>
+type State = {
+  contributors: ?Array<Contributor>,
+  isLoading: boolean,
+  isError: boolean,
+  errorMessage: string,
+};
 
-      <div className={classnames('row no-gutters', styles.actionContainer)}>
-        <div className={classnames('col-lg', styles.btnContainer)}>
-          <button onClick={props.toggleFeedback} className="btn btn-primary btn-svg btn-block">
-            <Mail className="svg" />
-            We need feedback!
-          </button>
+class ContributeContainer extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      contributors: null,
+      isLoading: true,
+      isError: false,
+      errorMessage: '',
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(CONTRIBUTORS_URL)
+      .then((response) => {
+        this.setState({
+          contributors: response.data,
+          isLoading: false,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          isError: true,
+          errorMessage: err.message,
+          isLoading: false,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <StaticPage title="Contribute">
+        <h3>Help us help you!</h3>
+        <p>
+          NUSMods is a 100% student-run, open source project. We rely on the continuous support of
+          our valued contributors and the NUS student community. Many students have reported issues,
+          suggested improvements, and even contributed code. Join us to make NUS a better place for
+          its students (your friends)! NUSMods is a fast-evolving project, and there are many things
+          to be done. Help us help you!
+        </p>
+
+        <br />
+        <h4>Here&apos;s how you can help</h4>
+
+        <div className={classnames('row no-gutters', styles.actionContainer)}>
+          <div className={classnames('col-lg', styles.btnContainer)}>
+            <button onClick={this.props.toggleFeedback} className="btn btn-primary btn-svg btn-block">
+              <Mail className="svg" />
+              We need feedback!
+            </button>
+          </div>
+          <div className={classnames('col-lg', styles.btnContainer)}>
+            <ExternalLink
+              href={config.contact.messenger}
+              className="btn btn-primary btn-svg btn-block"
+            >
+              <Layers className="svg" />
+              We need designers!
+            </ExternalLink>
+          </div>
+          <div className={classnames('col-lg', styles.btnContainer)}>
+            <ExternalLink
+              href={config.contact.githubRepo}
+              className="btn btn-primary btn-svg btn-block"
+            >
+              <GitHub className="svg" />
+              We need code!
+            </ExternalLink>
+          </div>
         </div>
-        <div className={classnames('col-lg', styles.btnContainer)}>
-          <ExternalLink
-            href={config.contact.messenger}
-            className="btn btn-primary btn-svg btn-block"
-          >
-            <Layers className="svg" />
-            We need designers!
-          </ExternalLink>
-        </div>
-        <div className={classnames('col-lg', styles.btnContainer)}>
-          <ExternalLink
-            href={config.contact.githubRepo}
-            className="btn btn-primary btn-svg btn-block"
-          >
-            <GitHub className="svg" />
-            We need code!
-          </ExternalLink>
-        </div>
-      </div>
 
-      <h4>Locate the venues</h4>
-      <p>
-        We also need help in locating the venues. All you have to do is choose a venue and mark
-        where it is on the map. If youre at the venue, you can also use your phone&apos;s GPS to get the
-        location automatically.
-      </p>
+        <br />
+        <h4>Locate the venues</h4>
+        <p>
+          We also need help in locating the venues. All you have to do is choose a venue and mark
+          where it is on the map. If youre at the venue, you can also use your phone&apos;s GPS to
+          get the location automatically.
+        </p>
 
-      <p>
-        <em>- NUSMods Team</em>
-      </p>
-    </StaticPage>
-  );
+        <br />
+        <h4>Contributors</h4>
+        <p>
+          This could be you! ;)
+        </p>
+        {this.state.isLoading && <Loader />}
+        {this.state.isError && (
+          <div className="alert alert-danger">
+            <strong>Something went wrong!</strong>
+            {this.state.errorMessage}
+          </div>
+        )}
+        {this.state.contributors && (
+          <div className="row">
+            {this.state.contributors
+              .filter(
+                (contributor) =>
+                  contributor.type === CONTRIBUTOR_TYPE_USER &&
+                  // Renovate used to report outdated dependencies as a user via the GitHub API,
+                  // hence we need to filter it out by its GitHub user ID.
+                  contributor.id !== CONTRIBUTOR_ID_RENOVATE,
+              )
+              .map((contributor) => (
+                <div className="col-md-3 col-6 text-center" key={contributor.id}>
+                  <ExternalLink href={contributor.html_url}>
+                    <img
+                      src={contributor.avatar_url}
+                      alt={`${contributor.login} thumbnail`}
+                      className={classnames(styles.thumbnail, 'img-fluid img-thumbnail')}
+                    />
+                    <span className={styles.contributorUsername}>{contributor.login}</span>
+                  </ExternalLink>
+                  <p>
+                    <ExternalLink
+                      className="text-muted"
+                      href={`https://github.com/nusmodifications/nusmods/commits?author=${
+                        contributor.login
+                      }`}
+                    >
+                      {contributor.contributions} commit
+                      {contributor.contributions !== 1 && 's'}
+                    </ExternalLink>
+                  </p>
+                </div>
+              ))}
+          </div>
+        )}
+      </StaticPage>
+    );
+  }
 }
 
 export default connect(
