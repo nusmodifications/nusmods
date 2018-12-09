27c7741d060e1a857ee95b56439c361fdee99ea2
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-12-09 17:28:55 +0800

    Add refresh alert and to dot navtab

--- www/src/js/views/layout/Navtabs.jsx
+++ www/src/js/views/layout/Navtabs.jsx
@@ -7,8 +7,11 @@ import { connect } from 'react-redux';
 import { NavLink, withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 
-import { BookOpen, Calendar, Clock, Heart, Map, Settings } from 'views/components/icons';
+import { BookOpen, Calendar, Clock, Heart, Map, Settings, Refresh } from 'views/components/icons';
+import ExternalLink from 'views/components/ExternalLink';
+import Online from 'views/components/Online';
 import { timetablePage } from 'views/routes/paths';
+import { updateServiceWorker } from 'bootstrapping/service-worker';
 
 import styles from './Navtabs.scss';
 
@@ -17,13 +20,13 @@ export const NAVTAB_HEIGHT = 48;
 type Props = {
   activeSemester: Semester,
   beta: boolean,
+  promptRefresh: boolean,
 };
 
 export function NavtabsComponent(props: Props) {
   const tabProps = {
     className: styles.link,
     activeClassName: styles.linkActive,
-    ariaCurrent: 'page',
   };
 
   return (
@@ -49,17 +52,31 @@ export function NavtabsComponent(props: Props) {
       <NavLink {...tabProps} to="/settings">
         <Settings />
         <span className={styles.title}>Settings</span>
+        {props.promptRefresh && (
+          <div className={classnames(styles.updateDot)}>Update Available</div>
+        )}
       </NavLink>
       <div className={styles.divider} />
-      <a
+      <ExternalLink
         className={classnames(tabProps.className, styles.hiddenOnMobile)}
         href="https://nuswhispers.com"
-        target="_blank"
-        rel="noreferrer noopener"
       >
         <Heart />
         <span className={styles.title}>Whispers</span>
-      </a>
+      </ExternalLink>
+      {props.promptRefresh && (
+        <Online>
+          <button
+            className={classnames(styles.refreshPrompt, 'alert alert-success')}
+            type="button"
+            onClick={updateServiceWorker}
+          >
+            <Refresh size={30} className={styles.refreshIcon} />
+            NUSMods update available
+            <div className="btn btn-sm btn-block btn-success">Refresh page</div>
+          </button>
+        </Online>
+      )}
     </nav>
   );
 }
@@ -67,5 +84,7 @@ export function NavtabsComponent(props: Props) {
 const connectedNavtabs = connect((state: State) => ({
   activeSemester: state.app.activeSemester,
   beta: state.settings.beta,
+  promptRefresh: true, // state.app.promptRefresh,
 }))(NavtabsComponent);
+
 export default withRouter(connectedNavtabs);
