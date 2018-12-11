27067cf77ba8b3afe97be555d4cb789d4044e7b8
Zhang Yi Jiang <mediumdeviation@gmail.com>
2018-12-11 16:58:29 +0800

    Use _paq array for tracking

--- www/src/js/bootstrapping/mamoto.js
+++ www/src/js/bootstrapping/mamoto.js
@@ -5,31 +5,23 @@ import type { Tracker } from 'types/views';
 import Raven from 'raven-js';
 import insertScript from 'utils/insertScript';
 
-let tracker: ?Tracker; // eslint-disable-line import/no-mutable-exports
+/* eslint-disable no-underscore-dangle */
 
 // Code mostly adopted from https://github.com/AmazingDreams/vue-matomo
-function configureMamoto(history: RouterHistory) {
+export function initializeMamoto() {
   const siteId = '1';
   const host = 'https://analytics.nusmods.com';
   const scriptSrc = `${host}/piwik.js`;
+
+  window._paq = window._paq || [];
 
   insertScript(scriptSrc, { defer: true, async: true })
     .then(() => {
       // Save a non-null instance of the tracker
       const mamoto: Tracker = window.Piwik.getTracker(`${host}/piwik.php`, siteId);
-      tracker = mamoto;
 
       // Track initial page view
       mamoto.trackPageView();
-
-      history.listen((location, action) => {
-        if (action === 'PUSH') {
-          // Wait a bit for the page title to update
-          setTimeout(() => {
-            mamoto.trackPageView(document.title);
-          }, 100);
-        }
-      });
     })
     .catch((error) => {
       if (error instanceof Error) {
@@ -44,4 +36,30 @@ function configureMamoto(history: RouterHistory) {
     });
 }
 
-export { tracker, configureMamoto };
+export function withTracker(action: (Tracker) => void) {
+  window._paq = window._paq || [];
+
+  window._paq.push([
+    // eslint-disable-next-line func-names
+    function() {
+      action(this);
+    },
+  ]);
+}
+
+export function trackPageView(history: RouterHistory) {
+  history.listen((location, action) => {
+    if (action === 'PUSH') {
+      // Wait a bit for the page title to update
+      setTimeout(() => {
+        withTracker((tracker) => tracker.trackPageView(document.title));
+      }, 100);
+    }
+  });
+}
+
+// Go to the Settings > Website > Custom Dimensions on https://analytics.nusmods.com
+// to set this up
+export const DIMENSIONS = {
+  theme: 1,
+};
