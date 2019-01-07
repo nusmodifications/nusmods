6e03e94341b86d2c72aca766ab90bd9d1340daf8
wn <weineng.a@gmail.com>
2019-01-08 03:10:22 +0800

    Fix lint

--- www/src/js/views/timetable/TimetableContent.jsx
+++ www/src/js/views/timetable/TimetableContent.jsx
@@ -340,9 +340,9 @@ class TimetableContent extends Component<Props, State> {
               <div className={styles.modulesSelect}>
                 {!readOnly && (
                   <ModulesSelectContainer
-                  semester={semester}
-                  timetable={this.props.timetable}
-                  resetTombstone={this.resetTombstone}
+                    semester={semester}
+                    timetable={this.props.timetable}
+                    resetTombstone={this.resetTombstone}
                   />
                 )}
               </div>
