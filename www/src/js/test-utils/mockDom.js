55f15533019e44f34993ff9081f1f98a0252fd5c
Julius S <jluuiss@gmail.com>
2019-01-16 13:52:15 +0800

    Downshift retain values (#1395)
    * Fix outer click for ModulesSelect
    
    * Fix outer click for GlobalSearch
    
    * Fix outer click for DropdownListFilters
    
    * Tweak logic
    
    * Tweak global search styles
    
    * Fix iOS scroll bug
    
    * Use CSS for autofocus fix
    
    * Try to fix gboard cursor issue

--- www/src/js/test-utils/mockDom.js
+++ www/src/js/test-utils/mockDom.js
@@ -1,9 +1,16 @@
 // @flow
-/* eslint-env jest */
 
 export default function mockDom() {
   // Mock some of the DOM environment functions that are missing from JSDom
   window.scrollTo = jest.fn();
   global.performance = { now: jest.fn() };
   global.matchMedia = jest.fn(() => ({ matches: jest.fn(), addListener: jest.fn() }));
+
+  // JSDom does not stub scrollIntoView - https://github.com/jsdom/jsdom/issues/1695
+  if (!Element.prototype.scrollIntoView) {
+    // Flow doesn't think this property is writable, which is true,
+    // but we need this to stub this method
+    // $FlowFixMe
+    Element.prototype.scrollIntoView = jest.fn();
+  }
 }
