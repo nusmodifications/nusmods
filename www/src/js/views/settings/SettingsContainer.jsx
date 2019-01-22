fce1c5952aa5a2d42ba094cad7c82d90b90a35d4
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-22 16:04:01 +0800

    Remove CORS settings and notifications  (#1464)
    * Remove CORS UI
    
    * Remove CORS utils and types

--- www/src/js/views/settings/SettingsContainer.jsx
+++ www/src/js/views/settings/SettingsContainer.jsx
@@ -1,37 +1,21 @@
 // @flow
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import deferComponentRender from 'views/hocs/deferComponentRender';
 import classnames from 'classnames';
 
 import type { Faculty } from 'types/modules';
 import type { Mode } from 'types/settings';
-import type { CorsNotificationSettings } from 'types/reducers';
 import type { State as StoreState } from 'reducers';
 
 import availableThemes from 'data/themes.json';
 import { selectTheme } from 'actions/theme';
-import {
-  selectNewStudent,
-  selectFaculty,
-  selectMode,
-  dismissCorsNotification,
-  enableCorsNotification,
-  toggleCorsNotificationGlobally,
-  toggleBetaTesting,
-} from 'actions/settings';
-// import FacultySelect from 'views/components/FacultySelect';
-// import NewStudentSelect from 'views/components/NewStudentSelect';
+import { selectNewStudent, selectFaculty, selectMode, toggleBetaTesting } from 'actions/settings';
 import ScrollToTop from 'views/components/ScrollToTop';
 import Timetable from 'views/timetable/Timetable';
 import Title from 'views/components/Title';
-import Toggle from 'views/components/Toggle';
 
-import CorsNotification, {
-  corsNotificationText,
-} from 'views/components/cors-info/CorsNotification';
 import Online from 'views/components/Online';
-import { currentRound } from 'utils/cors';
 import { supportsCSSVariables } from 'utils/css';
 
 import ThemeOption from './ThemeOption';
@@ -46,7 +30,6 @@ type Props = {
   faculty: Faculty,
   currentThemeId: string,
   mode: Mode,
-  corsNotification: CorsNotificationSettings,
   betaTester: boolean,
 
   selectTheme: Function,
@@ -55,9 +38,6 @@ type Props = {
   selectMode: Function,
 
   toggleBetaTesting: () => void,
-  dismissCorsNotification: Function,
-  enableCorsNotification: Function,
-  toggleCorsNotificationGlobally: Function,
 };
 
 class SettingsContainer extends Component<Props> {
@@ -84,43 +64,8 @@ class SettingsContainer extends Component<Props> {
     );
   }
 
-  renderCorsNotitificationOption(corsRound) {
-    const { corsNotification } = this.props;
-    const isSnoozed = corsRound && corsNotification.dismissed.includes(corsRound.round);
-
-    return (
-      <Fragment>
-        <div className={styles.toggleRow}>
-          <div className={styles.toggleDescription}>
-            <p>
-              {isSnoozed
-                ? 'You have snoozed reminders until the end of this round'
-                : 'You can also temporarily snooze the notification until the end of this round.'}
-            </p>
-          </div>
-          <div className={classnames('col-sm-4 offset-sm-1', styles.toggle)}>
-            <button
-              className="btn btn-outline-primary"
-              type="button"
-              onClick={() =>
-                isSnoozed
-                  ? this.props.enableCorsNotification(corsRound.round)
-                  : this.props.dismissCorsNotification(corsRound.round)
-              }
-            >
-              {isSnoozed ? 'Unsnooze' : 'Snooze'}
-            </button>
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
-
   render() {
-    const { corsNotification, currentThemeId } = this.props;
-
-    const corsRound = currentRound();
-    const corsText = corsNotificationText(false);
+    const { currentThemeId } = this.props;
 
     return (
       <div className={classnames(styles.settingsPage, 'page-container')}>
@@ -134,37 +79,6 @@ class SettingsContainer extends Component<Props> {
         <h1 className={styles.title}>Settings</h1>
         <hr />
 
-        {/* TODO: Finish the CORS bidding stats filter feature and re-enable this
-      <h4>New Student</h4>
-      <div className={styles.toggleRow}>
-        <div className={styles.toggleDescription}>
-          <p>For certain modules, places are reserved for new students in CORS Bidding Rounds
-                1 and 2. Enabling this will highlight those numbers and rounds.</p>
-        </div>
-        <div className={classnames('col-sm-4 offset-sm-1 col-5', styles.toggle)}>
-          <NewStudentSelect
-            newStudent={props.newStudent}
-            onSelectNewStudent={props.selectNewStudent}
-          />
-        </div>
-      </div>
-      <hr />
-
-      <h4>Faculty</h4>
-      <div className="row">
-        <div className="col-sm-7">
-          <p>Certain modules have places reserved for students in different faculties in rounds
-                1 and 2. Selecting your faculty will highlight those numbers and rounds.</p>
-
-          <p>CEG Students should select <strong>Joint Multi-Disciplinary Program</strong> due to the
-                unique nature of their course.</p>
-        </div>
-        <div className="col-sm-4 offset-sm-1">
-          <FacultySelect faculty={props.faculty} onChange={props.selectFaculty} />
-        </div>
-      </div>
-      <hr />
-      */}
         {supportsCSSVariables() && this.renderNightModeOption()}
 
         <h4 id="theme">Theme</h4>
@@ -189,29 +103,6 @@ class SettingsContainer extends Component<Props> {
             />
           ))}
         </div>
-
-        <hr />
-
-        <h4 id="cors">CORS Bidding Reminder</h4>
-
-        <div className={styles.notificationPreview}>
-          <CorsNotification hideCloseButton />
-        </div>
-
-        <div className={styles.toggleRow}>
-          <div className={styles.toggleDescription}>
-            <p>You can get a reminder about when CORS bidding starts with a small notification.</p>
-            {corsText && <p>{corsText}</p>}
-          </div>
-          <div className={styles.toggle}>
-            <Toggle
-              isOn={corsNotification.enabled}
-              onChange={this.props.toggleCorsNotificationGlobally}
-            />
-          </div>
-        </div>
-
-        {corsNotification.enabled && corsRound && this.renderCorsNotitificationOption(corsRound)}
 
         <hr />
 
@@ -240,9 +131,6 @@ const connectedSettings = connect(
     selectNewStudent,
     selectFaculty,
     selectMode,
-    toggleCorsNotificationGlobally,
-    dismissCorsNotification,
-    enableCorsNotification,
     toggleBetaTesting,
   },
 )(SettingsContainer);
