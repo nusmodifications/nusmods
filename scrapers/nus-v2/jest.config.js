3cb03b1d1d627f371360735d4be3fedaa59dfe5c
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-25 11:51:53 +0800

    Fix Jest and CircleCI configs

--- scrapers/nus-v2/jest.config.js
+++ scrapers/nus-v2/jest.config.js
@@ -42,9 +42,9 @@ module.exports = {
   // reporters: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    'src'
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
---
+++ scrapers/nus-v2/src/index.test.js
@@ -0,0 +1,5 @@
+// @flow
+
+test('Blank test', () => {
+  expect(1).toBeTruthy();
+});
