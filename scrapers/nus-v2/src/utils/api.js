02df37ea1cb1d3b73305b90b22a27d29da46e933
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-25 23:14:38 +0800

    Add fromTermCode util

--- scrapers/nus-v2/src/utils/api.js
+++ scrapers/nus-v2/src/utils/api.js
@@ -6,6 +6,8 @@
 
 /* eslint-disable import/prefer-default-export */
 
+import type { Semester } from '../types/modules';
+
 /**
  * Construct the 4 number term code from the academic year and semester
  */
@@ -13,4 +15,11 @@ export function getTermCode(semester: number | string, academicYear: string) {
   const year = /\d\d(\d\d)/.exec(academicYear);
   if (!year) throw new RangeError('academicYear should be in the format of YYYY/YYYY or YYYY-YY');
   return `${year[1]}${semester}0`;
+}
+
+export function fromTermCode(term: string): [string, Semester] {
+  const year = parseInt(term.slice(0, 2), 10);
+  const semester = parseInt(term.charAt(2), 10);
+
+  return [`20${year}/20${year + 1}`, semester];
 }
--- scrapers/nus-v2/src/utils/api.test.js
+++ scrapers/nus-v2/src/utils/api.test.js
@@ -1,11 +1,20 @@
 // @flow
 
-import { getTermCode } from './api';
+import { fromTermCode, getTermCode } from './api';
 
 describe(getTermCode, () => {
   test('should return term code', () => {
     expect(getTermCode(1, '2018/2019')).toEqual('1810');
     expect(getTermCode('2', '2018/2019')).toEqual('1820');
     expect(getTermCode('2', '2018/19')).toEqual('1820');
+  });
+});
+
+describe(fromTermCode, () => {
+  test('should return acad year and semester', () => {
+    expect(fromTermCode('1810')).toEqual(['2018/2019', 1]);
+    expect(fromTermCode('1820')).toEqual(['2018/2019', 2]);
+    expect(fromTermCode('1830')).toEqual(['2018/2019', 3]);
+    expect(fromTermCode('2020')).toEqual(['2020/2021', 2]);
   });
 });
