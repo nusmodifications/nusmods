f7dad4f268fefe593d43dd6dedbf442caf0849a0
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-26 16:00:29 +0800

    Update lint config

--- scrapers/nus-v2/.eslintrc.js
+++ scrapers/nus-v2/.eslintrc.js
@@ -33,5 +33,26 @@ module.exports = {
 
     // Let git handle the linebreaks instead.
     'linebreak-style': 'off',
+
+    // Override AirBnb to allow for-of, since this is on the server so regenerator-runtime
+    // overhead is trivial
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message:
+          'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
   },
 };
