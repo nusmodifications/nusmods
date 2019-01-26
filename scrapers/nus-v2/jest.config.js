22279f4f61e80c91f695c802e9f70fa051f1d85a
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-26 22:51:34 +0800

    Fix CI (hopefully)

--- scrapers/nus-v2/jest.config.js
+++ scrapers/nus-v2/jest.config.js
@@ -35,6 +35,11 @@ module.exports = {
   // A path to a module which exports an async function that is triggered once after all test suites
   // globalTeardown: null,
 
+  moduleNameMapper: {
+    // For CI, which doesn't have an env.json
+    '../env.json': '<rootDir>/src/__mock__/env.json',
+  },
+
   // A preset that is used as a base for Jest's configuration
   // preset: null,
 
@@ -42,9 +47,7 @@ module.exports = {
   // reporters: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    'src'
-  ],
+  roots: ['src'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
