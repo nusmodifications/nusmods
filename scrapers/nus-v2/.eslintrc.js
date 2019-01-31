5bf0ff979caaffb971942bb1c7fbe66aef6bff4e
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-01-31 14:54:06 +0800

    Reduce Prettier errors to warning

--- scrapers/nus-v2/.eslintrc.js
+++ scrapers/nus-v2/.eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   ],
 
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'warn',
 
     // Enable i++ in for loops
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
