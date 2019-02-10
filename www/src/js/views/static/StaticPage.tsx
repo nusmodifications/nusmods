b67d4f3aa366acdd3098529bee2cc360d49ca09d
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-02-10 20:08:17 +0800

    Fix StaticPage

--- www/src/js/views/static/StaticPage.tsx
+++ www/src/js/views/static/StaticPage.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import React from 'react';
 import classnames from 'classnames';
 
 import ScrollToTop from 'views/components/ScrollToTop';
@@ -7,7 +6,7 @@ import Title from 'views/components/Title';
 
 type Props = {
   title: string;
-  children: Node;
+  children: React.ReactNode;
   className?: string;
 };
 
