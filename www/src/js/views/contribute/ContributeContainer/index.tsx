464487141e55ad87cc3f3a15438759581e980c81
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-02-11 11:28:11 +0800

    Fix Contribute container

--- www/src/js/views/contribute/ContributeContainer/index.tsx
+++ www/src/js/views/contribute/ContributeContainer/index.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 
-import Loadable, { LoadingProps } from 'react-loadable';
+import Loadable, { LoadingComponentProps } from 'react-loadable';
 
 import LoadingSpinner from 'views/components/LoadingSpinner';
 import ApiError from 'views/errors/ApiError';
 import { retryImport } from 'utils/error';
 
-const AsyncContributeContainer: React.ComponentType<{}> = Loadable({
+const AsyncContributeContainer = Loadable({
   loader: () =>
     retryImport(() => import(/* webpackChunkName: "contribute" */ './ContributeContainer')),
-  loading: (props: LoadingProps) => {
+  loading: (props: LoadingComponentProps) => {
     if (props.error) {
       return <ApiError dataName="page" retry={props.retry} />;
-    } else if (props.pastDelay) {
+    }
+
+    if (props.pastDelay) {
       return <LoadingSpinner />;
     }
 
