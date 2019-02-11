5479227c6d53870d0e5be5735ea1be51d494c4f8
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-02-11 10:57:23 +0800

    Fix weekText test

--- www/src/js/utils/weekText.test.ts
+++ www/src/js/utils/weekText.test.ts
@@ -1,32 +1,38 @@
+import { AcadWeekInfo } from 'nusmoderator';
 import { noBreak } from 'utils/react';
 import { getWeekText } from './weekText';
 
-const defaultAWI = { year: '17/18', sem: 'Semester 2', type: 'Instructional', num: 2 };
+const defaultAcadWeekInfo: AcadWeekInfo = {
+  year: '17/18',
+  sem: 'Semester 2',
+  type: 'Instructional',
+  num: 2,
+};
 describe('#getWeekText()', () => {
   test('should return proper week text', () => {
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Instructional' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Instructional' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Reading' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Reading' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Reading Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Examination' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Examination' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Examination Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Recess' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Recess' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Recess Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Vacation' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Vacation' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Vacation Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Orientation' }))).toEqual(
+    expect(noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Orientation' }))).toEqual(
       noBreak('AY2017/18, Semester 2, Orientation Week 2'),
     );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Reading', num: undefined }))).toEqual(
-      noBreak('AY2017/18, Semester 2, Reading Week'),
-    );
-    expect(noBreak(getWeekText({ ...defaultAWI, type: 'Recess', num: undefined }))).toEqual(
-      noBreak('AY2017/18, Semester 2, Recess Week'),
-    );
+    expect(
+      noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Reading', num: undefined })),
+    ).toEqual(noBreak('AY2017/18, Semester 2, Reading Week'));
+    expect(
+      noBreak(getWeekText({ ...defaultAcadWeekInfo, type: 'Recess', num: undefined })),
+    ).toEqual(noBreak('AY2017/18, Semester 2, Recess Week'));
   });
 });
