5ccd962d8f3a5f661579314e4d5daae69d1a3972
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-02-18 10:50:09 +0800

    Add API function to get venue locations fro GH with fallback

--- www/src/js/apis/github.ts
+++ www/src/js/apis/github.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import qs from 'query-string';
 import { Contributor } from 'types/contributor';
+import { VenueLocationMap } from 'types/venues';
 
 // We proxy https://api.github.com/repos/nusmodifications/nusmods -> https://github.nusmods.com/repo
 // This allows us to cache the response to stop 403 rate limit error caused by the
 // school sharing a single IP address
-const baseUrl = 'https://github.nusmods.com/repo';
+const baseUrl = 'https://github.nusmods.com/';
 
 const contributorBlacklist = new Set([
   // Renovate used to report outdated dependencies as a user via the GitHub API,
@@ -13,12 +14,15 @@ const contributorBlacklist = new Set([
   25180681,
 ]);
 
+/**
+ * Get the contributors to the NUSMods repo
+ */
 export function getContributors(): Promise<Contributor[]> {
   const query = qs.stringify({
     per_page: 100,
   });
 
-  const url = `${baseUrl}/contributors?${query}`;
+  const url = `${baseUrl}/repo/contributors?${query}`;
   return axios
     .get<Contributor[]>(url)
     .then((response) =>
@@ -27,3 +31,30 @@ export function getContributors(): Promise<Contributor[]> {
       ),
     );
 }
+
+let memoizedVenuePromise: Promise<VenueLocationMap> | null = null;
+
+/**
+ * Download venue locations from the copy of data/venues.json hosted on GitHub
+ */
+export function getVenueLocations(): Promise<VenueLocationMap> {
+  if (memoizedVenuePromise) return memoizedVenuePromise;
+  const url = `${baseUrl}/venues`;
+  const promise = axios
+    .get<VenueLocationMap>(url)
+    .then((response) => response.data)
+    .catch(() =>
+      // Fall back to loading the bundled version if the venues cannot be fetched
+      import('data/venues').then((module) => module.default),
+    );
+
+  memoizedVenuePromise = promise;
+  return promise;
+}
+
+/**
+ * Clear the memoized value on getVenueLocations
+ */
+getVenueLocations.clear = () => {
+  memoizedVenuePromise = null;
+};
