1a0371423a2967b2c4fcffa3a4675b066821bff9
Amrut Prabhu <amrut08@gmail.com>
2019-02-26 19:38:50 +0800

    Add className prop to TimetableHighlight

--- www/src/js/views/timetable/TimetableHighlight.tsx
+++ www/src/js/views/timetable/TimetableHighlight.tsx
@@ -5,13 +5,14 @@ import styles from './TimetableHighlight.scss';
 
 type Props = {
   style?: React.CSSProperties;
+  className?: string;
 };
 
 /**
  * A highlighted time period in the timetable.
  */
 function TimetableHighlight(props: Props) {
-  return <div className={classnames(styles.highlight)} style={props.style} />;
+  return <div className={classnames(styles.highlight, props.className)} style={props.style} />;
 }
 
 export default TimetableHighlight;
