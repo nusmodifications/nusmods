d3934a5e8b3f377aa4bd47acbe92f0572a2cf688
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-03-01 09:41:55 +0800

    Update typing from @types

--- www/src/js/types/vendor/json2mq.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'json2mq' {
-  export interface QueryObject {
-    [property: string]: string | number | boolean;
-  }
-
-  export default function json2mq(query: QueryObject | QueryObject[]): string;
-}
