941d78c0260f27d15ab1952af22a2024d141d150
Amrut Prabhu <amrut08@gmail.com>
2019-03-01 22:16:48 +0800

    Add highlight search period in Venues page (#1583)
    * Propagate searchedPeriod props
    
    * Add type to represent Time Period
    
    * Propagate highlightPeriod value
    
    * Add TimetableHighlight component placeholder
    
    * Render TimetableHighlight component
    
    * Add TimetableHighlight css
    
    * Create style and calculate size for displayed highlight element
    
    * Display highlight behind lessons
    
    * Update timetable border timing to account for searchPeriod
    
    * Remove size prop from TimetableHighlight
    
    * Render TimetableHighlight in Row instead of Day
    
    * Clean up code
    
    * Prevent time going over 2400
    
    * Clean up
    
    * Remove unnecessary function
    
    * Fix lint
    
    * Fix lint
    
    * Remove unnecessary attributes. Rename variables and methods
    
    * Abstract out getElementStyle function
    
    * Fix unintended refactor
    
    * Add className prop to TimetableHighlight
    
    * Move TimetableHighlight to TimetableDay
    
    * Lift getHighlightPeriod up to VenueContainer
    
    * Changes based on review
    
    * Fix lint errors

--- www/src/js/views/venues/VenuesContainer.tsx
+++ www/src/js/views/venues/VenuesContainer.tsx
@@ -8,6 +8,7 @@ import qs from 'query-string';
 import { pick, mapValues, size, isEqual, get } from 'lodash';
 
 import { Venue, VenueDetailList, VenueSearchOptions } from 'types/venues';
+import { TimePeriod } from 'types/views';
 
 import deferComponentRender from 'views/hocs/deferComponentRender';
 import ApiError from 'views/errors/ApiError';
@@ -16,6 +17,11 @@ import LoadingSpinner from 'views/components/LoadingSpinner';
 import SearchBox from 'views/components/SearchBox';
 import { Clock, Map } from 'views/components/icons';
 import { venuePage } from 'views/routes/paths';
+import Modal from 'views/components/Modal';
+import Title from 'views/components/Title';
+import NoFooter from 'views/layout/NoFooter';
+import MapContext from 'views/components/map/MapContext';
+import makeResponsive from 'views/hocs/makeResponsive';
 
 import config from 'config';
 import nusmods from 'apis/nusmods';
@@ -23,11 +29,7 @@ import HistoryDebouncer from 'utils/HistoryDebouncer';
 import { searchVenue, filterAvailability, sortVenues } from 'utils/venues';
 import { breakpointDown } from 'utils/css';
 import { defer } from 'utils/react';
-import makeResponsive from 'views/hocs/makeResponsive';
-import Modal from 'views/components/Modal';
-import Title from 'views/components/Title';
-import NoFooter from 'views/layout/NoFooter';
-import MapContext from 'views/components/map/MapContext';
+import { convertIndexToTime } from 'utils/timify';
 
 import AvailabilitySearch, { defaultSearchOptions } from './AvailabilitySearch';
 import VenueList from './VenueList';
@@ -159,6 +161,22 @@ export class VenuesContainerComponent extends React.Component<Props, State> {
     });
   };
 
+  getHighlightPeriod(): TimePeriod | undefined {
+    const { isAvailabilityEnabled, searchOptions } = this.state;
+
+    if (!isAvailabilityEnabled) return undefined;
+
+    const day = searchOptions.day;
+    const startTime = convertIndexToTime(searchOptions.time * 2);
+    const endTime = convertIndexToTime(2 * (searchOptions.time + searchOptions.duration));
+
+    return {
+      day,
+      startTime,
+      endTime,
+    };
+  }
+
   selectedVenue(): Venue | null {
     const { venue } = this.props.match.params;
     if (!venue) return null;
@@ -251,14 +269,26 @@ export class VenuesContainerComponent extends React.Component<Props, State> {
       const venueDetail = venues.find(([venue]) => venue.toLowerCase() === lowercaseSelectedVenue);
       if (!venueDetail) return null;
       const [venue, availability] = venueDetail;
-      return <VenueDetails venue={venue} availability={availability} />;
+      return (
+        <VenueDetails
+          venue={venue}
+          availability={availability}
+          highlightPeriod={this.getHighlightPeriod()}
+        />
+      );
     }
 
     const [venue, availability] = matchedVenues[venueIndex];
     const [previous] = get(matchedVenues, String(venueIndex - 1), []);
     const [next] = get(matchedVenues, String(venueIndex + 1), []);
     return (
-      <VenueDetails venue={venue} availability={availability} next={next} previous={previous} />
+      <VenueDetails
+        venue={venue}
+        availability={availability}
+        next={next}
+        previous={previous}
+        highlightPeriod={this.getHighlightPeriod()}
+      />
     );
   }
 
