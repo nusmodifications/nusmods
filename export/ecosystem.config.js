401896a2307a971d0864cb9c9024fd737ec7d4bf
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-03-02 21:17:13 +0800

    Use same config file for both staging and prod

--- export/ecosystem.config.js
+++ export/ecosystem.config.js
@@ -1,22 +1,31 @@
 module.exports = {
-  apps : [{
-    name: 'Export',
-    script: 'app.js',
+  apps : [
+    {
+      name: 'Export (Staging)',
+      script: 'app.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '500M',
-    env: {
-      NODE_ENV: 'development',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3301,
+      },
     },
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3300,
+    {
+      name: 'Export',
+      script: '../../nusmods-export/app.js',
+      cwd: '../../nusmods-export',
+
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3300,
+      },
     },
-    env_staging: {
-      NODE_ENV: 'production',
-      PORT: 3301,
-    }
-  }],
+  ],
 };
--- export/ecosystem.staging.config.js
+++
@@ -1,1 +0,0 @@
-ecosystem.config.js
