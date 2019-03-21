6ea091e8163614ee12432466408f4bd54e35fb6c
Li Kai <hello+github@lik.ai>
2019-03-21 23:24:20 +0800

    Remove redundant files

--- api/hasura/src/schema.ts
+++ api/hasura/src/schema.ts
@@ -1,6 +1,5 @@
 /* tslint:disable */
 
-
 /**
  * AUTO-GENERATED FILE @ 2019-03-18 18:11:38 - DO NOT EDIT!
  *
@@ -9,37 +8,32 @@
  *
  */
 
-
 export namespace accountFields {
-    export type account_id = string;
-    export type created_at = Date;
-    export type updated_at = Date;
-    export type email = string;
-
+  export type account_id = string;
+  export type created_at = Date;
+  export type updated_at = Date;
+  export type email = string;
 }
 
 export interface account {
-    account_id: accountFields.account_id;
-    created_at: accountFields.created_at;
-    updated_at: accountFields.updated_at;
-    email: accountFields.email;
-
+  account_id: accountFields.account_id;
+  created_at: accountFields.created_at;
+  updated_at: accountFields.updated_at;
+  email: accountFields.email;
 }
 
 export namespace sessionFields {
-    export type session_id = string;
-    export type last_accessed_at = Date;
-    export type expires_at = Date;
-    export type account_id = string;
-    export type user_agent = string;
-
+  export type session_id = string;
+  export type last_accessed_at = Date;
+  export type expires_at = Date;
+  export type account_id = string;
+  export type user_agent = string;
 }
 
 export interface session {
-    session_id: sessionFields.session_id;
-    last_accessed_at: sessionFields.last_accessed_at;
-    expires_at: sessionFields.expires_at;
-    account_id: sessionFields.account_id;
-    user_agent: sessionFields.user_agent;
-
+  session_id: sessionFields.session_id;
+  last_accessed_at: sessionFields.last_accessed_at;
+  expires_at: sessionFields.expires_at;
+  account_id: sessionFields.account_id;
+  user_agent: sessionFields.user_agent;
 }
--- api/hasura/src/token.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-import config from './config';
-
-// TODO: Implement:
-// async function refresh() {}
-export default {
-  getRefreshToken,
-};
