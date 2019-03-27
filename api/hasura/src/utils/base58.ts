7f9e07f05c14c24246506b0204471ece85b9b18f
Li Kai <hello+github@lik.ai>
2019-03-28 01:59:14 +0800

    Remove non-firebase fiels

--- api/hasura/src/utils/base58.ts
+++
@@ -1,7 +0,0 @@
-import basex from 'base-x';
-
-// https://en.wikipedia.org/wiki/Base58
-const BASE_58_CHARACTERS = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
-const base58 = basex(BASE_58_CHARACTERS);
-
-export default base58;
