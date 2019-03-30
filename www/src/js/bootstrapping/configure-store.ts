57f45e48a17f090921cd118f9f155a9adfd02bd8
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-03-30 20:47:40 +0800

    Remove immutability-helper

--- www/src/js/bootstrapping/configure-store.ts
+++ www/src/js/bootstrapping/configure-store.ts
@@ -1,20 +1,12 @@
 import { createStore, applyMiddleware, compose, Store } from 'redux';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
-// @ts-ignore We're replacing immutability-helper anyway
-import update, { extend } from 'immutability-helper';
 import rootReducer, { State } from 'reducers';
 import requestsMiddleware from 'middlewares/requests-middleware';
 import ravenMiddleware from 'middlewares/raven-middleware';
 import { setAutoFreeze } from 'immer';
 
 import { FSA, GetState } from 'types/redux';
-
-// Extend immutability-helper with autovivification commands. This allows immutability-helper
-// to automatically create objects if it doesn't exist before
-// See: https://github.com/kolodny/immutability-helper#autovivification
-// @ts-ignore
-extend('$auto', (value, object) => (object ? update(object, value) : update({}, value)));
 
 // For redux-devtools-extensions - see
 // https://github.com/zalmoxisus/redux-devtools-extension
