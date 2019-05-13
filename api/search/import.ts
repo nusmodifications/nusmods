9de58ca1553db7ff51dd6f7428469f31e0f890c1
E-Liang Tan <eliang@eliangtan.com>
2019-05-13 22:46:24 +0800

    Add level analyzer to moduleCode

--- api/search/import.ts
+++ api/search/import.ts
@@ -12,6 +12,25 @@ const client = new Client({ node: 'http://localhost:9200' });
 
 const bulkBody = intersperse(data, { index: {} });
 
+// Tokenizes a string into an array of digits
+const first_digit_tokenizer = {
+  type: 'simple_pattern',
+  pattern: '[1-9]{1}',
+};
+
+// Only pick the first token
+const first_token_limit_filter = {
+  type: 'limit',
+  max_token_count: 1,
+};
+
+// Add 3 '0's to a number token
+const thousandizer_filter = {
+  type: 'pattern_replace',
+  pattern: '(\\d+)',
+  replacement: '$1000',
+};
+
 async function setup() {
   try {
     await client.indices.delete({
@@ -25,10 +44,39 @@ async function setup() {
     index: 'modules',
     include_type_name: false, // TODO: Remove when upgrading to Elasticsearch 7
     body: {
+      settings: {
+        analysis: {
+          analyzer: {
+            // An analyzer that produces a level string from a modcode, i.e.
+            // "CNS1010SX" => "1000", "CS2030" => "2000", etc.
+            level_analyzer: {
+              type: 'custom',
+              tokenizer: 'first_digit_tokenizer',
+              filter: ['first_token_limit_filter', 'thousandizer_filter'],
+            },
+          },
+          tokenizer: { first_digit_tokenizer },
+          filter: { first_token_limit_filter, thousandizer_filter },
+        },
+      },
       mappings: {
         properties: {
           workload: { type: 'text' },
           moduleCredit: { type: 'short' },
+          moduleCode: {
+            type: 'text',
+            fields: {
+              keyword: {
+                type: 'keyword',
+                ignore_above: 10,
+              },
+              level: {
+                type: 'text',
+                analyzer: 'level_analyzer',
+                fielddata: true, // To allow usage in MultiList on the frontend
+              },
+            },
+          },
         },
       },
     },
