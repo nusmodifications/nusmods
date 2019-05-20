a1b50b46c7e7389c9c5d1cdf4124df145b3aff0e
E-Liang Tan <eliang@eliangtan.com>
2019-05-20 18:46:38 +0800

    Split pager into its own component

--- website/src/views/modules/ModuleFinderContainer.tsx
+++ website/src/views/modules/ModuleFinderContainer.tsx
@@ -9,12 +9,12 @@ import {
   SearchkitManager,
   SearchkitProvider,
 } from 'searchkit';
-import classnames from 'classnames';
 
 import ModuleFinderSidebar from 'views/modules/ModuleFinderSidebar';
 import ModuleSearchBox from 'views/modules/ModuleSearchBox';
 import ModuleFinderNoHits from 'views/errors/ModuleFinderNoHits';
 import ModuleFinderApiError from 'views/errors/ModuleFinderApiError';
+import ModuleFinderPager from 'views/components/ModuleFinderPager';
 import { ModuleFinderHitModuleItem } from 'views/components/ModuleFinderItem';
 import LoadingSpinner from 'views/components/LoadingSpinner';
 import Title from 'views/components/Title';
@@ -54,35 +54,7 @@ const ModuleFinderContainer = () => {
               />
               <InitialLoader component={LoadingSpinner} />
             </ul>
-            <Pagination
-              showNumbers
-              listComponent={({ items, selectedItems, toggleItem }) => {
-                return (
-                  <nav aria-label="Module search result pagination">
-                    <ul className="pagination justify-content-center">
-                      {items.map(({ key, label, page, disabled }) => (
-                        <li
-                          key={key}
-                          className={classnames(
-                            'page-item',
-                            disabled ? 'disabled' : null,
-                            selectedItems.includes(key) ? 'active' : null,
-                          )}
-                        >
-                          <button
-                            type="button"
-                            className="page-link"
-                            onClick={() => toggleItem(key)}
-                          >
-                            {label}
-                          </button>
-                        </li>
-                      ))}
-                    </ul>
-                  </nav>
-                );
-              }}
-            />
+            <Pagination showNumbers listComponent={ModuleFinderPager} />
           </div>
 
           <div className="col-md-4 col-lg-3">
