1d4fbb1d0f1660ea929cb2a6d5f063f142202159
E-Liang Tan <eliang@eliangtan.com>
2019-06-16 21:00:45 +0800

    Add tests for ModuleFinderPagerButton

---
+++ website/src/views/components/ModuleFinderPagerButton.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { noop } from 'lodash';
+
+import ModuleFinderPagerButton from './ModuleFinderPagerButton';
+import styles from './ModuleFinderPagerButton.scss';
+
+describe(ModuleFinderPagerButton, () => {
+  const defaultProps = {
+    onClick: noop,
+    children: null,
+  };
+
+  test('should render inactive and enabled by default', () => {
+    const componentWrapper = shallow(<ModuleFinderPagerButton {...defaultProps} />);
+    expect(componentWrapper.exists('button')).toBe(true);
+    const button = componentWrapper.find('button');
+    expect(button.hasClass(styles.active)).toBe(false);
+    expect(button.hasClass(styles.disabled)).toBe(false);
+    expect(button.prop('disabled')).toBeFalsy();
+  });
+
+  test('should render active button', () => {
+    const componentWrapper = shallow(<ModuleFinderPagerButton {...defaultProps} active />);
+    const button = componentWrapper.find('button');
+    expect(button.hasClass(styles.active)).toBe(true);
+  });
+
+  test('should render disabled button', () => {
+    const componentWrapper = shallow(<ModuleFinderPagerButton {...defaultProps} disabled />);
+    const button = componentWrapper.find('button');
+    expect(button.hasClass(styles.disabled)).toBe(true);
+    expect(button.prop('disabled')).toBe(true);
+  });
+
+  test('should respond to clicks', () => {
+    const onClick = jest.fn();
+    const componentWrapper = shallow(
+      <ModuleFinderPagerButton {...defaultProps} onClick={onClick} />,
+    );
+    componentWrapper.find('button').simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
