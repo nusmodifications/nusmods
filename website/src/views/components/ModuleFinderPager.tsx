915f3026ef638f5b4fddc0e62254a23f40d90009
E-Liang Tan <eliang@eliangtan.com>
2019-06-23 15:02:57 +0800

    Implement pager component on module search page (#1856)
    * Implement custom Pagination component and update custom ModuleFinderPager
    
    Searchkit's Pagination component does not support last pages, and does
    not have go to first and go to last buttons. It is also too opinionated
    to customize. Most of our reimplementation is adapted from theirs.
    
    * Add multi-chevron icons
    
    * Style pager
    
    * Add tests for ModuleFinderPagerComponent
    
    * Add tests for ModuleFinderPagerButton
    
    * Add tooltips to pager arrow buttons

--- website/src/views/components/ModuleFinderPager.tsx
+++ website/src/views/components/ModuleFinderPager.tsx
@@ -1,33 +1,111 @@
 import * as React from 'react';
 import classnames from 'classnames';
+import { range } from 'lodash';
 
-type Props = {
-  items: { key: string; label: string; disabled: boolean }[];
-  selectedItems: string[];
-  toggleItem: (key: string) => void;
+import { breakpointDown } from 'utils/css';
+import makeResponsive from 'views/hocs/makeResponsive';
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'views/components/icons';
+import { PagerProps, FIRST_PAGE_INDEX } from 'views/components/searchkit/Pagination';
+import ModuleFinderPagerButton from 'views/components/ModuleFinderPagerButton';
+
+import styles from './ModuleFinderPager.scss';
+
+// Calculate first and last page nums to display in pager.
+export function displayPageRange(
+  selectedPage: number,
+  totalNumPages: number,
+  numVisiblePages: number,
+) {
+  if (selectedPage === 0 || numVisiblePages === 0 || totalNumPages === 0) return null;
+
+  let firstPageNum = selectedPage - Math.floor(numVisiblePages / 2);
+  let lastPageNum = firstPageNum + numVisiblePages - 1;
+  if (firstPageNum < FIRST_PAGE_INDEX) {
+    lastPageNum += FIRST_PAGE_INDEX - firstPageNum;
+    firstPageNum = FIRST_PAGE_INDEX;
+  }
+  lastPageNum = Math.min(totalNumPages, lastPageNum);
+  return { firstPageNum, lastPageNum };
+}
+
+type Props = PagerProps & {
+  readonly matchBreakpoint: boolean;
 };
 
-const ModuleFinderPager: React.FC<Props> = ({ items, selectedItems, toggleItem }) => {
+export const ModuleFinderPagerComponent: React.FC<Props> = ({
+  selectedPage,
+  totalNumPages,
+  onGoToFirst,
+  onGoToPrevious,
+  onGoToPage,
+  onGoToNext,
+  onGoToLast,
+  matchBreakpoint,
+}) => {
+  if (totalNumPages <= 0) return null;
+
+  function renderDesktopPages() {
+    // Number of pages to show in the pager
+    const numVisiblePages = 7;
+    const startEndPages = displayPageRange(selectedPage, totalNumPages, numVisiblePages);
+    if (!startEndPages) return null;
+
+    const { firstPageNum, lastPageNum } = startEndPages;
+    const pageRange = range(firstPageNum, lastPageNum + 1);
+
+    return pageRange.map((pageNum) => (
+      <ModuleFinderPagerButton
+        key={pageNum}
+        active={selectedPage === pageNum}
+        onClick={() => onGoToPage(pageNum)}
+      >
+        {pageNum}
+      </ModuleFinderPagerButton>
+    ));
+  }
+
+  function renderMobilePages() {
+    return <span className={styles.mobilePages}>Page {selectedPage}</span>;
+  }
+
   return (
     <nav aria-label="Module search result pagination">
-      <ul className="pagination justify-content-center">
-        {items.map(({ key, label, disabled }) => (
-          <li
-            key={key}
-            className={classnames(
-              'page-item',
-              disabled ? 'disabled' : null,
-              selectedItems.includes(key) ? 'active' : null,
-            )}
-          >
-            <button type="button" className="page-link" onClick={() => toggleItem(key)}>
-              {label}
-            </button>
-          </li>
-        ))}
+      <ul className={classnames('pagination justify-content-center', styles.paginationList)}>
+        <ModuleFinderPagerButton
+          tooltipTitle="First page"
+          disabled={selectedPage === FIRST_PAGE_INDEX}
+          onClick={onGoToFirst}
+        >
+          <ChevronsLeft className={styles.svg} />
+        </ModuleFinderPagerButton>
+        <ModuleFinderPagerButton
+          tooltipTitle="Previous page"
+          disabled={selectedPage === FIRST_PAGE_INDEX}
+          onClick={onGoToPrevious}
+        >
+          <ChevronLeft className={styles.svg} />
+        </ModuleFinderPagerButton>
+
+        {matchBreakpoint ? renderMobilePages() : renderDesktopPages()}
+
+        <ModuleFinderPagerButton
+          tooltipTitle="Next page"
+          disabled={selectedPage === totalNumPages}
+          onClick={onGoToNext}
+        >
+          <ChevronRight className={styles.svg} />
+        </ModuleFinderPagerButton>
+        <ModuleFinderPagerButton
+          tooltipTitle="Last page"
+          disabled={selectedPage === totalNumPages}
+          onClick={onGoToLast}
+        >
+          <ChevronsRight className={styles.svg} />
+        </ModuleFinderPagerButton>
       </ul>
     </nav>
   );
 };
 
+const ModuleFinderPager = makeResponsive(ModuleFinderPagerComponent, breakpointDown('md'));
 export default ModuleFinderPager;
