8e76af2e407f602dcecab538804009b6de280196
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-07-05 21:12:33 -0700

    Fix odd week logic for formatNumericWeeks (#1908)
    * Fix odd week logic for formatNumericWeeks
    
    * Fix odd/even week logic to be even more correcter

--- website/src/test-utils/timetable.ts
+++ website/src/test-utils/timetable.ts
@@ -3,7 +3,7 @@ import { ColoredLesson, Lesson } from 'types/timetables';
 
 export const EVERY_WEEK = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 export const EVEN_WEEK = [2, 4, 6, 8, 10, 12];
-export const ODD_WEEK = [1, 3, 5, 7, 9, 11];
+export const ODD_WEEK = [1, 3, 5, 7, 9, 11, 13];
 
 // A generic lesson with some default.
 export function createGenericLesson(
