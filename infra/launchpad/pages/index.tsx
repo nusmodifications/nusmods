ad73bbaf82003cae71764c4ce1d9224a2bcbb3d2
E-Liang Tan <eliang@eliangtan.com>
2019-07-07 18:03:57 +0800

    Reorganize and create bare /api endpoint

--- infra/launchpad/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>Welcome to Next.js!</div>;
-}
-
-export default Home;
---
+++ infra/launchpad/src/web/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+
+type Props = {};
+
+function Home(props: Props) {
+  return (
+    <>
+      <h1>Launchpad 2</h1>
+    </>
+  );
+}
+
+export default Home;
