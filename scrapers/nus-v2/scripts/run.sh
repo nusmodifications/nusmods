f4ab308d2926c5fb4f2acaf2d337fa5a5d2c919c
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-07-14 14:25:06 -0700

    Update run.sh

--- scrapers/nus-v2/scripts/run.sh
+++ scrapers/nus-v2/scripts/run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
+# Print date for logs
+date
+
 # Ensure cwd is the root of the v2 scraper project
 cd "$(dirname "$0")"
 cd ..
 
 # Run the scraper
 yarn build
+echo "Running scraper"
 node build/index.js all
 
 # Update docs
 yarn docs
 
 # Sync with live data
+echo "Syncing data"
 rsync -ahz --delete-after --exclude='cache/' data/ ../../../api.nusmods.com/v2
