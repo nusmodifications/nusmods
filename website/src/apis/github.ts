cc99458d3168586d38e1335987e207b704b5889a
Zhang Yijiang <mediumdeviation@gmail.com>
2019-07-18 21:22:38 -0700

    Allow devs to force local venues instead of loading from GitHub

--- website/src/apis/github.ts
+++ website/src/apis/github.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import qs from 'query-string';
+import { useRepoVenues } from 'utils/debug';
 import { Contributor } from 'types/contributor';
 import { VenueLocationMap } from 'types/venues';
 
@@ -38,6 +39,7 @@ let memoizedVenuePromise: Promise<VenueLocationMap> | null = null;
  * Download venue locations from the copy of data/venues.json hosted on GitHub
  */
 export function getVenueLocations(): Promise<VenueLocationMap> {
+  if (useRepoVenues()) return import('data/venues').then((module) => module.default);
   if (memoizedVenuePromise) return memoizedVenuePromise;
   const url = `${baseUrl}/venues`;
   const promise = axios
