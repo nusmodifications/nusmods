aaccd5b0c5aa457e8ddee91ef4e8b949a8076e60
Li Kai <hello+github@lik.ai>
2019-07-20 15:58:40 +0800

    Run docker-compose detached

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -4,14 +4,14 @@ git pull
 
 # Build and deploy green, we have 2 versions now
 docker-compose --project-name=green  -f docker-compose.prod.yml build --no-cache
-docker-compose --project-name=green  -f docker-compose.prod.yml up
+docker-compose --project-name=green  -f docker-compose.prod.yml up -d
 
 # Probably wait a while here
 
 # Build the new blue and restart blue, bringing it to latest
 docker-compose --project-name=blue -f docker-compose.prod.yml build --no-cache
 docker-compose --project-name=blue -f docker-compose.prod.yml down --remove-orphans
-docker-compose --project-name=blue -f docker-compose.prod.yml up
+docker-compose --project-name=blue -f docker-compose.prod.yml up -d
 
 # Probably wait a while here
 
