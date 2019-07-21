cea198f4b16f1cc88fed16dc84ca991981e9d959
Your Name <you@example.com>
2019-07-21 17:01:51 +0000

    Fix tls config

--- prod-up.sh
+++ prod-up.sh
@@ -10,6 +10,7 @@ set -x
 
 # Start docker-compose
 export GIT_COMMIT_HASH=$(git rev-parse HEAD)
+mkdir -p ~/data/traefik && touch acme.json && sudo chmod 600 acme.json
 docker-compose --project-name=machine -f infra/machine/docker-compose.yml build --no-cache
 docker-compose --project-name=machine -f infra/machine/docker-compose.yml up -d
 docker-compose --project-name=blue    -f docker-compose.prod.yml          build --no-cache
