3f42decf7706fe638297f1f83d6887dfe48f8d6e
Zhang Yi Jiang <mediumdeviation@gmail.com>
2019-08-10 15:05:00 -0700

    Should only support Safari Mobile 10+

--- website/src/bootstrapping/browser.ts
+++ website/src/bootstrapping/browser.ts
@@ -1,17 +1,13 @@
 import bowser from 'bowser';
 
-import { parseFloat } from 'types/utils';
-
 /* eslint-disable import/prefer-default-export */
 
-export const isBrowserSupported =
-  bowser.check(
-    {
-      msedge: '14',
-      chrome: '56',
-      firefox: '52',
-      safari: '10',
-    },
-    true,
-  ) ||
-  (bowser.ios && parseFloat(bowser.osversion) >= 10);
+export const isBrowserSupported = bowser.check(
+  {
+    msedge: '14',
+    chrome: '56',
+    firefox: '52',
+    safari: '10',
+  },
+  true,
+);
