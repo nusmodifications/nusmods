dbd9c40e7c1b546a1070c7a31890b02c90e9d9ca
E-Liang Tan <eliang@eliangtan.com>
2020-05-07 18:43:51 +0800

    Fix comment format nit

--- website/babel.config.js
+++ website/babel.config.js
@@ -37,7 +37,8 @@ module.exports = (api) => {
 
   if (IS_DEV || IS_PROD) {
     plugins.push(['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]);
-    // In production this cleans up hot reload code - https://github.com/gaearon/react-hot-loader#what-about-production
+    // In production this cleans up hot reload code
+    // See https://github.com/gaearon/react-hot-loader#what-about-production
     plugins.push('react-hot-loader/babel');
   }
 
