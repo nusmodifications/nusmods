9bbf263d0b0cb20cef934a4ca29c57dc10c4f4b5
E-Liang Tan <eliang@eliangtan.com>
2020-05-07 17:48:29 +0800

    Remove unused/redundant util types and clean up utils

--- website/src/types/utils.ts
+++ website/src/types/utils.ts
@@ -1,13 +1,8 @@
-/**
- * Various utility types and type utilities for making working with TypeScript
- * easier
- */
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// Various utility types and type utilities for making working with TypeScript
+// easier
 
 export type Subtract<T extends K, K> = Omit<T, keyof K>;
 
-export type Filter<T, U> = T extends U ? T : never; // Remove types from T that are not assignable to U
 export type FunctionPropertyNames<T> = {
   [K in keyof T]: T[K] extends Function ? K : never;
 }[keyof T];
@@ -19,8 +14,7 @@ export type Values<T extends {}> = T[keyof T];
 
 export const EMPTY_ARRAY: readonly unknown[] = [];
 
-export const notFalsy = (Boolean as any) as <T>(x: T | false) => x is T;
-export const notNull = <T>(x: T | null | undefined): x is T => x != null;
+export const notNull = <T>(x: T | null | undefined): x is T => x !== null;
 
 // TypeScript by default only allows string in parseFloat, even though this works fine
 export const parseFloat = (float: number | string): number =>
