663f9d2a4808463cac635ba76edffd33ce2fae49
E-Liang Tan <eliang@eliangtan.com>
2020-05-23 15:41:31 +0800

    Add tests for CommentCount

---
+++ website/src/views/components/disqus/CommentCount.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { CommentCountComponent } from './CommentCount';
+import insertScript from 'utils/insertScript';
+
+// const mockInsertScript = jest.fn();
+jest.mock('utils/insertScript', () => jest.fn().mockResolvedValueOnce('fuck'));
+
+const disqusConfig = {
+  url: 'https://nusmods.com/modules/CS1010/reviews',
+  identifier: 'CS1010',
+  title: 'CS1010 Programming Methodology',
+};
+
+describe(CommentCountComponent, () => {
+  test('should be disabled (render null) if user has enabled loadDisqusManually', () => {
+    const wrapper = mount(<CommentCountComponent loadDisqusManually {...disqusConfig} />);
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  test('should insert Disqus script if loadDisqusManually is false', () => {
+    (insertScript as jest.MockedFunction<typeof insertScript>).mockResolvedValueOnce(
+      new Event('mock event'),
+    );
+    mount(<CommentCountComponent loadDisqusManually={false} {...disqusConfig} />);
+    expect(insertScript).toBeCalled();
+  });
+});
