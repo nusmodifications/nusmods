ed6226d4f56bbd4c99bb53456a29a9ef9e1eef86
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-26 21:58:27 +0800

    Add left and right button to timetable timing, for display greater than sm

--- website/src/views/timetable/TimetableTimings.tsx
+++ website/src/views/timetable/TimetableTimings.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { range } from 'lodash';
+import classnames from 'classnames';
+
+import { ChevronLeft, ChevronRight } from 'react-feather';
 
 import { convertIndexToTime } from 'utils/timify';
 import styles from './TimetableTimings.scss';
@@ -13,20 +16,36 @@ const TimetableTimings: React.FC<Props> = (props) => {
   const indices = range(props.startingIndex, props.endingIndex);
 
   return (
-    <div className={styles.timings}>
-      {indices.map((i) => {
-        const time = convertIndexToTime(i);
+    <div>
+      <button
+        className={classnames('btn btn-inline', styles.buttonLeft)}
+        type="button"
+        aria-label="Previous Hours"
+      >
+        <ChevronLeft />
+      </button>
+      <div className={styles.timings}>
+        {indices.map((i) => {
+          const time = convertIndexToTime(i);
 
-        // Only mark even ticks
-        if (i % 2 === 1) return null;
+          // Only mark even ticks
+          if (i % 2 === 1) return null;
 
-        return (
-          <time key={time} className={styles.time}>
-            {time}
-          </time>
-        );
-      })}
-      <span />
+          return (
+            <time key={time} className={styles.time}>
+              {time}
+            </time>
+          );
+        })}
+        <span />
+      </div>
+      <button
+        className={classnames('btn btn-inline', styles.buttonRight)}
+        type="button"
+        aria-label="Previous Hours"
+      >
+        <ChevronRight />
+      </button>
     </div>
   );
 };
