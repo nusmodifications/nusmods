385765ec4d91b24dd86cd214810ddac8f80cacd0
ZhangYiJiang <mediumdeviation@gmail.com>
2020-07-25 20:04:35 -0700

    Use putMappings and putSettings to update existing indices

--- scrapers/nus-v2/src/services/io/elastic.ts
+++ scrapers/nus-v2/src/services/io/elastic.ts
@@ -47,46 +47,6 @@ async function createIndex(client: Client): Promise<Client> {
     await client.indices.create({
       index: INDEX_NAME,
       include_type_name: false, // TODO: Remove when upgrading to Elasticsearch 7
-      body: {
-        settings: {
-          analysis: {
-            analyzer: {
-              // An analyzer that produces a level string from a modcode, i.e.
-              // "CNS1010SX" => "1000", "CS2030" => "2000", etc.
-              level_analyzer: {
-                type: 'custom',
-                tokenizer: 'first_digit_tokenizer',
-                filter: ['first_token_limit_filter', 'thousandizer_filter'],
-              },
-            },
-            tokenizer: { first_digit_tokenizer },
-            filter: { first_token_limit_filter, thousandizer_filter },
-          },
-          index: {
-            max_result_window: 20000, // Default limit is 10k, but we have >11k mods
-          },
-        },
-        mappings: {
-          properties: {
-            workload: { type: 'text' },
-            moduleCredit: { type: 'short' },
-            moduleCode: {
-              type: 'text',
-              fields: {
-                keyword: {
-                  type: 'keyword',
-                  ignore_above: 10,
-                },
-                level: {
-                  type: 'text',
-                  analyzer: 'level_analyzer',
-                  fielddata: true, // To allow usage in MultiList on the frontend
-                },
-              },
-            },
-          },
-        },
-      },
     });
   } catch (e) {
     // Ignore resource exist exception as we will handle cases where the index
@@ -98,6 +58,53 @@ async function createIndex(client: Client): Promise<Client> {
       throw e;
     }
   }
+
+  await client.indices.putSettings({
+    index: INDEX_NAME,
+    body: {
+      analysis: {
+        analyzer: {
+          // An analyzer that produces a level string from a modcode, i.e.
+          // "CNS1010SX" => "1000", "CS2030" => "2000", etc.
+          level_analyzer: {
+            type: 'custom',
+            tokenizer: 'first_digit_tokenizer',
+            filter: ['first_token_limit_filter', 'thousandizer_filter'],
+          },
+        },
+        tokenizer: { first_digit_tokenizer },
+        filter: { first_token_limit_filter, thousandizer_filter },
+      },
+      index: {
+        max_result_window: 20000, // Default limit is 10k, but we have >11k mods
+      },
+    },
+  });
+
+  await client.indices.putMapping({
+    index: INDEX_NAME,
+    include_type_name: false, // TODO: Remove when upgrading to Elasticsearch 7
+    body: {
+      properties: {
+        workload: { type: 'text' },
+        moduleCredit: { type: 'short' },
+        moduleCode: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 10,
+            },
+            level: {
+              type: 'text',
+              analyzer: 'level_analyzer',
+              fielddata: true, // To allow usage in MultiList on the frontend
+            },
+          },
+        },
+      },
+    },
+  });
 
   return client;
 }
