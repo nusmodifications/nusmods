0f9844a2faf16073a85483c648443cada503efe2
E-Liang Tan <eliang@eliangtan.com>
2020-10-20 13:45:49 +0800

    Implement retryableLazy-based loading

--- website/src/views/tetris/TetrisContainer.tsx
+++ website/src/views/tetris/TetrisContainer.tsx
@@ -1,59 +1,54 @@
-import * as React from 'react';
-import Loadable, { LoadingComponentProps } from 'react-loadable';
+import React, { lazy, memo, Suspense, useCallback, useState } from 'react';
 
-import type { EmptyProps } from 'types/utils';
 import ApiError from 'views/errors/ApiError';
 import LoadingSpinner from 'views/components/LoadingSpinner';
+import ErrorBoundary from 'views/errors/ErrorBoundary';
 import type { Props as TetrisGameProps } from './TetrisGame';
 
-type Props = {
-  readonly TetrisGame: React.ComponentType<TetrisGameProps>;
-};
-
-type State = {
-  readonly game: number;
-};
-
-/**
- * Wrapper around TetrisGame which resets the game's internal state and components
- * by forcing a remount after each game via the key prop
- */
-class TetrisContainer extends React.PureComponent<Props, State> {
-  state = {
-    game: 0,
-  };
-
-  onResetGame = () => {
-    this.setState((state) => ({ game: state.game + 1 }));
-  };
-
-  render() {
-    const { TetrisGame } = this.props;
-    // Force a re-mount of the component, resetting its state by changing its key
-    return <TetrisGame resetGame={this.onResetGame} key={this.state.game} />;
-  }
+// Source: https://github.com/facebook/react/issues/14254#issuecomment-441717770
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function retryableLazy<T extends React.ComponentType<any>>(
+  lazyImport: () => Promise<{ default: T }>,
+  setComponent: (component: React.LazyExoticComponent<T>) => void,
+) {
+  setComponent(
+    lazy(() =>
+      lazyImport().catch((err) => {
+        retryableLazy(lazyImport, setComponent);
+        throw err;
+      }),
+    ),
+  );
 }
 
+let TetrisGame: React.ComponentType<TetrisGameProps>;
+retryableLazy(
+  () => import(/* webpackChunkName: "tetris" */ './TetrisGame'),
+  (component) => {
+    TetrisGame = component;
+  },
+);
+
 /**
- * Lazy load the TetrisGame component and pass it down to TetrisContainer
+ * Wrapper around TetrisGame which:
+ * - Lazy loads the TetrisGame component.
+ * - Resets the game's internal state and components by forcing a remount after
+ *   each game via the key prop
  */
-type Export = { TetrisGame: { default: React.ComponentType<TetrisGameProps> } };
-export default Loadable.Map<EmptyProps, Export>({
-  loader: {
-    TetrisGame: () => import(/* webpackChunkName: "tetris" */ './TetrisGame'),
-  },
-  loading: (props: LoadingComponentProps) => {
-    if (props.error) {
-      return <ApiError dataName="the page" retry={props.retry} />;
-    }
+const TetrisContainer = memo(() => {
+  const [game, setGame] = useState(0);
+  const onResetGame = useCallback(() => setGame(game + 1), [game]);
 
-    if (props.pastDelay) {
-      return <LoadingSpinner />;
-    }
-
-    return null;
-  },
-  render(loaded, props) {
-    return <TetrisContainer TetrisGame={loaded.TetrisGame.default} {...props} />;
-  },
+  return (
+    <ErrorBoundary
+      key={game}
+      errorPage={() => <ApiError dataName="the page" retry={onResetGame} />}
+    >
+      <Suspense fallback={<LoadingSpinner />}>
+        <TetrisGame resetGame={onResetGame} />;
+      </Suspense>
+    </ErrorBoundary>
+  );
 });
+
+export default TetrisContainer;
