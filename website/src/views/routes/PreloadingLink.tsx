82bae87f2388ebce971e231ed6386bb53dea29f4
E-Liang Tan <eliang@eliangtan.com>
2020-10-22 22:39:05 +0800

    Allow onMouseOver, onFocus and ref passthrough on Preloading(Nav)Link

--- website/src/views/routes/PreloadingLink.tsx
+++ website/src/views/routes/PreloadingLink.tsx
@@ -1,24 +1,42 @@
-import React, { useCallback, useContext } from 'react';
+import React, { forwardRef, useCallback, useContext } from 'react';
 import { LinkProps, Link, NavLink, NavLinkProps } from 'react-router-dom';
 import { RoutePreloaderContext } from './RoutePreloaderContext';
 
-function usePreloadCallbacks(to: LinkProps['to'] | NavLinkProps['to']) {
+function usePreloadCallbacks({ onMouseOver, onFocus, to }: LinkProps | NavLinkProps) {
   const preloaderContext = useContext(RoutePreloaderContext);
   if (!preloaderContext) {
     throw new Error('Preloading link components cannot be used outside RoutePreloaderContext.');
   }
   const preloadCode = useCallback(() => preloaderContext.preloadCode(to), [preloaderContext, to]);
-  return { preloadCode };
+
+  const combinedOnMouseOver = useCallback(
+    (e) => {
+      onMouseOver?.(e);
+      preloadCode();
+    },
+    [onMouseOver, preloadCode],
+  );
+  const combinedOnFocus = useCallback(
+    (e) => {
+      onFocus?.(e);
+      preloadCode();
+    },
+    [onFocus, preloadCode],
+  );
+
+  return { onMouseOver: combinedOnMouseOver, onFocus: combinedOnFocus };
 }
 
-export const PreloadingLink: React.FC<Omit<LinkProps, 'onMouseOver' | 'onFocus'>> = (props) => {
-  const { preloadCode } = usePreloadCallbacks(props.to);
-  return <Link {...props} onMouseOver={preloadCode} onFocus={preloadCode} />;
-};
+export const PreloadingLink = forwardRef<HTMLAnchorElement, LinkProps>(
+  function PreloadingLinkComponent(props, ref) {
+    const preloadCallbacks = usePreloadCallbacks(props);
+    return <Link {...props} {...preloadCallbacks} ref={ref} />;
+  },
+);
 
-export const PreloadingNavLink: React.FC<Omit<NavLinkProps, 'onMouseOver' | 'onFocus'>> = (
-  props,
-) => {
-  const { preloadCode } = usePreloadCallbacks(props.to);
-  return <NavLink {...props} onMouseOver={preloadCode} onFocus={preloadCode} />;
-};
+export const PreloadingNavLink = forwardRef<HTMLAnchorElement, LinkProps>(
+  function PreloadingNavLinkComponent(props, ref) {
+    const preloadCallbacks = usePreloadCallbacks(props);
+    return <NavLink {...props} {...preloadCallbacks} ref={ref} />;
+  },
+);
