d9f468be2f2a9f95c8f9f11534da7e8199488e9c
E-Liang Tan <eliang@eliangtan.com>
2020-11-10 13:35:11 +0800

    Functionalize RandomKawaii

--- website/src/views/components/RandomKawaii.tsx
+++ website/src/views/components/RandomKawaii.tsx
@@ -1,48 +1,20 @@
-import * as React from 'react';
+import { FC, HTMLAttributes, useState } from 'react';
 import { sample } from 'lodash';
 import { SpeechBubble, Mug, Browser, Ghost, KawaiiMood, KawaiiProps } from 'react-kawaii';
 
-type Props = {
-  size?: number;
-  mood?: KawaiiMood;
-  color?: string;
-  title?: string;
-  'aria-label'?: string;
-};
+type Props = HTMLAttributes<HTMLDivElement> & KawaiiProps;
 
 const icons = [SpeechBubble, Mug, Browser, Ghost];
 const defaultMoods: KawaiiMood[] = ['ko', 'sad', 'shocked'];
 
-class RandomKawaii extends React.PureComponent<Props> {
-  kawaii: React.ComponentType<KawaiiProps>;
-
-  defaultMood: KawaiiMood;
-
-  static defaultProps = {
-    mood: null,
-    color: '#FF715D',
-  };
-
-  constructor(props: Props) {
-    super(props);
-
-    /* eslint-disable @typescript-eslint/no-non-null-assertion */
-    this.kawaii = sample(icons)!;
-    this.defaultMood = sample(defaultMoods)!;
-    /* eslint-enable */
-  }
-
-  render() {
-    const { size, mood, color, ...wrapperProps } = this.props;
-    const Kawaii = this.kawaii;
-    const moodProp = mood || this.defaultMood;
-
-    return (
-      <div {...wrapperProps}>
-        <Kawaii size={size} mood={moodProp} color={color} />
-      </div>
-    );
-  }
-}
+const RandomKawaii: FC<Props> = ({ size, color = '#FF715D', mood, ...wrapperProps }) => {
+  const [Kawaii] = useState(() => sample(icons) ?? icons[0]);
+  const [defaultMood] = useState(() => sample(defaultMoods) ?? defaultMoods[0]);
+  return (
+    <div {...wrapperProps}>
+      <Kawaii size={size} color={color} mood={mood || defaultMood} />
+    </div>
+  );
+};
 
 export default RandomKawaii;
