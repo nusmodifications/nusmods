4559d71256329c4cdc9d8f56a55d3528b5bf29f7
E-Liang Tan <eliang@eliangtan.com>
2020-11-30 18:21:03 +0800

    Add React Testing Library
    For a more modern testing API that:
    
    1. Discourages testing implementation details.
    2. Makes it easier to test user-visible behavior.

--- website/jest.common.config.js
+++ website/jest.common.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   moduleFileExtensions: ['jsx', 'js', 'ts', 'tsx'],
-  setupFiles: ['<rootDir>/scripts/test.js'],
+  setupFilesAfterEnv: ['<rootDir>/scripts/test.js'],
   moduleNameMapper: {
     // Mock non JS files as strings
     '\\.(?:jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
