3ee467bec508334924f6ba37b944afb5b4b02012
E-Liang Tan <eliang@eliangtan.com>
2020-12-03 11:28:48 +0800

    Reduce test output noise by mocking react-feather in tests

--- website/src/views/components/__mocks__/RandomKawaii.tsx
+++ website/src/views/components/__mocks__/RandomKawaii.tsx
@@ -1,4 +1,4 @@
 import type { FC } from 'react';
 
-const MockRandomKawaii: FC = () => <>RandomKawaii component</>;
+const MockRandomKawaii: FC = jest.fn(() => <div data-testid="RandomKawaii component" />);
 export default MockRandomKawaii;
