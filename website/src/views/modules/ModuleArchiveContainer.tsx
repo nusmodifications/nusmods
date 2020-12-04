e4f5ae1e8671251db645b6826f2e9e40e8678cf9
E-Liang Tan <eliang@eliangtan.com>
2020-12-04 12:43:33 +0800

    Functionalize ModuleArchiveContainer

--- website/src/views/modules/ModuleArchiveContainer.tsx
+++ website/src/views/modules/ModuleArchiveContainer.tsx
@@ -1,13 +1,12 @@
-import * as React from 'react';
-import { AxiosError } from 'axios';
-import { connect, MapDispatchToPropsNonObject } from 'react-redux';
-import { match as Match, Redirect, RouteComponentProps, withRouter } from 'react-router-dom';
-import deferComponentRender from 'views/hocs/deferComponentRender';
+import { ComponentType, FC, useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { match as Match, Redirect, useLocation, useRouteMatch } from 'react-router-dom';
 import { get } from 'lodash';
 
-import type { Module, ModuleCode } from 'types/modules';
-import type { State as StoreState } from 'types/state';
+import type { AxiosError } from 'axios';
+import type { ModuleCode } from 'types/modules';
 import type { Dispatch } from 'types/redux';
+import type { State } from 'types/state';
 
 import { fetchModuleArchive } from 'actions/moduleBank';
 import { captureException } from 'utils/error';
@@ -15,32 +14,23 @@ import retryImport from 'utils/retryImport';
 import ApiError from 'views/errors/ApiError';
 import ModuleNotFoundPage from 'views/errors/ModuleNotFoundPage';
 import LoadingSpinner from 'views/components/LoadingSpinner';
+import deferComponentRender from 'views/hocs/deferComponentRender';
 import { moduleArchive } from 'views/routes/paths';
 
-import { Props as ModulePageContentProp } from './ModulePageContent';
+import type { Props as ModulePageContentProps } from './ModulePageContent';
 
 type Params = {
   year: string;
   moduleCode: string;
 };
 
-type OwnProps = RouteComponentProps<Params>;
-
-type DispatchProps = {
-  fetchModule: () => Promise<Module>;
-};
-
-type Props = OwnProps &
-  DispatchProps & {
-    module: Module | null;
-    moduleCode: ModuleCode;
-    archiveYear: string;
+function getPropsFromMatch(match: Match<Params>) {
+  const { year = '', moduleCode = '' } = match.params;
+  return {
+    moduleCode: moduleCode.toUpperCase(),
+    year: year.replace('-', '/'),
   };
-
-type State = {
-  ModulePageContent: React.ComponentType<ModulePageContentProp> | null;
-  error?: Error;
-};
+}
 
 /**
  * Wrapper component for the archive page that handles data fetching and error handling.
@@ -49,111 +39,85 @@ type State = {
  * error handling - the normal page tries to check the archives if this year's
  * API returns 404, while this page doesn't.
  */
-class ModuleArchiveContainer extends React.PureComponent<Props, State> {
-  state: State = {
-    ModulePageContent: null,
-  };
+export const ModuleArchiveContainerComponent: FC = () => {
+  const [
+    ModulePageContent,
+    setModulePageContent,
+  ] = useState<ComponentType<ModulePageContentProps> | null>(null);
+  const [error, setError] = useState<Error | undefined>();
 
-  componentDidMount() {
-    this.fetchModule();
-    this.fetchPageImport();
+  const dispatch = useDispatch<Dispatch>();
+
+  const handleFetchError = useCallback((fetchError: AxiosError) => {
+    setError(fetchError);
+    captureException(fetchError);
+  }, []);
+
+  useEffect(
+    () => {
+      // Try importing ModulePageContent thrice if we're online and
+      // getting the "Loading chunk x failed." error.
+      retryImport(() => import(/* webpackChunkName: "module" */ 'views/modules/ModulePageContent'))
+        .then((module) => setModulePageContent(() => module.default))
+        .catch(handleFetchError);
+    },
+    // Only load dynamic import on first mount.
+    // Assume `handleFetchError` will never change.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  const match = useRouteMatch<Params>();
+  const { moduleCode, year } = getPropsFromMatch(match);
+
+  // Fetch module on mount and when params are updated
+  const fetchModuleForDisplay = useCallback(() => {
+    setError(undefined);
+    dispatch(fetchModuleArchive(moduleCode, year)).catch(handleFetchError);
+  }, [dispatch, handleFetchError, moduleCode, year]);
+
+  const previousModuleCode = useRef<ModuleCode>();
+  const previousArchiveYear = useRef<string>();
+  useEffect(() => {
+    if (previousModuleCode.current !== moduleCode || previousArchiveYear.current !== year) {
+      previousModuleCode.current = moduleCode;
+      previousArchiveYear.current = year;
+      fetchModuleForDisplay();
+    }
+  }, [fetchModuleForDisplay, moduleCode, year]);
+
+  const location = useLocation();
+
+  const module = useSelector(({ moduleBank }: State) =>
+    get(moduleBank.moduleArchive, [moduleCode, year], null),
+  );
+
+  if (get(error, ['response', 'status'], 200) === 404) {
+    return <ModuleNotFoundPage moduleCode={moduleCode} tryArchive={false} />;
   }
 
-  componentDidUpdate(prevProps: Props) {
-    if (
-      prevProps.moduleCode !== this.props.moduleCode ||
-      prevProps.archiveYear !== this.props.archiveYear
-    ) {
-      this.fetchModule();
+  // If there is an error but module data can still be found, we assume module has
+  // been loaded at some point, so we just show that instead
+  if (error && !module) {
+    return <ApiError dataName="module information" retry={fetchModuleForDisplay} />;
+  }
+
+  // Redirect to canonical URL
+  if (module) {
+    const canonicalUrl = moduleArchive(moduleCode, year, module.title);
+    if (match.url !== canonicalUrl) {
+      return <Redirect to={{ ...location, pathname: canonicalUrl }} />;
     }
   }
 
-  fetchModule = () => {
-    this.setState({ error: undefined });
-    this.props.fetchModule().catch(this.handleFetchError);
-  };
-
-  fetchPageImport() {
-    // Try importing ModulePageContent thrice if we're online and
-    // getting the "Loading chunk x failed." error.
-    retryImport(() => import(/* webpackChunkName: "module" */ 'views/modules/ModulePageContent'))
-      .then((module) => this.setState({ ModulePageContent: module.default }))
-      .catch(this.handleFetchError);
+  if (module && ModulePageContent) {
+    // Unique key forces component to remount whenever the user moves to
+    // a new module. This allows the internal state (eg. currently selected
+    // timetable semester) of <ModulePageContent> to be consistent
+    return <ModulePageContent key={`${year}-${moduleCode}`} archiveYear={year} module={module} />;
   }
 
-  handleFetchError = (error: AxiosError) => {
-    this.setState({ error });
-    captureException(error);
-  };
-
-  render() {
-    const { ModulePageContent, error } = this.state;
-    const { module, moduleCode, match, location, archiveYear } = this.props;
-
-    if (get(error, ['response', 'status'], 200) === 404) {
-      return <ModuleNotFoundPage moduleCode={moduleCode} tryArchive={false} />;
-    }
-
-    // If there is an error but module data can still be found, we assume module has
-    // been loaded at some point, so we just show that instead
-    if (error && !module) {
-      return <ApiError dataName="module information" retry={this.fetchModule} />;
-    }
-
-    // Redirect to canonical URL
-    if (module) {
-      const canonicalUrl = moduleArchive(moduleCode, archiveYear, module.title);
-      if (match.url !== canonicalUrl) {
-        return <Redirect to={{ ...location, pathname: canonicalUrl }} />;
-      }
-    }
-
-    if (module && ModulePageContent) {
-      // Unique key forces component to remount whenever the user moves to
-      // a new module. This allows the internal state (eg. currently selected
-      // timetable semester) of <ModulePageContent> to be consistent
-      return (
-        <ModulePageContent
-          key={`${archiveYear}-${moduleCode}`}
-          archiveYear={archiveYear}
-          module={module}
-        />
-      );
-    }
-
-    return <LoadingSpinner />;
-  }
-}
-
-const getPropsFromMatch = (match: Match<Params>) => {
-  const { year = '', moduleCode = '' } = match.params;
-  return {
-    moduleCode: moduleCode.toUpperCase(),
-    year: year.replace('-', '/'),
-  };
+  return <LoadingSpinner />;
 };
 
-const mapStateToProps = ({ moduleBank }: StoreState, ownProps: OwnProps) => {
-  const { moduleCode, year } = getPropsFromMatch(ownProps.match);
-  return {
-    moduleCode,
-    archiveYear: year,
-    module: get(moduleBank.moduleArchive, [moduleCode, year], null),
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch, ownProps: OwnProps) => {
-  const { moduleCode, year } = getPropsFromMatch(ownProps.match);
-  return {
-    fetchModule: () => dispatch<Module>(fetchModuleArchive(moduleCode, year)),
-  };
-};
-
-const connectedModuleArchiveContainer = connect(
-  mapStateToProps,
-  // Cast required because the version of Dispatch defined by connect does not have the extensions defined
-  // in our Dispatch
-  mapDispatchToProps as MapDispatchToPropsNonObject<DispatchProps, OwnProps>,
-)(ModuleArchiveContainer);
-export const ModuleArchiveContainerComponent = withRouter(connectedModuleArchiveContainer);
 export default deferComponentRender(ModuleArchiveContainerComponent);
