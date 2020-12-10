39651ac31419391f784b3de661c04435b646e6e0
branson <ngbranson567@gmail.com>
2020-12-10 16:23:46 +0800

    Refactor to use FC and useMediaQuery

--- website/src/views/components/SideMenu.tsx
+++ website/src/views/components/SideMenu.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
+import { memo, FC, useEffect } from 'react';
 import classnames from 'classnames';
 
 import { Menu, X as Close } from 'react-feather';
-import makeResponsive from 'views/hocs/makeResponsive';
+import useMediaQuery from 'views/hooks/useMediaQuery';
 import disableScrolling from 'utils/disableScrolling';
 import { breakpointUp } from 'utils/css';
 import Fab from './Fab';
@@ -14,66 +14,55 @@ type Props = {
   openIcon?: React.ReactNode;
   closeIcon?: React.ReactNode;
   isOpen: boolean;
-  matchBreakpoint: boolean;
   toggleMenu: (boolean: boolean) => void;
 };
 
 export const OPEN_MENU_LABEL = 'Open menu';
 export const CLOSE_MENU_LABEL = 'Close menu';
 
-export class SideMenuComponent extends React.PureComponent<Props> {
-  static defaultProps = {
-    openIcon: <Menu aria-label={OPEN_MENU_LABEL} />,
-    closeIcon: <Close aria-label={CLOSE_MENU_LABEL} />,
-  };
+const DEFAULT_OPEN_ICON = <Menu aria-label={OPEN_MENU_LABEL} />;
+const DEFAULT_CLOSE_ICON = <Close aria-label={CLOSE_MENU_LABEL} />;
 
-  componentDidMount() {
-    disableScrolling(this.isSideMenuShown());
-  }
+export const SideMenuComponent: FC<Props> = ({
+  openIcon = DEFAULT_OPEN_ICON,
+  closeIcon = DEFAULT_CLOSE_ICON,
+  ...props
+}) => {
+  const matchBreakpoint = useMediaQuery(breakpointUp('md'));
 
-  componentDidUpdate() {
-    disableScrolling(this.isSideMenuShown());
-  }
+  const isSideMenuShown = props.isOpen && !matchBreakpoint;
 
-  componentWillUnmount() {
-    // Force unset noscroll when unmounting so the user gets scrolling back if
-    // they navigate out of the parent component without closing the menu
-    disableScrolling(false);
-  }
+  const { isOpen, toggleMenu, children } = props;
 
-  isSideMenuShown() {
-    return this.props.isOpen && !this.props.matchBreakpoint;
-  }
+  useEffect(() => {
+    disableScrolling(isSideMenuShown);
+  });
 
-  render() {
-    const { isOpen, toggleMenu, children, openIcon, closeIcon } = this.props;
+  return (
+    <>
+      <Fab className={styles.fab} onClick={() => toggleMenu(!isOpen)}>
+        {isOpen ? closeIcon : openIcon}
+      </Fab>
 
-    return (
-      <>
-        <Fab className={styles.fab} onClick={() => toggleMenu(!isOpen)}>
-          {isOpen ? closeIcon : openIcon}
-        </Fab>
+      {isSideMenuShown && (
+        // Key events are not sent to this div.
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+        <div className={styles.overlay} onClick={() => toggleMenu(false)} />
+      )}
 
-        {this.isSideMenuShown() && (
-          // Key events are not sent to this div.
-          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-          <div className={styles.overlay} onClick={() => toggleMenu(false)} />
-        )}
-
-        {/* boundaryContainer defines the top and bottom boundaries which sideMenu can extend to */}
-        <div className={styles.boundaryContainer}>
-          {/*
+      {/* boundaryContainer defines the top and bottom boundaries which sideMenu can extend to */}
+      <div className={styles.boundaryContainer}>
+        {/*
             sideMenu is the scrollable menu element. On mobile, it expands
             from the bottom of the screen to the top boundary of the
             container; i.e. if the menu's content is shorter than the
             container, it'll appear as a little action sheet rising from the
             bottom of the screen.
           */}
-          <div className={classnames(styles.sideMenu, { [styles.isOpen]: isOpen })}>{children}</div>
-        </div>
-      </>
-    );
-  }
-}
+        <div className={classnames(styles.sideMenu, { [styles.isOpen]: isOpen })}>{children}</div>
+      </div>
+    </>
+  );
+};
 
-export default makeResponsive(SideMenuComponent, breakpointUp('md'));
+export default memo(SideMenuComponent);
