cdb4e604c4cdbcced112c12a41bdf4ae4ef4b103
E-Liang Tan <eliang@eliangtan.com>
2020-12-19 10:19:11 +0800

    Remove deprecated V1 NUS scraper
    Replaced by the V2 scraper ~2 years ago:
    https://github.com/nusmodifications/nusmods/pull/1482
    
    RIP

--- scrapers/nus/jsonp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$basedir = dirname(__FILE__);
-$path = realpath($basedir . strtok($_SERVER['REQUEST_URI'], '?'));
-if ($path && strpos($path, $basedir) === 0) {
-    header('Content-type: application/javascript');
-    echo $_GET['callback'] . '(' . file_get_contents($path) . ')';
-}
