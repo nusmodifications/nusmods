13a65db3fcf18d12bd081d78e33c9654c3176f3d
E-Liang Tan <eliang@eliangtan.com>
2020-12-27 16:09:00 +0800

    Move NUSMods API into global singleton
    So that we only run setup code once, even if we have multiple AYs.
    
    This prevents the scraper from trying to create multiple
    schools/AYs/semesters.  Okay, this is a bit of a lie, because if we have
    multiple AYs the school setup code will run multiple times. This should
    be fine though, since we should only be setting up schools extremely
    rarely.
    
    This also allows all GraphQLPersist instances to share the same queue,
    which is nice as every task has its own persist instance.

--- scrapers/nus-v2/src/services/io/graphql.ts
+++ scrapers/nus-v2/src/services/io/graphql.ts
@@ -1,37 +1,16 @@
-import { ClientError, GraphQLClient } from 'graphql-request';
+import { ClientError } from 'graphql-request';
 import { groupBy } from 'lodash';
-import Queue from 'promise-queue';
 
-import config from '../../config';
 import logger from '../logger';
 import type { LessonTime, Module, ModuleCode, RawLesson } from '../../types/modules';
 import type { Persist } from '../../types/persist';
 import { NUSModsApiError } from '../../utils/errors';
+import { clientForAcadYear, NUSModsAPIClient } from '../nusmods-api';
 import {
-  getSdk,
-  Sdk,
-  QueryAdminInfoQuery,
   UpsertCourseCourseOfferingLessonGroupInput,
   Day,
   Scalars,
-} from '../nusmods-api';
-
-// TODO: Move to config
-const schoolShortName = 'NUS';
-
-// TODO: Move to nusmoderator? Move the one in website's app-config as well
-const semesterNames = {
-  '1': 'Semester 1',
-  '2': 'Semester 2',
-  '3': 'Special Term I',
-  '4': 'Special Term II',
-};
-const semesterNumbers = {
-  'Semester 1': '1',
-  'Semester 2': '2',
-  'Special Term I': '3',
-  'Special Term II': '4',
-};
+} from '../nusmods-api/__generated__/client';
 
 function lessonTimeToGraphQLTime(time: LessonTime): Scalars['Time'] {
   const hour = time.substring(0, 2);
@@ -69,49 +48,12 @@ function timetableToLessonGroups(
 /* eslint-disable class-methods-use-this */
 export default class GraphQLPersist implements Persist {
   /**
-   * Map of semester numbers (i.e. 1, 2, 3, 4) to `Semester` node `ID`s on the
-   * server.
+   * NUSMods GraphQL server client.
    */
-  #semesterIds: { [key: string]: string } = {};
-  /**
-   * The `AcadYear` node `ID` on the server.
-   */
-  #acadYearId = '';
-
-  /**
-   * NUSMods GraphQL server client. Not meant to be used directly; use
-   * `callApi` instead.
-   * @see callApi
-   */
-  readonly #nusmodsGraphqlServer: Promise<Sdk>;
-
-  /**
-   * Promise queue used to limit concurrent calls to the NUSMods GraphQL API
-   * server. Not meant to be used directly; use `callApi` instead.
-   * @see callApi
-   */
-  readonly #queue: Queue;
-
-  /**
-   * Wrapper around #nusmodsGraphqlServer and #queue that pushes the call into
-   * a queue.
-   */
-  private async callApi<T>(caller: (gql: Sdk) => Promise<T>): Promise<T> {
-    const gql = await this.#nusmodsGraphqlServer;
-    return this.#queue.add(() => caller(gql));
-  }
+  readonly #nusmodsApi: Promise<NUSModsAPIClient>;
 
   constructor(academicYear: string) {
-    if (!config.graphQLConfig) {
-      throw new Error('graphQLConfig in config.json is not set');
-    }
-
-    this.#queue = new Queue(config.graphQLConfig.concurrency);
-
-    const client = new GraphQLClient(config.graphQLConfig.url);
-    client.setHeader('Authorization', config.graphQLConfig.token);
-    const sdk = getSdk(client);
-    this.#nusmodsGraphqlServer = this.populateAdminInfo(sdk, academicYear);
+    this.#nusmodsApi = clientForAcadYear(academicYear);
   }
 
   // ///////////////////////////////////////////////////////////
@@ -132,12 +74,13 @@ export default class GraphQLPersist implements Persist {
   // ///////////////////////////////////////////////////////////
 
   async module(_moduleCode: ModuleCode, module: Module) {
+    const api = await this.#nusmodsApi;
     try {
-      await this.callApi((gql) =>
+      await api.callApi((gql) =>
         // Sync course
         gql.UpsertCourse({
           input: {
-            acadYearId: this.#acadYearId,
+            acadYearId: api.acadYearId,
             // Basic info
             code: module.moduleCode,
             title: module.title,
@@ -157,7 +100,7 @@ export default class GraphQLPersist implements Persist {
 
             // Semester data
             courseOfferings: module.semesterData.map((semesterData) => ({
-              semesterId: this.#semesterIds[semesterData.semester],
+              semesterId: api.semesterIds[semesterData.semester],
               // TODO: faculty: module.faculty,
               department: module.department,
               // workloadString: module.workload, // TODO: Convert back to workload list? Or have a workload array? I think we should just make a list
@@ -190,9 +133,10 @@ export default class GraphQLPersist implements Persist {
   }
 
   async getModuleCodes() {
-    const data = await this.callApi((gql) =>
+    const api = await this.#nusmodsApi;
+    const data = await api.callApi((gql) =>
       gql.QueryCourseCodes({
-        acadYearId: this.#acadYearId,
+        acadYearId: api.acadYearId,
       }),
     );
     const ay = data.node;
@@ -203,10 +147,11 @@ export default class GraphQLPersist implements Persist {
   }
 
   deleteModule = async (moduleCode: ModuleCode) => {
-    await this.callApi((gql) =>
+    const api = await this.#nusmodsApi;
+    await api.callApi((gql) =>
       gql.DeleteCourse({
         input: {
-          acadYearId: this.#acadYearId,
+          acadYearId: api.acadYearId,
           code: moduleCode,
         },
       }),
@@ -229,117 +174,4 @@ export default class GraphQLPersist implements Persist {
 
   // Ignore since `venueInformation` gets more detailed info
   venueList = () => Promise.resolve();
-
-  // ///////////////////////////////////////////////////////////
-  // Initialization
-  // ///////////////////////////////////////////////////////////
-
-  /**
-   * Ensure that the school, the current AY, and all semesters are in the API.
-   */
-  private async populateAdminInfo(gql: Sdk, academicYear: string) {
-    let existingAyData: QueryAdminInfoQuery;
-    try {
-      existingAyData = await this.#queue.add(() =>
-        gql.QueryAdminInfo({
-          schoolShortName,
-          academicYear,
-        }),
-      );
-    } catch (e) {
-      if (e instanceof ClientError && e.response.errors?.[0]?.message.endsWith('not found')) {
-        // Errors received are * not found errors. We'll create whatever we
-        // need, so we'll just pretend there were no errors and continue.
-        existingAyData = e.response.data;
-      } else {
-        throw e;
-      }
-    }
-
-    let schoolId = existingAyData?.schoolWithShortName?.id;
-    if (!schoolId) {
-      schoolId = await this.createSchool(gql);
-    }
-    if (!schoolId) {
-      throw new NUSModsApiError('Could not get or create school');
-    }
-
-    let ayId = existingAyData?.schoolWithShortName?.acadYearWithName?.id;
-    if (!ayId) {
-      ayId = await this.createAcadYear(gql, schoolId, academicYear);
-    }
-    if (!ayId) {
-      throw new NUSModsApiError('Could not get or create acad year');
-    }
-    this.#acadYearId = ayId;
-
-    const semesters = existingAyData?.schoolWithShortName?.acadYearWithName?.semesters;
-    if (semesters) {
-      semesters.forEach((semester) => {
-        if (!semester) {
-          // This should never happen, so we just throw and fix manually by
-          // manually adding the missing semeseter.
-          throw new NUSModsApiError('Academic year has a missing semester!');
-        }
-        const { name, id } = semester;
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        this.#semesterIds[semesterNumbers[name as keyof typeof semesterNumbers]] = id!;
-      });
-    } else {
-      await this.createSemesters(gql);
-    }
-
-    return gql;
-  }
-
-  private async createSchool(gql: Sdk): Promise<string | undefined> {
-    const schoolData = await this.#queue.add(() =>
-      gql.CreateSchool({
-        input: {
-          shortName: schoolShortName,
-          // TODO: Move to config
-          longName: 'National University of Singapore',
-        },
-      }),
-    );
-    return schoolData.createSchool?.school?.id;
-  }
-
-  private async createAcadYear(
-    gql: Sdk,
-    schoolId: string,
-    academicYear: string,
-  ): Promise<string | undefined> {
-    const newAyData = await this.#queue.add(() =>
-      gql.CreateAcadYear({
-        input: {
-          schoolId,
-          name: academicYear,
-        },
-      }),
-    );
-    return newAyData.createAcadYear?.acadYear?.id;
-  }
-
-  private async createSemesters(gql: Sdk) {
-    // Use for loop so that we create semesters sequentially, so that semesters
-    // have increasing IDs. While not strictly necessary, it just looks better
-    // and is more intuitive.
-    for (const [semester, name] of Object.entries(semesterNames)) {
-      // eslint-disable-next-line no-await-in-loop
-      const semData = await this.#queue.add(() =>
-        gql.CreateSemester({
-          input: {
-            acadYearId: this.#acadYearId,
-            name,
-          },
-        }),
-      );
-      const semId = semData.createSemester?.semester?.id;
-      if (!semId) {
-        throw new NUSModsApiError(`Could not create semester ${semester} (${name})`);
-      }
-      this.#semesterIds[semester] = semId;
-    }
-  }
 }
