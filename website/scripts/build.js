e727d020c5fb95cd9ec61e275ea9322d2340a2ca
E-Liang Tan <eliang@eliangtan.com>
2021-01-03 13:05:16 +0800

    Print process.env

--- website/scripts/build.js
+++ website/scripts/build.js
@@ -135,6 +135,8 @@ async function buildAll(previousDistFileSizes) {
 }
 
 async function main() {
+  console.log('process.env:', process.env);
+
   console.log('Building version', chalk.cyan(parts.appVersion().versionStr));
 
   // First, read the current file sizes in build directory.
