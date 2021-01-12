8e5a16e3bcff99acd1a5b96d12768111ff69bd5c
Teik Jun <teikjunhci@gmail.com>
2021-01-13 01:14:51 +0800

    Replace `ScrollToTop` with `useScrollToTop` (#3041)
    * Improve test for previous PR
    
    * Functionalize ModulePageContent
    
    * Functionalize SettingsContainer
    
    * Fix broken enzyme test by migrating to RTL
    
    * Format code
    
    * Remove ScrollToTop
    
    * Minor stylistic change
    
    * Some changes
    
    * Make requested changes for ModulePageContent
    
    * Make requested changes for SettingsContainer
    
    * Make requested changes to ModulePageContent test
    
    * Fix formatting
    
    * Fix typechecking issue
    
    * Use Set instead of Array for checking membership
    
    Co-authored-by: Zhang Yi Jiang <mediumdeviation@gmail.com>

--- website/src/test-utils/renderWithRouterMatch.tsx
+++ website/src/test-utils/renderWithRouterMatch.tsx
@@ -13,7 +13,7 @@ export default function renderWithRouterMatch(
   children: ReactNode,
   {
     path = '/',
-    location,
+    location = '/',
   }: {
     path?: string;
     location?: Parameters<typeof createHistory>[0];
