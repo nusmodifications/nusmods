1c8d0d22851971f7c6bf360cbefb4b44a0ca7c51
Alissa Yarmantho <38494574+alissayarmantho@users.noreply.github.com>
2021-01-21 15:08:10 +0800

    Add MPE page introduction content (#3183)

--- website/src/views/mpe/MpeContainer.tsx
+++ website/src/views/mpe/MpeContainer.tsx
@@ -1,10 +1,28 @@
+import styles from './MpeContainer.scss';
+
 type Props = {
   placeholder: true; // Remove this when new props are added.
 };
 
 const MpeContainer: React.FC<Props> = () => (
-  <div>
-    <h1>MPE</h1>
+  <div className={styles.pageContainer}>
+    <div className="col-md-9">
+      <header className={styles.header}>
+        <h2>Module Preference Exercise</h2>
+        <h4>AY2021/2022 - Semester 2</h4>
+      </header>
+      <h4 className={styles.subtitle}>Overview</h4>
+
+      <p>
+        The Module Preference Exercise (MPE) is a project initiated by NUS to better understand
+        students’ demand for specific modules. Students who have completed this exercise{' '}
+        <strong>will receive tie-breaker benefit</strong> during the ModReg period.
+      </p>
+      <p>
+        Do take note that this is only a planning exercise;{' '}
+        <strong>it does not enroll you in the modules.</strong>
+      </p>
+    </div>
   </div>
 );
 
