bb12242123ab2e08c024fb27f734dad2d04a594f
Chua Bing Quan <chipneverdies@gmail.com>
2021-01-23 04:22:25 +0800

    Implement HTTP Status 405 handler

---
+++ utils/methodNotFound.ts
@@ -0,0 +1,12 @@
+export default allowedMethods => async (req, res) => {
+  try {
+    const allowHeaderValue =
+      Object.keys(allowedMethods).reduce((acc, method) => `${acc}, ${method}`)
+    res.setHeader('Allow', allowHeaderValue)
+    res.status(405).json({
+      message: 'Method not allowed'
+    })
+  } catch (err) {
+    throw err
+  }
+}
