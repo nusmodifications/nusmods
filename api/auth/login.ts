567e774a79c1af85afcaca4194454c878504f6bd
Chua Bing Quan <chipneverdies@gmail.com>
2021-01-23 04:28:18 +0800

    Implement login API endpoint

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -1,5 +1,51 @@
-const saml = require('saml').Saml20;
+import { authenticate } from '../../auth'
+import rescue from '../../utils/rescue'
+import handleMethodNotFound from '../../utils/methodNotFound'
 
-export default async (req, res) => {
-  
+const errors = {
+  noRelayState: 'ERR_NO_RELAY_STATE'
 }
+
+const allowedMethods = {
+  POST: 'POST'
+}
+
+const loginHandler = async (req, res) => {
+  try {
+    switch (req.method) {
+      case allowedMethods.POST:
+        await handlePost(req, res)
+        break
+      default:
+        await handleDefault(req, res)
+        break
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+const handlePost = async (req, res) => {
+  try {
+    const { token, relayState } = await authenticate(req)
+    if (!relayState) {
+      throw new Error(errors.noRelayState)
+    }
+
+    const userURL = new URL(relayState)
+    userURL.searchParams.append('token', token)
+
+    res.redirect(userURL.toString())
+  } catch (err) {
+    if (err.message === errors.noRelayState) {
+      return res.json({
+        message: 'Relay state not found in request'
+      })
+    }
+    throw err
+  }
+}
+
+const handleDefault = handleMethodNotFound(allowedMethods)
+
+export default rescue(loginHandler)
