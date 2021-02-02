e016245dec66514afebc2b3321305e13532a2bf7
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-02 20:33:53 +0800

    Decouple rank from module card

--- website/src/views/mpe/form/ModuleCard.tsx
+++ website/src/views/mpe/form/ModuleCard.tsx
@@ -2,22 +2,19 @@ import styles from './ModuleCard.scss';
 
 type Props = {
   moduleTitle?: string;
-  moduleCode?: string;
-  amountMC?: number;
-
-  // Remove this when new props are added.
+  moduleCode: string;
+  moduleCredits: number;
 };
 
 const ModuleCard: React.FC<Props> = (props) => (
   <div className={styles.container}>
-    <div className={styles.rank}> 1 </div>
     <div className={styles.card}>
       <div className={styles.side}>
         <img className={styles.image} src="https://i.ibb.co/48FtBq1/doticon.png" alt="dots" />
       </div>
       <div className={styles.modulecode}>{props.moduleCode}</div>
       <div className={styles.moduletitle}>{props.moduleTitle}</div>
-      <div className={styles.mc}>{props.amountMC} MC</div>
+      <div className={styles.mc}>{props.moduleCredits} MC</div>
       <div className={styles.moduletype}>Module Type</div>
     </div>
     <img
@@ -27,4 +24,5 @@ const ModuleCard: React.FC<Props> = (props) => (
     />
   </div>
 );
+
 export default ModuleCard;
