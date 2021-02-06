c9198144e1a4a368d7cea8f434863730a014be4d
Nicolas C <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-02-06 10:00:00 +0800

    Add tasks to do

---
+++ website/scripts/mpe.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+
+const SSO_URL = "/api/auth/sso"
+const MPE_URL = "/api/submissions"
+
+// Get Link to Redirect user to NUS Login Page
+function getAuthenticationURL() {
+    
+}
+
+// Get MPE Preference
+    // Catch 404 not found
+    // Return empty array
+
+    // else
+    // Strip rank and only include module + module type
+
+// Update MPE Preference
+    // takes in an array of preferences and put to server
+
+    // JSON return a message -> errors can be found here throw exact message
