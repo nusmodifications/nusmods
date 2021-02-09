66cbabee70b68562af3a2b8aaa2bc9156a4d891f
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-09 21:10:55 +0800

    Remove serverless utilities

--- website/src/serverless/utils/methodNotFound.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelApiHandler } from '@vercel/node';
-
-export default (allowedMethods: string[]): VercelApiHandler => async (req, res) => {
-  const allowHeaderValue = Object.keys(allowedMethods).reduce((acc, method) => `${acc}, ${method}`);
-  res.setHeader('Allow', allowHeaderValue);
-  res.status(405).json({
-    message: 'Method not allowed',
-  });
-};
