4d669717c97cb7bb99947b924a26e5485ea3fd19
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-09 21:11:56 +0800

    Refactor MPE APIs to use new routing abstraction

--- website/api/nus/mpe/submissions.ts
+++ website/api/nus/mpe/submissions.ts
@@ -1,16 +1,17 @@
-import type { VercelApiHandler } from '@vercel/node';
-import { getSubmissionById, createSubmission } from '../../../src/serverless/mpe';
-import { User, verifyLogin } from '../../../src/serverless/nus-auth';
-import rescue from '../../../src/serverless/utils/rescue';
-import handleMethodNotFound from '../../../src/serverless/utils/methodNotFound';
+import {
+  getSubmissionById,
+  createSubmission,
+} from "../../../src/serverless/mpe";
+import { User, verifyLogin } from "../../../src/serverless/nus-auth";
+import {
+  Router,
+  defaultFallback,
+  defaultRescue,
+  Handler,
+  RouteHandlers,
+} from "../../../src/serverless/router";
 
-const allowedMethods = {
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
-};
-
-const handleGet: VercelApiHandler = async (req, res) => {
+const handleGet: Handler = async (req, res) => {
   try {
     const user = (req as any).user as User;
     const submission = await getSubmissionById(user.accountName);
@@ -18,7 +19,7 @@ const handleGet: VercelApiHandler = async (req, res) => {
   } catch (err) {
     if (err.response.status === 404) {
       res.status(404).json({
-        message: 'No MPE preferences are found for requesting user',
+        message: "No MPE preferences are found for requesting user",
       });
     } else {
       throw err;
@@ -26,32 +27,24 @@ const handleGet: VercelApiHandler = async (req, res) => {
   }
 };
 
-const handlePost: VercelApiHandler = async (req, res) => {
+const handlePost: Handler = async (req, res) => {
   // TODO: Identify error type in the catch block and throw relevant error.
   // eslint-disable-next-line no-useless-catch
   try {
     const user = (req as any).user as User;
     await createSubmission(user.accountName, req.body);
     res.json({
-      message: 'Your MPE preferences are successfully recorded',
+      message: "Your MPE preferences are successfully recorded",
     });
   } catch (err) {
     throw err;
   }
 };
 
-const handleDefault = handleMethodNotFound(Object.keys(allowedMethods));
-
-const submissionHandler: VercelApiHandler = (req, res) => {
-  switch (req.method) {
-    case allowedMethods.GET:
-      return handleGet(req, res);
-    case allowedMethods.POST:
-    case allowedMethods.PUT:
-      return handlePost(req, res);
-    default:
-      return handleDefault(req, res);
-  }
+const routeHandlers: RouteHandlers = {
+  GET: verifyLogin(handleGet),
+  POST: verifyLogin(handlePost),
+  PUT: verifyLogin(handlePost),
 };
 
-export default rescue(verifyLogin(submissionHandler));
+export default Router(routeHandlers, defaultFallback, defaultRescue(true));
