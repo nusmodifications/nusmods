5d9d55029009ea70c70f9db11534c1c22a57548a
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-10 21:26:54 +0800

    Add typings to MPE API calls

--- website/src/apis/mpe.ts
+++ website/src/apis/mpe.ts
@@ -1,15 +1,16 @@
 import NUSModsApi from '../apis/nusmods';
-const axios = require('axios');
+import axios from 'axios';
+import type { MpePreference } from '../types/mpe';
+import type { Module, ModuleCode } from '../types/modules';
 
 // API URL to get NUS SSO Redirect Link
-const SSO_PATH = "auth/sso"
+const SSO_PATH = 'auth/sso';
 // API URL to interact with MPE submission
-const MPE_PATH = "submissions"
+const MPE_PATH = 'submissions';
 
 const mpe = axios.create({
-    baseURL: 'https://nusmods.com/api/',
-  })
-  
+  baseURL: 'https://nusmods.com/api/',
+});
 
 /**
  * Helper function to fetch details of a NUS module to add to MPE
@@ -17,26 +18,29 @@ const mpe = axios.create({
  * @params  moduleCode String code of module to fetch
  * @returns array      Details of the module from the NUSMods API
  */
-async function fetchModuleDetails(moduleCode) {
-    return await axios.get(NUSModsApi.moduleDetailsUrl(moduleCode));
-}
+const fetchModuleDetails = async (moduleCode: ModuleCode) => {
+  try {
+    const resp = await axios.get<Module>(NUSModsApi.moduleDetailsUrl(moduleCode));
+    return resp.data;
+  } catch (err) {
+    throw err;
+  }
+};
 
 /**
  * Get link to NUS Login Page
  * 
  * @return String Link to NUS login page
  */
-export async function getSSOLink() {
-    try {
-        const res = await mpe.get(SSO_PATH)
-        return res.request.responseURL
-    } catch(err) {
-        throw new Error(err.response.data.message);
-    }
+export const getSSOLink = async (): Promise<string> => {
+  try {
+    const resp = await mpe.get(SSO_PATH);
+    return resp.request.responseURL;
+  } catch (err) {
+    throw new Error(err.response.data.message);
+  }
 }
 
-
-  
 
 /**
  * Get Students MPE preference
@@ -48,41 +52,42 @@ export async function getSSOLink() {
  *   moduleCredits: number;
  * }
  */
-export async function getMPEPreference() {
-    try {
-        const res = await mpe.get(MPE_PATH)
-        const rawPreferences = res.data.preferences
+export const getMPEPreferences = async () => {
+  try {
+    const resp = await mpe.get(MPE_PATH);
+    const rawPreferences = resp.data.preferences;
 
-        // Fetch module details from NUSMods API
-        const modules = await Promise.all(rawPreferences.map(p => fetchModuleDetails(p.moduleCode)))
-        return modules.map(m => ({
-            modulesTitle:  m.data.title,
-            moduleCode:    m.data.moduleCode,
-            moduleCredits: m.data.moduleCredit
-        }))    
-    } catch(err) {
-        // User has no existing MPE preferences
-        if (err.response.status == 404) {
-            return []
-        }
-        throw new Error(err.response.data.message);
+    // Fetch module details from NUSMods API
+    // @ts-ignore
+    const modules = await Promise.all<Module>(rawPreferences.map(p => fetchModuleDetails(p.moduleCode)));
+    return modules.map(m => ({
+      modulesTitle: m.title,
+      moduleCode: m.moduleCode,
+      moduleCredits: m.moduleCredit
+    }));
+  } catch (err) {
+    // User has no existing MPE preferences
+    if (err.response.status == 404) {
+      return []
     }
+    throw new Error(err.response.data.message);
+  }
 }
 
 /**
  * Makes a PUT request to update students MPE preference
  * @param preferences  array of module preferences to replace current MPE preferences
  */
-export async function updateMPEPreference(rawPreferences) {
-    try {
-        const preferences = rawPreferences.map(p => ({
-            moduleCode: p.moduleCode,
-            moduleType: p.moduleType,
-            credits: p.credits
-        }))
-        const res = await mpe.put(MPE_PATH, preferences)
-        return res.data.message
-    } catch(err) {
-        throw new Error(err.response.data.message)
-    }
-}
+export const updateMPEPreference = async (preferences: MpePreference[]) => {
+  try {
+    const submission = preferences.map(p => ({
+      moduleCode: p.moduleCode,
+      moduleType: p.moduleType,
+      credits: p.moduleCredits
+    }));
+    const res = await mpe.put(MPE_PATH, submission);
+    return res.data.message;
+  } catch (err) {
+    throw new Error(err.response.data.message)
+  }
+};
