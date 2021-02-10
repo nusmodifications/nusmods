49d5af77921dc0148adabb745e04eea2673a2af1
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-10 23:43:38 +0800

    Add hook to handle and manage token state

--- website/src/apis/mpe.ts
+++ website/src/apis/mpe.ts
@@ -2,22 +2,61 @@ import NUSModsApi from '../apis/nusmods';
 import axios from 'axios';
 import type { MpePreference } from '../types/mpe';
 import type { Module, ModuleCode } from '../types/modules';
+import { useEffect } from 'react';
+import getLocalStorage from 'storage/localStorage';
+import { Location, History } from 'history';
 
-// API URL to get NUS SSO Redirect Link
-const SSO_PATH = 'auth/sso';
-// API URL to interact with MPE submission
-const MPE_PATH = 'submissions';
+const SSO_PATH = '/auth/sso';
+const MPE_PATH = '/submissions';
+const TOKEN_STORAGE_KEY = 'nus-auth-token';
+const TOKEN_URL_QUERY = 'token';
+
+export const ERR_SESSION_EXPIRED = new Error('User session has expired, please login again');
+
+const storage = getLocalStorage();
+
+const getToken = (): string | null => {
+  return storage.getItem(TOKEN_STORAGE_KEY);
+};
+
+const setToken = (token: string): void => {
+  return storage.setItem(TOKEN_STORAGE_KEY, token);
+};
 
 const mpe = axios.create({
-  baseURL: 'https://nusmods.com/api/',
+  baseURL: `${NUSMODS_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://nusmods.com'}/api/nus`
 });
 
-/**
- * Helper function to fetch details of a NUS module to add to MPE
- * 
- * @params  moduleCode String code of module to fetch
- * @returns array      Details of the module from the NUSMods API
- */
+mpe.interceptors.request.use(config => {
+  config.headers.Authorization = getToken();
+  return config;
+});
+
+mpe.interceptors.response.use(resp => resp, err => {
+  if (err?.response?.status === 401) {
+    return Promise.reject(ERR_SESSION_EXPIRED);
+  }
+  return Promise.reject(err);
+});
+
+export const useHandleLogin = (location: Location, history: History): void => {
+  const params = new URLSearchParams(location.search);
+  const token = params.get(TOKEN_URL_QUERY);
+  useEffect(() => {
+    if (token !== null) {
+      setToken(token);
+      params.delete(TOKEN_URL_QUERY);
+      history.replace({
+        search: params.toString()
+      });
+    }
+  });
+};
+
+export const isLoggedIn = () => getToken() !== null;
+
 const fetchModuleDetails = async (moduleCode: ModuleCode) => {
   try {
     const resp = await axios.get<Module>(NUSModsApi.moduleDetailsUrl(moduleCode));
@@ -27,31 +66,17 @@ const fetchModuleDetails = async (moduleCode: ModuleCode) => {
   }
 };
 
-/**
- * Get link to NUS Login Page
- * 
- * @return String Link to NUS login page
- */
 export const getSSOLink = async (): Promise<string> => {
   try {
     const resp = await mpe.get(SSO_PATH);
-    return resp.request.responseURL;
+    return resp.data;
+    // TODO: Intercept redirect link when CORS issue is resolved.
+    // return resp.request.responseURL;
   } catch (err) {
-    throw new Error(err.response.data.message);
+    throw err;
   }
-}
+};
 
-
-/**
- * Get Students MPE preference
- * 
- * @returns preference[] Array of preferences with the following key-value pairs
- * Preference = {
- *   moduleTitle: string;
- *   moduleCode: string;
- *   moduleCredits: number;
- * }
- */
 export const getMPEPreferences = async () => {
   try {
     const resp = await mpe.get(MPE_PATH);
@@ -72,12 +97,8 @@ export const getMPEPreferences = async () => {
     }
     throw new Error(err.response.data.message);
   }
-}
+};
 
-/**
- * Makes a PUT request to update students MPE preference
- * @param preferences  array of module preferences to replace current MPE preferences
- */
 export const updateMPEPreference = async (preferences: MpePreference[]) => {
   try {
     const submission = preferences.map(p => ({
