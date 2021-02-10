68a752d435f790f82d29d5e808396b6d824e7ba5
Chua Bing Quan <chipneverdies@gmail.com>
2021-02-10 21:25:29 +0800

    Add MPE type definitions

---
+++ website/src/types/mpe.ts
@@ -0,0 +1,30 @@
+import { ModuleTitle, ModuleCode } from './modules';
+
+export interface EssentialMajor {
+  type: "01"
+}
+
+export interface EssentialSecondMajor {
+  type: "02"
+}
+
+export interface Elective {
+  type: "03"
+}
+
+export interface UnrestrictedElective {
+  type: "04"
+}
+
+export type ModuleType =
+  | EssentialMajor
+  | EssentialSecondMajor
+  | Elective
+  | UnrestrictedElective;
+
+export type MpePreference = {
+  moduleTitle: ModuleTitle;
+  moduleCode: ModuleCode;
+  moduleType: ModuleType;
+  moduleCredits: number;
+};
