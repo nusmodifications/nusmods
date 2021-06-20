1d9f877b6f3fd8a61288815d7ca7038fde38d55c
E-Liang Tan <eliang@eliangtan.com>
2021-06-20 17:33:24 +0800

    Update NUSMods for AY21/22 (#3287)

--- website/src/views/components/notfications/Announcements.tsx
+++ website/src/views/components/notfications/Announcements.tsx
@@ -1,9 +1,7 @@
 import { memo, useState, useCallback } from 'react';
 import classnames from 'classnames';
-import { Target } from 'react-feather';
-import { Link } from 'react-router-dom';
+import { Heart } from 'react-feather';
 
-import { enableMpe } from 'featureFlags';
 import storage from 'storage';
 import { announcementKey } from 'storage/keys';
 import CloseButton from 'views/components/CloseButton';
@@ -12,13 +10,14 @@ import styles from './Announcements.scss';
 /**
  * If false, hides announcement.
  */
-const enableAnnouncements = enableMpe;
+const enableAnnouncements = true;
 
 /**
  * Unique key for the current announcement. If the announcement is not
  * dismissible, set the key to null. Otherwise, set it to a string.
  *
  * Previous keys:
+ * - 'ay202122-new-data' - AY2021/22 data is available
  * - 'ay202021-new-data' - AY2020/21 data is available
  * - 'ay201920-new-data' - AY2019/20 data is available
  * - 'nusmods-is-official' - NUSMods switch to official APIs
@@ -26,7 +25,7 @@ const enableAnnouncements = enableMpe;
  * - 'ay201819-new-data' - AY2018/19 data is available
  * - 'ay201819-s2-new-data' - S2 data available
  */
-const key = announcementKey(null);
+const key = announcementKey('ay202122-new-data');
 
 const Announcements = memo(() => {
   const [isOpen, setIsOpen] = useState(() => {
@@ -47,29 +46,22 @@ const Announcements = memo(() => {
   return (
     <div
       className={classnames(
-        'alert alert-primary no-export',
+        'alert alert-success no-export',
         styles.announcement,
-        styles.wrapButtons,
+        // styles.wrapButtons, // Uncomment if needed
       )}
     >
-      <Target className={styles.backgroundIcon} />
+      <Heart className={styles.backgroundIcon} />
 
       <div className={styles.body}>
-        <h3>Module Planning Exercise for AY2021/22 Semester 1 Now Open!</h3>
+        <h3>AY2021/22 modules now available!</h3>
         <p className={styles.bodyElement}>
-          From 1st-14th March, use our MPE form to pick modules you're thinking of reading in
-          Semester 1. While this isn't ModReg, your participation in MPE will help NUS to plan
-          resources and module timetables.
+          NUSMods now has AY2021/22 module information available. The data is accurate but subject
+          to changes. Happy new academic year!
         </p>
-        <p className={styles.bodyElement}>All the best for your midterms! You got this 💪</p>
       </div>
 
-      <div className={styles.buttons}>
-        <Link to="/mpe" className="btn btn-primary">
-          Plan now!
-        </Link>
-        {key && <CloseButton onClick={dismiss} />}
-      </div>
+      <div className={styles.buttons}>{key && <CloseButton onClick={dismiss} />}</div>
     </div>
   );
 });
