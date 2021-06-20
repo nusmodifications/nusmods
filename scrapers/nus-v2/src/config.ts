0267150fc7eaa0f08923f1d11e63b73981e4ef29
E-Liang Tan <eliang@eliangtan.com>
2021-06-20 15:26:07 +0800

    Upgrade scraper config for AY21/22 (#3286)

--- scrapers/nus-v2/src/config.ts
+++ scrapers/nus-v2/src/config.ts
@@ -44,7 +44,7 @@ const config: Config = {
   apiConcurrency: env.apiConcurrency || 5,
 
   // Other config
-  academicYear: '2020/2021',
+  academicYear: '2021/2022',
   dataPath: path.resolve(__dirname, '../data'),
 };
 
