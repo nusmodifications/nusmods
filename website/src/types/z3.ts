c7dfbd15caaadf57f10fb6ee64e1527e9d79fdcd
Sriram Sami <frizensami@gmail.com>
2021-06-28 16:14:49 +0800

    Add callback defs to z3 types

--- website/src/types/z3.ts
+++ website/src/types/z3.ts
@@ -22,3 +22,14 @@ export interface Z3Message {
   kind: Z3MessageKind;
   msg: string;
 }
+
+/**
+ * Callbacks from the Z3 Manager
+ * */
+export interface Z3Callbacks {
+    onZ3Initialized: any;
+    onSmtlib2InputCreated(s: string): any;
+    onOutput(s: string): any;
+    // onTimetableOutput(timetable: TimetableOutput): any;
+    onTimetableOutput(timetable: any): any;
+}
