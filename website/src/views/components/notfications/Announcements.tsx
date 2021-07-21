be3b458c1338f346c3a16a3dd8a310e7ee4532a7
Christopher Goh <chrisgzf@gmail.com>
2021-07-22 07:36:53 +0800

    Add banner to apologise for search outage

--- website/src/views/components/notfications/Announcements.tsx
+++ website/src/views/components/notfications/Announcements.tsx
@@ -1,6 +1,7 @@
 import { memo, useState, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import classnames from 'classnames';
-import { Heart } from 'react-feather';
+import { Search } from 'react-feather';
 
 import storage from 'storage';
 import { announcementKey } from 'storage/keys';
@@ -17,6 +18,7 @@ const enableAnnouncements = true;
  * dismissible, set the key to null. Otherwise, set it to a string.
  *
  * Previous keys:
+ * - 'ay202122-2107-search-outage' - Module search outage apology
  * - 'ay202122-new-data' - AY2021/22 data is available
  * - 'ay202021-new-data' - AY2020/21 data is available
  * - 'ay201920-new-data' - AY2019/20 data is available
@@ -25,7 +27,7 @@ const enableAnnouncements = true;
  * - 'ay201819-new-data' - AY2018/19 data is available
  * - 'ay201819-s2-new-data' - S2 data available
  */
-const key = announcementKey('ay202122-new-data');
+const key = announcementKey('ay202122-2107-search-outage');
 
 const Announcements = memo(() => {
   const [isOpen, setIsOpen] = useState(() => {
@@ -46,18 +48,24 @@ const Announcements = memo(() => {
   return (
     <div
       className={classnames(
-        'alert alert-success no-export',
+        'alert alert-info no-export',
         styles.announcement,
         // styles.wrapButtons, // Uncomment if needed
       )}
     >
-      <Heart className={styles.backgroundIcon} />
+      <Search className={styles.backgroundIcon} />
 
       <div className={styles.body}>
-        <h3>AY2021/22 modules now available!</h3>
+        <h3>Module search is now working!</h3>
         <p className={styles.bodyElement}>
-          NUSMods now has AY2021/22 module information available. The data is accurate but subject
-          to changes. Happy new academic year!
+          We've noticed that some of you have recently faced issues with our{' '}
+          <Link
+            className={styles.announcementLink}
+            to={{ pathname: '/modules', search: '?sem[0]=1&sem[1]=2&sem[2]=3&sem[3]=4' }}
+          >
+            module search page
+          </Link>{' '}
+          and we've fixed it. Sorry for the inconvenience, and all the best for ModReg!
         </p>
       </div>
 
