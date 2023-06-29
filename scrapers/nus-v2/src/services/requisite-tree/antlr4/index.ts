2b4af1e735535ef08ca83bc1fc2ae1a340bc9f06
Ken Jin <28750310+Fidget-Spinner@users.noreply.github.com>
2023-06-29 20:52:33 +0800

    chore: remove accidental commit

--- scrapers/nus-v2/src/services/requisite-tree/antlr4/index.ts
+++
@@ -1,5 +0,0 @@
-import {NusModsParser} from './NusModsParser';
-import {NusModsVisitor} from './NusModsVisitor';
-import {NusModsLexer} from './NusModsLexer';
-
-export {NusModsParser, NusModsVisitor, NusModsLexer};
