13b824d365b3c7082c913c57a3babe20ed20028f
Alissa Yarmantho <38494574+alissayarmantho@users.noreply.github.com>
2023-10-09 00:06:54 +0800

    Change inS1 and inS2 MPE to CPEx and fix private key parsing (#3632)
    * chore(deps): update dependency postcss to v8.4.31 [security] (#3630)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * feat(cpex): add standalone scraper (#3622)
    
    * chore(cpex): set term to next term instead of current (#3631)
    
    * Change MPE to CPE and Open MPE (#3623)
    
    * Fix serverless function not working (#3623)
    
    * Change modules to courses (#3623)
    
    * Change inS1 and inS2 from mpe to cpex and fix private key parsing (#3623)
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Christopher Goh <chrisgzf@gmail.com>
    Co-authored-by: Ravern Koh <ravernkoh@gmail.com>

--- scrapers/cpex-scraper/scripts/run.sh
+++ scrapers/cpex-scraper/scripts/run.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Print date for logs
 date
 
-# Ensure cwd is the root of the v2 scraper project
+# Ensure cwd is the root of the CPEx scraper project
 cd "$(dirname "$0")"
 cd ..
 
@@ -13,15 +13,12 @@ rm -rf build
 yarn build
 
 # Run the scraper
-echo "Running scraper"
-node build/index.js all
-
-# Update docs
-yarn docs
+echo "Running CPEx scraper"
+node build/src/index.js
 
 # Sync with live data
 echo "Syncing data"
-rsync -ahz --delete-after --exclude='cache/' --exclude='mpeModules.json' --exclude='cpexModules.json' data/ ../../../api.nusmods.com/v2
+\cp data/cpexModules.json ../../../api.nusmods.com/v2
 
 # pm2 doesn't restart processes that have stopped, so this just noops until
 # the next cron restart
