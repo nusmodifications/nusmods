e2936bd9acca8370b4864e3cb6697198829b7eaf
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-09-08 20:31:53 +0800

    test

--- website/src/views/components/disqus/DisqusComments.tsx
+++ website/src/views/components/disqus/DisqusComments.tsx
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import { PureComponent, useCallback, useEffect, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { connect } from 'react-redux';
 
@@ -26,7 +26,88 @@ type State = {
 
 const SCRIPT_ID = 'dsq-embed-scr';
 
-class DisqusComments extends PureComponent<Props, State> {
+// Convert the DisqusComments component to functional component.
+function DisqusComments({
+  loadDisqusManually,
+  colorScheme,
+  identifier,
+  url,
+  title,
+}: Props): JSX.Element {
+  const [allowDisqus, setAllowDisqus] = useState(false);
+  const isFirstRender = useRef(true);
+
+  const getDisqusConfig = useCallback(
+    () =>
+      // Disqus is configured using a function that modifies 'this', so we cannot use
+      // arrow functions here, which also means we need to rebind values from the outer
+      // this if we need to use them inside the function
+
+      // Can't be arsed to type this bullshit
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      function configDisqus(this: any) {
+        /* eslint-disable react/no-this-in-sfc */
+        this.page.identifier = identifier;
+        this.page.url = url;
+        this.page.title = title;
+        /* eslint-enable react/no-this-in-sfc */
+      },
+    [identifier, url, title],
+  );
+
+  const loadInstance = useCallback(() => {
+    if (loadDisqusManually && !allowDisqus) return;
+
+    if (window.DISQUS) {
+      // See https://help.disqus.com/customer/portal/articles/472107
+      window.DISQUS.reset({
+        reload: true,
+        config: getDisqusConfig(),
+      });
+    } else {
+      // Inject the Disqus script if we're loading it for the first time, ie. when
+      // window.DISQUS is not set
+      window.disqus_config = getDisqusConfig(); // eslint-disable-line camelcase
+      window.disqus_shortname = config.disqusShortname; // eslint-disable-line camelcase
+
+      insertScript(`https://${config.disqusShortname}.disqus.com/embed.js`, {
+        id: SCRIPT_ID,
+        async: true,
+      }).catch(getScriptErrorHandler('Disqus comments'));
+    }
+  }, [allowDisqus, getDisqusConfig, loadDisqusManually]);
+
+  useEffect(() => {
+    // Wait a bit for the page colors to change before reloading instance
+    // 2 second delay is found empirically, and is longer than necessary to
+    // account for lag in slower user agents
+    if (isFirstRender.current) {
+      loadInstance();
+      isFirstRender.current = false;
+    } else {
+      setTimeout(loadInstance, 2000);
+    }
+  }, [loadInstance, colorScheme]);
+
+  if (loadDisqusManually && !allowDisqus) {
+    return (
+      <div className="text-center">
+        <button
+          type="button"
+          onClick={() => setAllowDisqus(true)}
+          className={classnames(styles.loadDisqusBtn, 'btn btn-lg btn-outline-primary')}
+        >
+          <MessageSquare />
+          Load Disqus Comments
+        </button>
+      </div>
+    );
+  }
+
+  return <div id="disqus_thread" />;
+}
+
+class _DisqusComments extends PureComponent<Props, State> {
   override state = {
     allowDisqus: false,
   };
