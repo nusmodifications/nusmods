807c18fe4793d4792c6b927aedb2127a2ffe1d37
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-12-04 21:18:52 +0800

    footer: Migrate to RTL

--- website/src/views/layout/Footer.test.tsx
+++ website/src/views/layout/Footer.test.tsx
@@ -1,8 +1,12 @@
-import { shallow } from 'enzyme';
-
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { FooterComponent } from 'views/layout/Footer';
 
 test('is a footer element', () => {
-  const actual = shallow(<FooterComponent toggleFeedback={jest.fn()} lastUpdatedDate={null} />);
-  expect(actual.type()).toBe('footer');
+  render(
+    <MemoryRouter>
+      <FooterComponent toggleFeedback={jest.fn()} lastUpdatedDate={null} />
+    </MemoryRouter>,
+  );
+  expect(screen.getByRole('contentinfo')).toBeInTheDocument();
 });
