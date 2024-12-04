d4f035226e35a9a75f9447b80ec4e5fea96c5c0f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-12-04 21:07:14 +0800

    warning: Migrate to RTL

--- website/src/views/errors/Warning.test.tsx
+++ website/src/views/errors/Warning.test.tsx
@@ -1,7 +1,8 @@
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import Warning from './Warning';
 
 test('it displays warning message', () => {
-  const wrapper = shallow(<Warning message="abcde/ghi123!@#$" />);
-  expect(wrapper.find('[children="abcde/ghi123!@#$"]')).toHaveLength(1);
+  const message = 'abcde/ghi123!@#$';
+  render(<Warning message={message} />);
+  expect(screen.getByRole('heading')).toHaveTextContent(message);
 });
