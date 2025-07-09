f2c2dc1912736b0f426a2cf20a69474f9b32d23b
leslie yip <leslieyip02@gmail.com>
2025-07-09 23:34:25 +0800

    refactor: move optimiser test defaults into test-utils

--- website/src/utils/optimiser.test.ts
+++ website/src/utils/optimiser.test.ts
@@ -4,6 +4,14 @@ import { Module, WorkingDays } from 'types/modules';
 import { shuffle } from 'lodash';
 import { OptimiseResponse } from 'apis/optimiser';
 import {
+  defaultLectureOption,
+  defaultLectureSlot,
+  defaultRecitationOption,
+  defaultRecitationSlot,
+  defaultTutorialOption,
+  defaultTutorialSlot,
+} from 'test-utils/optimiser';
+import {
   getConflictingDays,
   getDaysForLessonType,
   getDisplayText,
@@ -19,69 +27,6 @@ import {
 import { getModuleTimetable } from './modules';
 
 const defaultModule = CS1010S;
-const defaultLectureOption = {
-  moduleCode: 'CS1010S',
-  lessonType: 'Lecture',
-  colorIndex: 0,
-  lessonKey: 'CS1010S|Lecture',
-  displayText: 'CS1010S Lecture',
-  days: ['Wednesday'],
-};
-const defaultRecitationOption = {
-  moduleCode: 'CS1010S',
-  lessonType: 'Recitation',
-  colorIndex: 0,
-  lessonKey: 'CS1010S|Recitation',
-  displayText: 'CS1010S Recitation',
-  days: ['Thursday', 'Friday'],
-};
-const defaultTutorialOption = {
-  moduleCode: 'CS1010S',
-  lessonType: 'Tutorial',
-  colorIndex: 0,
-  lessonKey: 'CS1010S|Tutorial',
-  displayText: 'CS1010S Tutorial',
-  days: ['Monday', 'Tuesday'],
-};
-const defaultLectureSlot = {
-  classNo: '1',
-  day: 'Wednesday',
-  endTime: '1000',
-  lessonType: 'Lecture',
-  startTime: '0800',
-  venue: 'LT27',
-  coordinates: { x: 103.7809, y: 1.2969925 },
-  StartMin: 480,
-  EndMin: 600,
-  DayIndex: 2,
-  LessonKey: 'CS1010S|Lecture',
-};
-const defaultRecitationSlot = {
-  classNo: '17',
-  day: 'Friday',
-  endTime: '1800',
-  lessonType: 'Recitation',
-  startTime: '1700',
-  venue: 'BIZ2-0201',
-  coordinates: { x: 103.7748, y: 1.2935857 },
-  StartMin: 1020,
-  EndMin: 1080,
-  DayIndex: 4,
-  LessonKey: 'CS1010S|Recitation',
-};
-const defaultTutorialSlot = {
-  classNo: '16',
-  day: 'Monday',
-  endTime: '1600',
-  lessonType: 'Tutorial',
-  startTime: '1500',
-  venue: 'BIZ2-0226',
-  coordinates: { x: 103.7752, y: 1.2932994 },
-  StartMin: 900,
-  EndMin: 960,
-  DayIndex: 0,
-  LessonKey: 'CS1010S|Tutorial',
-};
 
 describe('getLessonKey', () => {
   it('should format unique key', () => {
