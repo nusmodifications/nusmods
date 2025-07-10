6729d4fe454aa579af932b464ef03b928e03e702
leslie yip <leslieyip02@gmail.com>
2025-07-11 00:41:50 +0800

    lint: remove unused import

--- website/src/views/optimiser/OptimiserForm/OptimiserFreeDaySelect.tsx
+++ website/src/views/optimiser/OptimiserForm/OptimiserFreeDaySelect.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { dropRight, omit } from 'lodash';
+import { dropRight } from 'lodash';
 import { useCallback } from 'react';
 import { DayText, WorkingDays } from 'types/modules';
 import { OptimiserFormFields } from 'views/hooks/useOptimiserForm';
