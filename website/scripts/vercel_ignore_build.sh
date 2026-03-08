7963cf07061ae8c175e8e9f54a4b0215246cf067
jloh02 <jonathanloh02@gmail.com>
2026-03-08 23:09:57 +0800

    fix: update for build tag

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# is a forked repo
 if [[ "$VERCEL_GIT_REPO_OWNER" != "nusmodifications" ]]; then
     if [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
         echo "✅ Pull request ID: $VERCEL_GIT_PULL_REQUEST_ID"
@@ -12,8 +13,11 @@ else
     if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
         echo "✅ Core branch detected"
         exit 1
+    elif [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[build]"* ]]; then
+        echo "✅ Build commit detected: $VERCEL_GIT_COMMIT_MESSAGE"
+        exit 1
     else
-        echo "🛑 Core branch not detected ($VERCEL_GIT_COMMIT_REF)"
+        echo "🛑 Build cancelled"
         exit 0
     fi
 fi
