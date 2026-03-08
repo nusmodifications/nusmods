f0be0f4938e9d11ec6dde8a6370c7c9dda1bda67
jloh02 <jonathanloh02@gmail.com>
2026-03-08 22:59:04 +0800

    feat: cleanup messages

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 if [[ "$VERCEL_GIT_REPO_OWNER" != "nusmodifications" &&  -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
-    echo "✅ Fork detected on PR. Proceeding with build."
+    echo "✅ Pull request from fork detected. Proceeding with build."
     exit 1
 fi
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || \
-      "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
-  echo "✅ Core branch detected ($VERCEL_GIT_COMMIT_REF). Proceeding with build."
-  exit 1
+if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
+    echo "✅ Core branch detected ($VERCEL_GIT_COMMIT_REF). Proceeding with build."
+    exit 1
 fi
 
 echo "🛑 Skipping build."
