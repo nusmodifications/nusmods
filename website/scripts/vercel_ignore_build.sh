0e29bea499660103bb8822acbc0512de8fa86a55
jloh02 <jonathanloh02@gmail.com>
2026-03-08 22:58:21 +0800

    fix: update vercel ignore

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# production pushing 
+if [[ "$VERCEL_GIT_REPO_OWNER" != "nusmodifications" &&  -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
+    echo "✅ Fork detected on PR. Proceeding with build."
+    exit 1
+fi
+
 if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || \
       "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
   echo "✅ Core branch detected ($VERCEL_GIT_COMMIT_REF). Proceeding with build."
   exit 1
-
-# PR
-# (note: for maintainers, there is a race condition that may cause the PR build to be ignored if the PR is created and the workflow runs immediately without a PR ID. Just redeploy.)
-elif [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
-  echo "✅ Pull Request detected (ID: $VERCEL_GIT_PULL_REQUEST_ID). Proceeding with build."
-  exit 1
-
-# ignore build
-else
-  echo "🛑 Skipping build."
-  exit 0
 fi
+
+echo "🛑 Skipping build."
+exit 0
+
