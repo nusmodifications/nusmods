6ba37f21263ed1885678536c284aa162a7e4a8b2
jloh02 <jonathanloh02@gmail.com>
2026-03-08 16:34:16 +0800

    fix: allow builds on PRs

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VERCEL_GIT_COMMIT_REF" != "production" ] && [ "$VERCEL_GIT_COMMIT_REF" != "cpex-staging" ]; then 
-    exit 0; 
-else 
-    # Proceed with the build
-    exit 1; 
+# production pushing 
+if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || \
+      "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
+  echo "✅ Core branch detected ($VERCEL_GIT_COMMIT_REF). Proceeding with build."
+  exit 1
+
+# PR
+elif [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
+  echo "✅ Pull Request detected (ID: $VERCEL_GIT_PULL_REQUEST_ID). Proceeding with build."
+  exit 1
+
+# ignore build
+else
+  echo "🛑 Skipping build."
+  exit 0
 fi
