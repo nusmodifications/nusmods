dd4ad9cf7bbd1bbc01925ce9bad502e574e407d3
jloh02 <jonathanloh02@gmail.com>
2026-03-08 16:59:04 +0800

    fix wording

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -7,7 +7,7 @@ if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || \
   exit 1
 
 # PR
-# (note: for maintainers, there is a race condition that may cause the PR build to be ignored if the PR is merged before the build starts. In that case, the build will be ignored because it will look like a push to master/production. However, this is a rare edge case and can be mitigated by ensuring that PRs are merged after the build starts.)
+# (note: for maintainers, there is a race condition that may cause the PR build to be ignored if the PR is created and the workflow runs immediately without a PR ID. Just redeploy.)
 elif [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
   echo "✅ Pull Request detected (ID: $VERCEL_GIT_PULL_REQUEST_ID). Proceeding with build."
   exit 1
