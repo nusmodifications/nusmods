4e04a5d49527ad717374d6378049bb0f53f32d84
jloh02 <jonathanloh02@gmail.com>
2026-03-08 23:04:51 +0800

    feat: update clearer ignore logic

--- website/scripts/vercel_ignore_build.sh
+++ website/scripts/vercel_ignore_build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_REPO_OWNER" != "nusmodifications" &&  -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
-    echo "✅ Pull request from fork detected. Proceeding with build."
-    exit 1
+if [[ "$VERCEL_GIT_REPO_OWNER" != "nusmodifications" ]]; then
+    if [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
+        echo "✅ Pull request ID: $VERCEL_GIT_PULL_REQUEST_ID"
+        exit 1
+    else
+        echo "🛑 No pull request ID found"
+        exit 0
+    fi
+else
+    if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
+        echo "✅ Core branch detected"
+        exit 1
+    else
+        echo "🛑 Core branch not detected ($VERCEL_GIT_COMMIT_REF)"
+        exit 0
+    fi
 fi
-
-if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "production" ]]; then
-    echo "✅ Core branch detected ($VERCEL_GIT_COMMIT_REF). Proceeding with build."
-    exit 1
-fi
-
-echo "🛑 Skipping build."
-exit 0
-
