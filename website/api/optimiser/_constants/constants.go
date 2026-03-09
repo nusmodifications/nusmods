cba65b68d61381a98b7d839b7c1e333b280a9ab0
Thejus <thejusunni03@gmail.com>
2026-03-09 11:43:48 +0800

    refactor(constants): change variable declarations to constants for better clarity

--- website/api/optimiser/_constants/constants.go
+++ website/api/optimiser/_constants/constants.go
@@ -35,18 +35,20 @@ var LessonTypeAbbrev = map[string]string{
 //go:embed venues.json
 var VenuesJson []byte
 
-var ModulesURL = "https://api.nusmods.com/v2/%s/modules/%s.json"
+const ModulesURL = "https://api.nusmods.com/v2/%s/modules/%s.json"
 
-var NUSModsTimetableBaseURL = "https://nusmods.com/timetable"
+const NUSModsTimetableBaseURL = "https://nusmods.com/timetable"
 
-var MAX_WALK_DISTANCE = 0.250 // 250 meters
-var NO_VENUE_PENALTY = 100.0
-var LUNCH_BONUS = -300.0
-var NO_LUNCH_PENALTY = 300.0
-var GAP_PENALTY_THRESHOLD = 120 // 2 hours in minutes
-var GAP_PENALTY_RATE = 100.0
-var LUNCH_REQUIRED_TIME = 60 // 1 hour in minutes
-var CONSECUTIVE_HOURS_PENALTY_RATE = 100
+const (
+	MAX_WALK_DISTANCE            = 0.250 // 250 meters
+	NO_VENUE_PENALTY             = 100.0
+	LUNCH_BONUS                  = -300.0
+	NO_LUNCH_PENALTY             = 300.0
+	GAP_PENALTY_THRESHOLD        = 120 // 2 hours in minutes
+	GAP_PENALTY_RATE             = 100.0
+	LUNCH_REQUIRED_TIME          = 60 // 1 hour in minutes
+	CONSECUTIVE_HOURS_PENALTY_RATE = 100
+)
 
 // This is used by [nusmods_link.SerializeLessonIndices] to serialize the result of optimiser into a timetable share link to return to the client
-var MODULE_CODE_SEPARATOR = ";"
+const MODULE_CODE_SEPARATOR = ";"
