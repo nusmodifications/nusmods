4d08ce50dfea5245bd59806f29346ca92cab6fb1
leslie <leslieyip02@gmail.com>
2026-03-12 02:18:34 +0800

    feat: [build] add GitHub API token

--- vercel_ignore_build.sh
+++ vercel_ignore_build.sh
@@ -17,7 +17,8 @@ if [[ -n "$VERCEL_GIT_PULL_REQUEST_ID" ]]; then
     
     # Query the GitHub API to find the origin of the PR branch.
     dnf install -y jq
-    PR_HEAD_REPO=$(curl -s "https://api.github.com/repos/nusmodifications/$VERCEL_GIT_REPO_SLUG/pulls/$VERCEL_GIT_PULL_REQUEST_ID" | jq -r '.head.repo.full_name')
+    PR_URL="https://api.github.com/repos/nusmodifications/$VERCEL_GIT_REPO_SLUG/pulls/$VERCEL_GIT_PULL_REQUEST_ID" 
+    PR_HEAD_REPO=$(curl -s -m 10 -H "Authorization: Bearer $GITHUB_API_TOKEN" "$PR_URL" | jq -r '.head.repo.full_name')
 
     if [[ -z "$PR_HEAD_REPO" ]]; then
         echo "🛑 Failed to fetch PR info from GitHub API. Build cancelled."
