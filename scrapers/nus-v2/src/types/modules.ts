041218d19bbbf0077cc8f3ab680196d0f2a23a24
Yangshun <tay.yang.shun@gmail.com>
2026-03-15 17:00:47 +0800

    fix(scrapers): satisfy new array-type lint rules

--- scrapers/nus-v2/src/types/modules.ts
+++ scrapers/nus-v2/src/types/modules.ts
@@ -14,7 +14,7 @@ export type LessonTime = StartTime | EndTime;
 export type ModuleCode = string; // E.g. "CS3216"
 export type ModuleTitle = string;
 export type Semester = number; // E.g. 1/2/3/4. 3 and 4 means special sem i and ii.
-export type Workload = string | number[];
+export type Workload = string | Array<number>;
 export type Venue = string;
 
 export type WeekRange = {
@@ -29,12 +29,12 @@ export type WeekRange = {
   weeks?: Array<number>;
 };
 
-export type Weeks = number[] | WeekRange;
+export type Weeks = Array<number> | WeekRange;
 
 // Recursive tree of module codes and boolean operators for the prereq tree
 export type PrereqTree =
   | string
-  | { and?: Array<PrereqTree>; nOf?: [number, PrereqTree[]]; or?: Array<PrereqTree> };
+  | { and?: Array<PrereqTree>; nOf?: [number, Array<PrereqTree>]; or?: Array<PrereqTree> };
 
 // Auxiliary data types
 export type Day =
